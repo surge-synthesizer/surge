2f8f3fe214f66cbe3c7e09ed85af0e29f9255aa5
Paul Walker <paul@pwjw.com>
2018-12-09 10:39:01 -0500

    put in vst for the PR

--- build-osx.sh
+++ build-osx.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 premake5 xcode4
-#xcodebuild build -configuration Release -project surge-vst2.xcodeproj
-#xcodebuild build -configuration Release -project surge-vst3.xcodeproj
+xcodebuild build -configuration Release -project surge-vst2.xcodeproj
+xcodebuild build -configuration Release -project surge-vst3.xcodeproj
 xcodebuild build -configuration Release -project surge-au.xcodeproj
