738b535d891b29480bdebe33d36dd80d6d7870e1
Esa Juhani Ruoho <esaruoho@gmail.com>
2018-12-13 17:58:11 +0200

    Swedish->English

--- src/common/dsp/Oscillator.cpp
+++ src/common/dsp/Oscillator.cpp
@@ -120,7 +120,8 @@ void osc_sine::process_block(float pitch, float drift, bool stereo, bool FM, flo
 
          // const __m128 scale = _mm_set1_ps(0.000030517578125);
 
-         // HACK för att testa sine(__m64)
+			// HACK fÃ¶r att testa sine(__m64)
+			// TRANSLATE: HACK for testing sine(__m64)
          // const __m64 rate = _mm_set1_pi16(0x0040);
 
          /*m64phase = _mm_add_pi16(m64phase,rate);
@@ -147,7 +148,8 @@ void osc_sine::process_block(float pitch, float drift, bool stereo, bool FM, flo
 /* add controls:
 input L/R
 gain
-limiter?*/
+limiter?
+*/
 
 osc_audioinput::osc_audioinput(SurgeStorage* storage, OscillatorStorage* oscdata, pdata* localcopy)
     : Oscillator(storage, oscdata, localcopy)
