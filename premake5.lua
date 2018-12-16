ec0cebcc400fd8a2b0ff03ccb00e22e1650d06f1
Esa Juhani Ruoho <esaruoho@gmail.com>
2018-12-16 20:05:02 +0200

    from https://github.com/kurasu/surge/pull/70/files
    from https://github.com/kurasu/surge/pull/70/files

--- premake5.lua
+++ premake5.lua
@@ -307,8 +307,11 @@ if VST24SDK then
 	files {
 		"src/vst2/**.cpp",
 		"src/vst2/**.h",
-		VST24SDK .. "/public.sdk/source/vst2.x/**.cpp",
-		"vst3sdk/public.sdk/source/vst2.x/**.cpp",
+		VST24SDK .. "/public.sdk/source/vst2.x/audioeffect.cpp",
+		VST24SDK .. "/public.sdk/source/vst2.x/audioeffectx.cpp",
+		VST24SDK .. "/public.sdk/source/vst2.x/vstplugmain.cpp",
+--	"vst3sdk/public.sdk/source/vst/vst2wrapper/vst2wrapper.cpp",
+--	"vst3sdk/public.sdk/source/vst/vst2wrapper/vst2wrapper.sdk.cpp",
 		VSTGUI .. "plugin-bindings/aeffguieditor.cpp",
 	}
 
