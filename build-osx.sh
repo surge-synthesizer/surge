2052f60b9336439e45933e266bd47944344a8d91
Rich Elmes <richie@juic3.com>
2018-12-17 19:33:21 +0100

    Only build VST2 under macOS if VST2SDK_DIR variable set, clean projects before building.

--- build-osx.sh
+++ build-osx.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 premake5 xcode4
-xcodebuild build -configuration Release -project surge-vst2.xcodeproj
+if [ -n "$VST2SDK_DIR"]; then
+	xcodebuild clean -project surge-vst2.xcodeproj
+	xcodebuild build -configuration Release -project surge-vst2.xcodeproj
+fi
+xcodebuild clean -project surge-vst3.xcodeproj
 xcodebuild build -configuration Release -project surge-vst3.xcodeproj
+xcodebuild clean -project surge-au.xcodeproj
 xcodebuild build -configuration Release -project surge-au.xcodeproj
