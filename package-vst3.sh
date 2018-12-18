bd61d91836315724346038f0efa9ad1e752aa107
Keith Zantow <kzantow@gmail.com>
2018-12-18 17:23:47 -0500

    Fix VST2 project creation and packaging

--- package-vst3.sh
+++ package-vst3.sh
@@ -2,18 +2,19 @@
 
 # input config
 RES_SRC_LOCATION="resources"
-PACKAGE_SRC_LOCATION="$RES_SRC_LOCATION/osx-vst2"
+PACKAGE_SRC_LOCATION="$RES_SRC_LOCATION/osx-vst3"
 BITMAP_SRC_LOCATION="$RES_SRC_LOCATION/bitmaps"
 BUNDLE_RES_SRC_LOCATION="$RES_SRC_LOCATION/osx-resources"
-EXEC_LOCATION="target/vst2/Release/Surge.dylib"
+EXEC_LOCATION="target/vst3/Release/Surge.dylib"
+#EXEC_LOCATION="target/vst3/Debug/Surge-Debug.dylib"
 
 # output configs
 OUTPUT_DIR=products
-BUNDLE_NAME="Surge.vst"
+BUNDLE_NAME="Surge.vst3"
 BUNDLE_DIR="$OUTPUT_DIR/$BUNDLE_NAME"
 EXEC_TARGET_NAME="Surge"
 
-echo Creating VST Bundle...
+echo "Creating VST 3 Bundle..."
 
 # create basic bundle structure
 
@@ -32,6 +33,4 @@ cp $PACKAGE_SRC_LOCATION/* "$BUNDLE_DIR/Contents/"
 # copy bundle resources
 cp -R "$BUNDLE_RES_SRC_LOCATION" "$BUNDLE_DIR/Contents/Resources"
 cp $BITMAP_SRC_LOCATION/* "$BUNDLE_DIR/Contents/Resources/"
-mkdir -p "$BUNDLE_DIR/Contents/Data"
-cp -rf resources/data "$BUNDLE_DIR/Contents/Data"
-
+rsync -r --delete resources/data/ "$BUNDLE_DIR/Contents/Data/"
--- premake5.lua
+++ premake5.lua
@@ -153,7 +153,6 @@ function plugincommon()
 		"libs/filesystem/filesystem.cpp",
 		"src/common/vt_dsp/*.cpp",
 		"src/common/thread/*.cpp",
-		"vst3sdk/pluginterfaces/base/*.cpp",
 		}
 
 	includedirs {
@@ -312,7 +311,6 @@ if VST24SDK then
 		"src/vst2/**.cpp",
 		"src/vst2/**.h",
 		VST24SDK .. "/public.sdk/source/vst2.x/**.cpp",
-		"vst3sdk/public.sdk/source/vst2.x/**.cpp",
 		VSTGUI .. "plugin-bindings/aeffguieditor.cpp",
 	}
 
@@ -323,7 +321,6 @@ if VST24SDK then
 	includedirs {
 		"src/vst2",
 		VST24SDK,
-		"vst3sdk"
 	}
 
 	configuration { "Debug" }
@@ -385,6 +382,7 @@ files {
 	"vst3sdk/public.sdk/source/vst/vstcomponentbase.cpp",
 	"vst3sdk/public.sdk/source/vst/vstbus.cpp",
 	"vst3sdk/public.sdk/source/vst/vstparameters.cpp",
+	"vst3sdk/pluginterfaces/base/*.cpp",
 	}
 
 excludes {
