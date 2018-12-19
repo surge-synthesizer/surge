ea436f2137e8808e30f13b208e764254a1d0fe08
Keith Zantow <kzantow@gmail.com>
2018-12-18 21:22:26 -0500

    add local dev install script for all builds

--- install-local.sh
+++ install-local.sh
@@ -1,5 +1,9 @@
 ./build-osx.sh
 
+rsync -r "resources/data/" "$HOME/Library/Application Support/Surge/"
+
+if [ -n "$VST2SDK_DIR" ]; then
+    rsync -r --delete "products/Surge.vst/" ~/Library/Audio/Plug-Ins/VST/Surge.vst/
+fi
 rsync -r --delete "products/Surge.component/" ~/Library/Audio/Plug-Ins/Components/Surge.component/
-rsync -r --delete "products/Surge.vst/" ~/Library/Audio/Plug-Ins/VST/Surge.vst/
 rsync -r --delete "products/Surge.vst3/" ~/Library/Audio/Plug-Ins/VST3/Surge.vst3/
