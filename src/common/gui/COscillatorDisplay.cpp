615f76a77ddcac1a8952b4d0528830429632e49b
Esa Juhani Ruoho <esaruoho@gmail.com>
2019-03-06 22:07:05 +0200

    open WT context menu with RMB too

--- src/common/gui/COscillatorDisplay.cpp
+++ src/common/gui/COscillatorDisplay.cpp
@@ -259,7 +259,7 @@ bool COscillatorDisplay::onDrop(IDataPackage* drag, const CPoint& where)
 
 CMouseEventResult COscillatorDisplay::onMouseDown(CPoint& where, const CButtonState& button)
 {
-   if (!(button & kLButton))
+   if (!((button & kLButton) || (button & kRButton)))
       return kMouseDownEventHandledButDontNeedMovedOrUpEvents;
 
    assert(oscdata);
