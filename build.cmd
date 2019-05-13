e1852501d37fb01a4e8887e83b9961e29e549f65
Jarkko Sakkinen <jarkko.sakkinen@iki.fi>
2019-05-13 22:59:49 +0300

    Enable cmake build for surge-headless on Windows
    Add necessary to tweaks to CMakeLists.txt to generate build files for
    Windows and remove surge-headless target from premake5.lua. Update
    build.cmd to run surge-headless build by using CMakeLists.txt

--- build.cmd
+++ build.cmd
@@ -1,9 +1,14 @@
-git submodule update --init --recursive || goto error
+REM We can still at least try to build even if Git is not installed.
+git submodule update --init --recursive || echo "Git failed, submodules not updated."
 
 premake5 vs2017 || goto error
 REM msbuild /target:Build surge-vst2.vcxproj /maxcpucount /p:Configuration=Release;Platform=x64 || goto error
 REM msbuild /target:Build surge-vst3.vcxproj /maxcpucount /p:Configuration=Release;Platform=x64 || goto error
 msbuild buildtask.xml /maxcpucount
+
+rd /s /q build
+cmake -Bbuild .
+msbuild /target:Build build\surge-headless.vcxproj
 
 exit /b 0
 
