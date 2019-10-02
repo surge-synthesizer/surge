bf392b23460de392f93f9f226ddf6b0e575436bd
Paul <baconpaul@users.noreply.github.com>
2019-10-02 15:01:40 -0400

    VST3 Midi Mapping (partially complete) (#1227)
    VST3 Midi Mapipng was outside the range of parameters so strict VST3
    hosts - notably cubase - dropped mod wheel, pitch wheel, etc...
    
    Addresses #1050 and subject to further testing may close it.

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+((nil . ((indent-tabs-mode . nil)))
+
+ (c-mode . ((c-file-style . "linux")
+            (tab-width . 3)
+            (c-basic-offset . 3)
+            ))
+
+ (c++-mode . ((c-file-style . "linux")
+              (tab-width . 3)
+              (c-basic-offset . 3)
+              ))
+ )
