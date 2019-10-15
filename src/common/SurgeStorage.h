b6618b42a2a87ce621c2f3b8b8208fd8cc1cffd6
Paul <paul@pwjw.com>
2019-10-15 09:24:09 -0400

    Provide a cleaner error message for windows.wt
    If Windows.wt is missing, surge can go horribly awry. We
    issued no error message in this case. So do so now.
    
    Closes #1247

--- src/common/SurgeStorage.h
+++ src/common/SurgeStorage.h
@@ -550,7 +550,7 @@ public:
 
    void load_wt(int id, Wavetable* wt);
    void load_wt(std::string filename, Wavetable* wt);
-   void load_wt_wt(std::string filename, Wavetable* wt);
+   bool load_wt_wt(std::string filename, Wavetable* wt);
    // void load_wt_wav(std::string filename, Wavetable* wt);
    void load_wt_wav_portable(std::string filename, Wavetable *wt);
    void clipboard_copy(int type, int scene, int entry);
