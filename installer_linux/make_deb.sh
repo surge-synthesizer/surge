44e2fedc335cd32984395e8243c85fd6a7d7b562
Brian Ginsburg <7957636+bgins@users.noreply.github.com>
2020-02-29 07:23:03 -0800

    Update deb package and linux storage location (#1592)
    Update linux build to use "surge" instead of "Surge" as a data path.
    Add checks in SurgeStorage.cpp for the lowercase datapath and use the lowercase name for SURGE_NAME in the deb.
    Update debian control file description and maintainer. Add Source field.
    Rename LICENSE to copyright in the deb.
    Compress the changelog, update its location in the deb, and change its date formatting.
    Remove executable bit from .so files.
    Lowercase surge name in xdgDataPath case.
    Update azure pipeline with lowercase name.

--- installer_linux/make_deb.sh
+++ installer_linux/make_deb.sh
@@ -31,7 +31,7 @@ fi
 
 
 # Names
-SURGE_NAME=Surge
+SURGE_NAME=surge
 PACKAGE_NAME="$SURGE_NAME"
 # SURGE_NAME=surge-synthesizer
 
@@ -53,20 +53,23 @@ if [[ -f ${PACKAGE_NAME}/DEBIAN/control ]]; then
 fi
 touch ${PACKAGE_NAME}/DEBIAN/control
 cat <<EOT >> ${PACKAGE_NAME}/DEBIAN/control
+Source: ${PACKAGE_NAME}
 Package: ${PACKAGE_NAME}
 Version: $DEB_VERSION
 Architecture: amd64
-Maintainer: surgeteam
+Maintainer: surgeteam <noreply@github.com>
 Depends: libcairo2, libfontconfig1, libfreetype6, libx11-6, libxcb-cursor0, libxcb-util1, libxcb-xkb1, libxcb1, libxkbcommon-x11-0, libxkbcommon0, fonts-lato, xdg-utils, zenity
 Provides: vst-plugin
 Section: sound
 Priority: optional
-Description: Surge Synthesizer plugin
+Description: Subtractive hybrid synthesizer virtual instrument
+ Surge includes VST2, VST3, and LV2 virtual instrument formats for use in compatible hosts.
 EOT
 
-DATE=`date`
+touch ${PACKAGE_NAME}/usr/share/${SURGE_NAME}/doc/changelog.Debian
+DATE=`date --rfc-email`
 MSG=`git log -n 1 --pretty="%s (git hash %H)"`
-cat <<EOT > ${PACKAGE_NAME}/DEBIAN/changelog
+cat <<EOT > ${PACKAGE_NAME}/usr/share/${SURGE_NAME}/doc/changelog.Debian
 ${PACKAGE_NAME} (${DEB_VERSION}) stable; urgency=medium
 
   * ${MSG}
@@ -74,20 +77,26 @@ ${PACKAGE_NAME} (${DEB_VERSION}) stable; urgency=medium
 
  -- Surge Synthesizer Team <noreply@github.com>  ${DATE}
 EOT
+gzip -9 -n ${PACKAGE_NAME}/usr/share/${SURGE_NAME}/doc/changelog.Debian
 
 #copy data and vst plugins
 
-cp ../LICENSE ${PACKAGE_NAME}/usr/share/${SURGE_NAME}/doc
+cp ../LICENSE ${PACKAGE_NAME}/usr/share/${SURGE_NAME}/doc/copyright
 cp -r ../resources/data/* ${PACKAGE_NAME}/usr/share/${SURGE_NAME}/
 cp ../target/vst2/Release/Surge.so ${PACKAGE_NAME}/usr/lib/vst/${SURGE_NAME}.so
 
 # Once VST3 works, this will be ../products/vst3
 cp -r ../products/Surge.vst3 ${PACKAGE_NAME}/usr/lib/vst3/
 
-#copy the lv2 bundle
+# copy the lv2 bundle
 cp -r ../target/lv2/Release/Surge.lv2 ${PACKAGE_NAME}/usr/lib/lv2/
 
-#build package
+# set permissions on shared libraries
+chmod -R 0644 ${PACKAGE_NAME}/usr/lib/vst/${SURGE_NAME}.so
+find ${PACKAGE_NAME}/usr/lib/vst3/ -type f -iname "*.so" | xargs chmod 0644
+find ${PACKAGE_NAME}/usr/lib/lv2/ -type f -iname "*.so" | xargs chmod 0644
+
+# build package
 
 mkdir -p product
 dpkg-deb --build ${PACKAGE_NAME} product/${PACKAGE_NAME}-linux-x64-${VERSION}.deb
