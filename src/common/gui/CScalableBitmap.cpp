71b4c7813f7b4c494d8dac981b8c24b71d714903
Paul Walker (baconpaul) <paul@pwjw.com>
2020-06-07 16:41:09 -0400

    Linux Transparenty Fix 1-of-2
    This change re-enables linux transaprency and with the updates
    we made previously to VSTGUI mostly works just fine, except in one
    case which is documented in #2053

--- src/common/gui/CScalableBitmap.cpp
+++ src/common/gui/CScalableBitmap.cpp
@@ -380,7 +380,9 @@ void CScalableBitmap::draw (CDrawContext* context, const CRect& rect, const CPoi
           context->setClipRect(clipR);
 
 #if LINUX
-          alpha = 1.0;
+         if( alpha > 1.0 ) alpha /= 255.0;
+         if( alpha > 1.0 ) alpha = 1.0;
+         if( alpha < 0.0 ) alpha = 0.0;
 #endif
           offscreenCache[offset]->draw(context, drect, CPoint(0, 0), alpha);
           context->setClipRect(origClip);
