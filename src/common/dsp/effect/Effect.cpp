6f3be402f9da4ed984e0e2850eec6b13455a64f0
EvilDragon <mario.kruselj@gmail.com>
2020-06-25 21:01:58 +0200

    Add Mix slider to EQ, enum for EQ parameter names (#2195)
    * Add Mix slider to EQ, enum for EQ parameter names
    
    * SSE the EQ processing buffer accumulation
    
    * Wrong SSE command used -_-

--- src/common/dsp/effect/Effect.cpp
+++ src/common/dsp/effect/Effect.cpp
@@ -86,11 +86,12 @@ void Effect::init_ctrltypes()
 Eq3BandEffect::Eq3BandEffect(SurgeStorage* storage, FxStorage* fxdata, pdata* pd)
     : Effect(storage, fxdata, pd), band1(storage), band2(storage), band3(storage)
 {
-   gain.set_blocksize(BLOCK_SIZE);
-
-   band1.setBlockSize(BLOCK_SIZE * slowrate); // does not matter ATM as tey're smoothed
+   band1.setBlockSize(BLOCK_SIZE * slowrate); // does not matter ATM as they're smoothed
    band2.setBlockSize(BLOCK_SIZE * slowrate);
    band3.setBlockSize(BLOCK_SIZE * slowrate);
+
+   gain.set_blocksize(BLOCK_SIZE);
+   mix.set_blocksize(BLOCK_SIZE);
 }
 
 Eq3BandEffect::~Eq3BandEffect()
@@ -109,23 +110,26 @@ void Eq3BandEffect::setvars(bool init)
 {
    if (init)
    {
-      gain.set_target(1.f); // db_to_linear(fxdata->p[9].val.f));
-      gain.instantize();
-      band1.coeff_peakEQ(band1.calc_omega(fxdata->p[1].val.f * (1.f / 12.f)), fxdata->p[2].val.f,
-                         1.f); // Set the bands to 0dB so the EQ fades in initiallt
-      band2.coeff_peakEQ(band2.calc_omega(fxdata->p[4].val.f * (1.f / 12.f)), fxdata->p[5].val.f,
-                         1.f);
-      band3.coeff_peakEQ(band3.calc_omega(fxdata->p[7].val.f * (1.f / 12.f)), fxdata->p[8].val.f,
-                         1.f);
+      // Set the bands to 0dB so the EQ fades in init
+      band1.coeff_peakEQ(band1.calc_omega(fxdata->p[eq3_freq1].val.f * (1.f / 12.f)), fxdata->p[eq3_bw1].val.f, 1.f);
+      band2.coeff_peakEQ(band2.calc_omega(fxdata->p[eq3_freq2].val.f * (1.f / 12.f)), fxdata->p[eq3_bw2].val.f, 1.f);
+      band3.coeff_peakEQ(band3.calc_omega(fxdata->p[eq3_freq3].val.f * (1.f / 12.f)), fxdata->p[eq3_bw3].val.f, 1.f);
+
       band1.coeff_instantize();
       band2.coeff_instantize();
       band3.coeff_instantize();
+
+      gain.set_target(1.f);
+      mix.set_target(1.f);
+
+      gain.instantize();
+      mix.instantize();
    }
    else
    {
-      band1.coeff_peakEQ(band1.calc_omega(*f[1] * (1.f / 12.f)), *f[2], *f[0]);
-      band2.coeff_peakEQ(band2.calc_omega(*f[4] * (1.f / 12.f)), *f[5], *f[3]);
-      band3.coeff_peakEQ(band3.calc_omega(*f[7] * (1.f / 12.f)), *f[8], *f[6]);
+      band1.coeff_peakEQ(band1.calc_omega(*f[eq3_freq1] * (1.f / 12.f)), *f[eq3_bw1], *f[eq3_gain1]);
+      band2.coeff_peakEQ(band2.calc_omega(*f[eq3_freq2] * (1.f / 12.f)), *f[eq3_bw2], *f[eq3_gain2]);
+      band3.coeff_peakEQ(band3.calc_omega(*f[eq3_freq3] * (1.f / 12.f)), *f[eq3_bw3], *f[eq3_gain3]);
    }
 }
 
@@ -135,12 +139,18 @@ void Eq3BandEffect::process(float* dataL, float* dataR)
       setvars(false);
    bi = (bi + 1) & slowrate_m1;
 
-   band1.process_block(dataL, dataR);
-   band2.process_block(dataL, dataR);
-   band3.process_block(dataL, dataR);
+   copy_block(dataL, L, BLOCK_SIZE_QUAD);
+   copy_block(dataR, R, BLOCK_SIZE_QUAD);
 
-   gain.set_target_smoothed(db_to_linear(*f[9]));
-   gain.multiply_2_blocks(dataL, dataR, BLOCK_SIZE_QUAD);
+   band1.process_block(L, R);
+   band2.process_block(L, R);
+   band3.process_block(L, R);
+
+   gain.set_target_smoothed(db_to_linear(*f[eq3_gain]));
+   gain.multiply_2_blocks(L, R, BLOCK_SIZE_QUAD);
+
+   mix.set_target_smoothed(limit_range(*f[eq3_mix], -1.f, 1.f));
+   mix.fade_2_blocks_to(dataL, L, dataR, R, dataL, dataR, BLOCK_SIZE_QUAD);
 }
 
 void Eq3BandEffect::suspend()
@@ -183,48 +193,60 @@ void Eq3BandEffect::init_ctrltypes()
 {
    Effect::init_ctrltypes();
 
-   fxdata->p[0].set_name("Gain 1");
-   fxdata->p[0].set_type(ct_decibel);
-   fxdata->p[1].set_name("Frequency 1");
-   fxdata->p[1].set_type(ct_freq_audible);
-   fxdata->p[2].set_name("Bandwidth 1");
-   fxdata->p[2].set_type(ct_bandwidth);
+   fxdata->p[eq3_gain1].set_name("Gain 1");
+   fxdata->p[eq3_gain1].set_type(ct_decibel);
+   fxdata->p[eq3_freq1].set_name("Frequency 1");
+   fxdata->p[eq3_freq1].set_type(ct_freq_audible);
+   fxdata->p[eq3_bw1].set_name("Bandwidth 1");
+   fxdata->p[eq3_bw1].set_type(ct_bandwidth);
 
-   fxdata->p[3].set_name("Gain 2");
-   fxdata->p[3].set_type(ct_decibel);
-   fxdata->p[4].set_name("Frequency 2");
-   fxdata->p[4].set_type(ct_freq_audible);
-   fxdata->p[5].set_name("Bandwidth 2");
-   fxdata->p[5].set_type(ct_bandwidth);
+   fxdata->p[eq3_gain2].set_name("Gain 2");
+   fxdata->p[eq3_gain2].set_type(ct_decibel);
+   fxdata->p[eq3_freq2].set_name("Frequency 2");
+   fxdata->p[eq3_freq2].set_type(ct_freq_audible);
+   fxdata->p[eq3_bw2].set_name("Bandwidth 2");
+   fxdata->p[eq3_bw2].set_type(ct_bandwidth);
 
-   fxdata->p[6].set_name("Gain 3");
-   fxdata->p[6].set_type(ct_decibel);
-   fxdata->p[7].set_name("Frequency 3");
-   fxdata->p[7].set_type(ct_freq_audible);
-   fxdata->p[8].set_name("Bandwidth 3");
-   fxdata->p[8].set_type(ct_bandwidth);
+   fxdata->p[eq3_gain3].set_name("Gain 3");
+   fxdata->p[eq3_gain3].set_type(ct_decibel);
+   fxdata->p[eq3_freq3].set_name("Frequency 3");
+   fxdata->p[eq3_freq3].set_type(ct_freq_audible);
+   fxdata->p[eq3_bw3].set_name("Bandwidth 3");
+   fxdata->p[eq3_bw3].set_type(ct_bandwidth);
 
-   fxdata->p[9].set_name("Gain");
-   fxdata->p[9].set_type(ct_decibel);
+   fxdata->p[eq3_gain].set_name("Gain");
+   fxdata->p[eq3_gain].set_type(ct_decibel);
+   fxdata->p[eq3_mix].set_name("Mix");
+   fxdata->p[eq3_mix].set_type(ct_percent_bidirectional);
+   fxdata->p[eq3_mix].val_default.f = 1.f;
 
-   for (int i = 0; i < 10; i++)
+   for (int i = 0; i < eq3_num_ctrls; i++)
       fxdata->p[i].posy_offset = 1 + 2 * (i / 3);
 }
 void Eq3BandEffect::init_default_values()
 {
-   fxdata->p[0].val.f = 0.f;
-   fxdata->p[1].val.f = -2.5f * 12.f;
-   fxdata->p[2].val.f = 2.f;
+   fxdata->p[eq3_gain1].val.f = 0.f;
+   fxdata->p[eq3_freq1].val.f = -2.5f * 12.f;
+   fxdata->p[eq3_bw1].val.f = 2.f;
 
-   fxdata->p[3].val.f = 0.f;
-   fxdata->p[4].val.f = 0.5f * 12.f;
-   fxdata->p[5].val.f = 2.f;
+   fxdata->p[eq3_gain2].val.f = 0.f;
+   fxdata->p[eq3_freq2].val.f = 0.5f * 12.f;
+   fxdata->p[eq3_bw2].val.f = 2.f;
 
-   fxdata->p[6].val.f = 0.f;
-   fxdata->p[7].val.f = 4.5f * 12.f;
-   fxdata->p[8].val.f = 2.f;
+   fxdata->p[eq3_gain3].val.f = 0.f;
+   fxdata->p[eq3_freq3].val.f = 4.5f * 12.f;
+   fxdata->p[eq3_bw3].val.f = 2.f;
 
-   fxdata->p[9].val.f = 0.f;
+   fxdata->p[eq3_gain].val.f = 0.f;
+   fxdata->p[eq3_mix].val.f = 1.f;
+}
+
+void Eq3BandEffect::handleStreamingMismatches(int streamingRevision, int currentSynthStreamingRevision)
+{
+   if (streamingRevision <= 12)
+   {
+       fxdata->p[eq3_mix].val.f = 1.f;
+   }
 }
 
 /* chorus */
