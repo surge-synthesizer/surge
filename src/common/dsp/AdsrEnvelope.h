133a6c8179545cae75a01e55ae31713469bb91ee
Paul <baconpaul@users.noreply.github.com>
2020-07-05 22:22:13 -0400

    Update the Copyright Notice (#2297)
    Copyright now spans multiple individuals; update the various
    old copyright notices in the code
    
    Closes #2284

--- src/common/dsp/AdsrEnvelope.h
+++ src/common/dsp/AdsrEnvelope.h
@@ -1,6 +1,18 @@
-//-------------------------------------------------------------------------------------------------------
-//	Copyright 2005 Claes Johanson & Vember Audio
-//-------------------------------------------------------------------------------------------------------
+/*
+** Surge Synthesizer is Free and Open Source Software
+**
+** Surge is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html
+**
+** Copyright 2004-2020 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** Surge was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Surge
+** open source in September 2018.
+*/
+
 #pragma once
 
 #include "DspUtilities.h"
