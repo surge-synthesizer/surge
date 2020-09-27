4009a807f8ee23e09abdf9b2a1d7636e2f4895ec
Paul Walker <paul@pwjw.com>
2020-09-27 19:37:52 -0400

    Vertical Label; Drag Threshold; Small Skin Change
    - Vertical label is now vertically centered. Closes #2704
    - Control drag has a 3px threshold. Closes #2703
    - Fix a tiny classic skin problem

---
+++ src/common/gui/CVerticalLabel.h
@@ -0,0 +1,28 @@
+/*
+** Surge Synthesizer is Free and Open Source Software
+**
+** Surge is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html
+**
+** Copyright 2004-2020 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** Surge was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Surge
+** open source in September 2018.
+*/
+
+#pragma once
+#include "vstcontrols.h"
+
+class CVerticalLabel : public VSTGUI::CTextLabel
+{
+public:
+   CVerticalLabel(const VSTGUI::CRect &pos, VSTGUI::UTF8String txt ) : CTextLabel( pos, txt ) {}
+
+   virtual void draw(VSTGUI::CDrawContext *pContext) override;
+
+   CLASS_METHODS(CVerticalLabel, VSTGUI::CTextLabel );
+
+};
