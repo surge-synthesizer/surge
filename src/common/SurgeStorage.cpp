surge-synthesizer/surge

commit c8766229087617fbb333da445ffa5af6763cea02

Windows: Switch to Unicode (#2724)
This makes Windows builds Unicode-aware, most notably unlocking the full
range of NTFS file system paths.

To avoid #ifdef, paths in std::strings are now UTF-8, like on other
platforms. They are converted to and from the native UTF-16 as needed.
This is mostly transparent, except when converting between fs::path and
std::string. There, the following should be observed:

- Use path_to_string() instead of .generic_path()
- Don't pass strings into fs::path API, convert them to paths first via
  string_to_path()

Otherwise, on Windows and if the path contains chars other than 7-bit
ASCII, either a system_error will be thrown, or (less likely) the path
could be mistranscoded.

This should become less and less of an issue as fs::path <-> std::string
conversions are refactored away.

Fixes #2579

src/common/SurgeStorage.cpp
@@ -287,24 +287,23 @@ SurgeStorage::SurgeStorage(std::string suppliedDataPath) : otherscene_clients(0)
 #if TARGET_RACK
    datapath = suppliedDataPath;
 #else
-   bool foundSurge = false;
-   std::string dllPath = "";
+   fs::path dllPath;
 
    // First check the portable mode sitting beside me
    {
-      CHAR path[MAX_PATH];
+      WCHAR pathBuf[MAX_PATH];
       HMODULE hm = NULL;
 
-      if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | 
-                            GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
-                            (LPCSTR) &dummyExportedWindowsToLookupDLL, &hm) == 0)
+      if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS |
+                                GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+                            reinterpret_cast<LPCTSTR>(&dummyExportedWindowsToLookupDLL), &hm) == 0)
       {
          int ret = GetLastError();
          printf( "GetModuleHandle failed, error = %d\n", ret);
          // Return or however you want to handle an error.
          goto bailOnPortable;
       }
-      if (GetModuleFileName(hm, path, sizeof(path)) == 0)
+      if (GetModuleFileName(hm, pathBuf, std::size(pathBuf)) == 0)
       {
          int ret = GetLastError();
          printf( "GetModuleFileName failed, error = %d\n", ret);
@@ -312,64 +311,56 @@ SurgeStorage::SurgeStorage(std::string suppliedDataPath) : otherscene_clients(0)
          goto bailOnPortable;
       }
 
-      // The path variable should now contain the full filepath for this DLL.
-      std::string pn = path;
-      int ep;
-      if( (ep = pn.rfind( "\\" )) != string::npos )
+      // The pathBuf variable should now contain the full filepath for this DLL.
+      fs::path path(pathBuf);
+      path.remove_filename();
+      dllPath = path;
+      path /= L"SurgeData";
+      if (fs::is_directory(path))
       {
-         dllPath = pn.substr(0,ep);;
-         auto spath = pn.substr( 0, ep ) + "\\SurgeData\\";
-         datapath = spath;
-         if( fs::is_directory( fs::path( spath ) ) )
-         {
-            foundSurge = true;
-         }
-         else
-         {
-            datapath = "";
-         }
+         datapath = path_to_string(path);
       }
    }
 bailOnPortable:
-   
-   PWSTR commonAppData;
-   if( ! foundSurge ) {
+
+   if (datapath.empty())
+   {
+      PWSTR commonAppData;
       if (!SHGetKnownFolderPath(FOLDERID_ProgramData, 0, nullptr, &commonAppData))
       {
-         CHAR path[4096];
-         wsprintf(path, "%S\\Surge\\", commonAppData);
-         datapath = path;
-         if( fs::is_directory( fs::path( datapath ) ) )
-            foundSurge = true;
-         else
-            datapath = "";
+         fs::path path(commonAppData);
+         path /= L"Surge";
+         if (fs::is_directory(path))
+         {
+            datapath = path_to_string(path);
+         }
       }
    }
-   
-   if( ! foundSurge ) {
+
+   if (datapath.empty())
+   {
       PWSTR localAppData;
       if (!SHGetKnownFolderPath(FOLDERID_LocalAppData, 0, nullptr, &localAppData))
       {
-         CHAR path[4096];
-         wsprintf(path, "%S\\Surge\\", localAppData);
-         datapath = path;
-         foundSurge = true;
+         fs::path path(localAppData);
+         path /= L"Surge";
+         datapath = path_to_string(path);
       }
    }
 
    // Portable - first check for dllPath\\SurgeUserData
-   if( dllPath != "" && fs::is_directory( fs::path( dllPath + "\\SurgeUserData\\" )))
+   if (!dllPath.empty() && fs::is_directory(dllPath / L"SurgeUserData"))
    {
-      userDataPath = dllPath + "\\SurgeUserData\\";
+      userDataPath = path_to_string(dllPath / L"SurgeUserData");
    }
    else 
    {
       PWSTR documentsFolder;
       if (!SHGetKnownFolderPath(FOLDERID_Documents, 0, nullptr, &documentsFolder))
       {
-         CHAR path[4096];
-         wsprintf(path, "%S\\Surge\\", documentsFolder);
-         userDataPath = path;
+         fs::path path(documentsFolder);
+         path /= L"Surge";
+         userDataPath = path_to_string(path);
       }
    }
 #endif
@@ -382,6 +373,10 @@ bailOnPortable:
    {
        userDataPath = userSpecifiedDataPath;
    }
+
+   // append separator if not present
+   userDataPath = Surge::Storage::appendDirectory(userDataPath, std::string());
+   datapath = Surge::Storage::appendDirectory(datapath, std::string());
 
    userFXPath = Surge::Storage::appendDirectory(userDataPath, "FXSettings");
    
@@ -640,9 +635,9 @@ void SurgeStorage::refreshPatchOrWTListAddDir(bool userDir,
 {
    int category = categories.size();
 
-   fs::path patchpath = (userDir ? userDataPath : datapath);
+   fs::path patchpath = string_to_path(userDir ? userDataPath : datapath);
    if (!subdir.empty())
-      patchpath.append(subdir);
+      patchpath /= string_to_path(subdir);
 
    if (!fs::is_directory(patchpath))
    {
@@ -678,46 +673,31 @@ void SurgeStorage::refreshPatchOrWTListAddDir(bool userDir,
    ** with a substr in the main loop, so get the length once
    */
    std::vector<PatchCategory> local_categories;
-   int patchpathSubstrLength= patchpath.generic_string().size() + 1;
-   if (patchpath.generic_string().back() == '/' || patchpath.generic_string().back() == '\\')
-       patchpathSubstrLength --;
+   auto patchpathStr(path_to_string(patchpath));
+   auto patchpathSubstrLength = patchpathStr.size() + 1;
+   if (patchpathStr.back() == '/' || patchpathStr.back() == '\\')
+      patchpathSubstrLength--;
    
    for (auto &p : alldirs )
    {
       PatchCategory c;
-#if WINDOWS && ! TARGET_RACK
-      /*
-      ** Windows filesystem names are properly wstrings which, if we want them to 
-      ** display properly in vstgui, need to be converted to UTF8 using the 
-      ** windows widechar API. Linux and Mac do not require this.
-      */
-      std::wstring str = p.wstring().substr(patchpathSubstrLength);
-      c.name = Surge::Storage::wstringToUTF8(str);
-#else
-      c.name = p.generic_string().substr(patchpathSubstrLength);
-#endif
+      c.name = path_to_string(p).substr(patchpathSubstrLength);
       c.internalid = category;
 
       c.numberOfPatchesInCatgory = 0;
       for (auto& f : fs::directory_iterator(p))
       {
-         std::string xtn = f.path().extension().generic_string();
+         std::string xtn = path_to_string(f.path().extension());
          if (filterOp(xtn))
          {
             Patch e;
             e.category = category;
             e.path = f.path();
-#if WINDOWS && ! TARGET_RACK
-              std::wstring str = f.path().filename().wstring();
-              str = str.substr(0, str.size() - xtn.length());
-              e.name = Surge::Storage::wstringToUTF8(str);
-#else
-              e.name = f.path().filename().generic_string();
-              e.name = e.name.substr(0, e.name.size() - xtn.length());
-#endif
-              items.push_back(e);
+            e.name = path_to_string(f.path().filename());
+            e.name = e.name.substr(0, e.name.size() - xtn.length());
+            items.push_back(e);
 
-              c.numberOfPatchesInCatgory ++;
+            c.numberOfPatchesInCatgory++;
          }
       }
 
@@ -933,7 +913,7 @@ void SurgeStorage::load_wt(int id, Wavetable* wt, OscillatorStorage *osc)
    if (!wt)
       return;
 
-   load_wt(wt_list[id].path.generic_string(), wt, osc);
+   load_wt(path_to_string(wt_list[id].path), wt, osc);
 
    if( osc )
    {
@@ -946,10 +926,7 @@ void SurgeStorage::load_wt(string filename, Wavetable* wt, OscillatorStorage *os
 {
    if( osc )
    {
-      char sep = '/';
-#if WINDOWS
-      sep = '\\';
-#endif
+      char sep = PATH_SEPARATOR;
       auto fn = filename.substr(filename.find_last_of(sep) + 1, filename.npos);
       std::string fnnoext = fn.substr( 0, fn.find_last_of('.' ) );
       
@@ -1673,7 +1650,7 @@ void SurgeStorage::rescanUserMidiMappings()
    if( fs::is_directory( fs::path( userMidiMappingsPath ) ) ) {
       for( auto &d : fs::directory_iterator( fs::path( userMidiMappingsPath ) ) )
       {
-         auto fn = d.path().generic_string();
+         auto fn = path_to_string(d.path());
          std::string ending = ".srgmid";
          if( fn.length() >= ending.length() && ( 0 == fn.compare( fn.length() - ending.length(), ending.length(), ending ) ) )
          {
@@ -1920,20 +1897,6 @@ string makeStringVertical(string& source)
    }
    return oss.str();
 }
-
-#if WINDOWS
-std::string wstringToUTF8(const std::wstring &str)
-{
-    std::string ret;
-    int len = WideCharToMultiByte(CP_UTF8, 0, str.c_str(), str.length(), NULL, 0, NULL, NULL);
-    if (len > 0)
-    {
-        ret.resize(len);
-        WideCharToMultiByte(CP_UTF8, 0, str.c_str(), str.length(), &ret[0], len, NULL, NULL);
-    }
-    return ret;
-}
-#endif    
 
 std::string appendDirectory( const std::string &root, const std::string &path1 )
 {

CMakeLists.txt
@@ -378,13 +378,10 @@ set(SURGE_GUI_INCLUDES
 
 ### OS SPECIFIC SECTION
 if( APPLE )
-  set(SURGE_OS_SOURCES
-    src/mac/UserInteractionsMac.mm
-    )
-
   set(SURGE_OS_GUI_SOURCES
     src/mac/DisplayInfoMac.mm
     src/mac/RuntimeFontMac.cpp
+    src/mac/UserInteractionsMac.mm
     src/mac/cocoa_utils.mm
     vstgui.surge/vstgui/vstgui_mac.mm
     vstgui.surge/vstgui/vstgui_uidescription_mac.mm
@@ -448,12 +445,6 @@ elseif( UNIX AND NOT APPLE )
     set( cxml src/linux/ConfigurationXml.S )
   endif()
 
-  set(SURGE_OS_SOURCES
-    src/linux/UserInteractionsLinux.cpp
-    ${cxml}
-    ${CMAKE_BINARY_DIR}/lintemp/ScalablePiggy.S
-    )
-
   file(GLOB LINUX_VSTGUI_GLOB vstgui.surge/vstgui/lib/platform/linux/*.cpp )
 
   # Our friends at steinberg have this maddening habig of using #warning DEPRECATED in linux code
@@ -462,13 +453,16 @@ elseif( UNIX AND NOT APPLE )
 
 
   set(SURGE_OS_GUI_SOURCES
+    ${CMAKE_BINARY_DIR}/lintemp/ScalablePiggy.S
+    ${LINUX_VSTGUI_GLOB}
+    ${cxml}
     src/linux/DisplayInfoLinux.cpp
     src/linux/RuntimeFontLinux.cpp
-    vstgui.surge/vstgui/vstgui.cpp
-    vstgui.surge/vstgui/vstgui_uidescription.cpp
+    src/linux/UserInteractionsLinux.cpp
     vstgui.surge/vstgui/lib/platform/common/genericoptionmenu.cpp
     vstgui.surge/vstgui/lib/platform/common/generictextedit.cpp
-    ${LINUX_VSTGUI_GLOB}
+    vstgui.surge/vstgui/vstgui.cpp
+    vstgui.surge/vstgui/vstgui_uidescription.cpp
     )
 
   find_package(PkgConfig REQUIRED)
@@ -563,12 +557,13 @@ elseif( UNIX AND NOT APPLE )
 elseif( WIN32 )
 
   set(SURGE_OS_SOURCES
-    src/windows/UserInteractionsWin.cpp
+    src/windows/surge_win_fopen_utf8.cpp
     )
 
   set(SURGE_OS_GUI_SOURCES
     src/windows/DisplayInfoWin.cpp
     src/windows/RuntimeFontWin.cpp
+    src/windows/UserInteractionsWin.cpp
     src/windows/surge.rc
     vstgui.surge/vstgui/vstgui_win32.cpp
     vstgui.surge/vstgui/vstgui_uidescription_win32.cpp
@@ -584,6 +579,8 @@ elseif( WIN32 )
     TIXML_USE_STL
     USE_LIBPNG
     _CRT_SECURE_NO_WARNINGS=1
+    UNICODE
+    _UNICODE
     ${SURGE_EXTRA_FILTERS_DEFINITIONS}
     )
 
@@ -981,6 +978,7 @@ endif()
 if( BUILD_HEADLESS )
   add_executable(surge-headless
     ${SURGE_COMMON_SOURCES}
+    ${SURGE_OS_SOURCES}
     ${SURGE_GENERATED_SOURCES}
     ${LIB_MIDIFILE_SOURCES}
     src/headless/main.cpp

libs/filesystem/filesystem.cpp
@@ -41,13 +41,13 @@ namespace std { namespace experimental { namespace filesystem {
         return p;
     }
     
-    path path::filename() {
+    path path::filename() const {
         auto idx = this->p.find_last_of("/");
         path p(this->p.substr(idx+1));
         return p;
     }
     
-    path path::extension() {
+    path path::extension() const {
         auto idx = this->p.find_last_of(".");
         if( idx == std::string::npos )
             return path( "" );
@@ -56,7 +56,7 @@ namespace std { namespace experimental { namespace filesystem {
         return res;
     }
 
-    path path::stem() {
+    path path::stem() const {
         auto idx = this->p.find_last_of("/");
         auto q = this->p.substr(idx+1);
         idx = q.find_last_of(".");

libs/filesystem/filesystem.h
@@ -37,15 +37,16 @@ namespace std { namespace experimental { namespace filesystem {
         operator std::string();
         
         void append(std::string s);
+        path& operator /=(const path& path) { append(path.p); return *this; }
         
         const char* c_str();
         
         std::string generic_string() const;
         
-        path filename();
-        path stem();
+        path filename() const;
+        path stem() const;
        
-        path extension();
+        path extension() const;
     };
     
     class file {

src/common/ImportFilesystem.h
@@ -23,3 +23,21 @@ namespace fs = std::experimental::filesystem;
 #else
 #error FILESYSTEM is not configured by build system
 #endif
+
+inline std::string path_to_string(const fs::path& path)
+{
+#if WINDOWS && !TARGET_RACK
+   return path.u8string();
+#else
+   return path.generic_string();
+#endif
+}
+
+inline fs::path string_to_path(const std::string& path)
+{
+#if WINDOWS && !TARGET_RACK
+   return fs::u8path(path);
+#else
+   return fs::path(path);
+#endif
+}

src/common/SurgeStorage.h
@@ -930,15 +930,6 @@ bool isValidUTF8( const std::string &testThis);
 std::string findReplaceSubstring(std::string &source, const std::string &from, const std::string &to);
 std::string makeStringVertical(std::string &source);
 
-#if WINDOWS
-/*
-** Windows filesystem names are properly wstrings which, if we want them to
-** display properly in vstgui, need to be converted to UTF8 using the
-** windows widechar API. Linux and Mac do not require this.
-*/
-std::string wstringToUTF8(const std::wstring &ws);
-#endif
-
 std::string appendDirectory( const std::string &root, const std::string &path1 );
 std::string appendDirectory( const std::string &root, const std::string &path1, const std::string &path2 );
 std::string appendDirectory( const std::string &root, const std::string &path1, const std::string &path2, const std::string &path3 );

src/common/SurgeSynthesizer.cpp
@@ -2502,8 +2502,8 @@ DWORD WINAPI loadPatchInBackgroundThread(LPVOID lpParam)
    }
    if( synth->has_patchid_file )
    {
-      fs::path p(synth->patchid_file);
-      auto s = p.stem().generic_string();
+      auto p(string_to_path(synth->patchid_file));
+      auto s = path_to_string(p.stem());
       synth->has_patchid_file = false;
       synth->allNotesOff();
       synth->loadPatchByPath( synth->patchid_file, -1, s.c_str() );

src/common/SurgeSynthesizerIO.cpp
@@ -155,7 +155,7 @@ void SurgeSynthesizer::loadPatch(int id)
    patchid = id;
 
    Patch e = storage.patch_list[id];
-   loadPatchByPath( e.path.generic_string().c_str(), e.category, e.name.c_str() );
+   loadPatchByPath(path_to_string(e.path).c_str(), e.category, e.name.c_str());
 }
 
 bool SurgeSynthesizer::loadPatchByPath( const char* fxpPath, int categoryId, const char* patchName )
@@ -347,8 +347,8 @@ void SurgeSynthesizer::savePatch()
    if (storage.getPatch().category.empty())
       storage.getPatch().category = "Default";
 
-   fs::path savepath = getUserPatchDirectory();
-   savepath.append(storage.getPatch().category);
+   fs::path savepath = string_to_path(getUserPatchDirectory());
+   savepath /= (string_to_path(storage.getPatch().category));
 
    create_directories(savepath);
 
@@ -376,7 +376,7 @@ void SurgeSynthesizer::savePatch()
    }
 
    fs::path filename = savepath;
-   filename.append(legalname + ".fxp");
+   filename /= string_to_path(legalname + ".fxp");
 
    if (fs::exists(filename))
    {

commit 6618850ab5f44809eb302df56762e04e2d39f792

macOS: Fix missing user patches regression
Broken by c8766229087617fbb333da445ffa5af6763cea02

src/common/SurgeStorage.cpp
@@ -375,7 +375,6 @@ bailOnPortable:
    }
 
    // append separator if not present
-   userDataPath = Surge::Storage::appendDirectory(userDataPath, std::string());
    datapath = Surge::Storage::appendDirectory(datapath, std::string());
 
    userFXPath = Surge::Storage::appendDirectory(userDataPath, "FXSettings");