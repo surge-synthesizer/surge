7df73b63845fb60987b037d9b191c14fc188f65e
Mario Kruselj <mario.kruselj@gmail.com>
2020-10-01 09:54:04 +0200

    LFO amplitude properly quantizes when Ctrl held

--- src/common/Parameter.cpp
+++ src/common/Parameter.cpp
@@ -1059,6 +1059,7 @@ void Parameter::bound_value(bool force_integer)
       case ct_osc_feedback:
       case ct_osc_feedback_negative:
       case ct_detuning:
+      case ct_lfoamplitude:
       {
          val.f = floor(val.f * 100) / 100.0;
          break;
