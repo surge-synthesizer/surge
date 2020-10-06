6e90a4f4c9bc34012ffceafa840fb71fde9c673d
Matthias von Faber <mvf@users.noreply.github.com>
2020-10-06 23:00:58 +0200

    CMake: Abolish OS_COMPILE_OPTIONS (#2788)
    This makes compile options cross-platform and globalizes them, making
    them apply to (future) CMake subdirectories.
    
    A few flags were changed:
    - The macOS warning settings are now effective for all Clang builds.
    - Suppressed some noisy Windows Clang warnings caused by headers.
    - Linux: -f{function,data}-sections was dropped. Didn't have much of
      an effect anyway without --gc-sections, and enabling that only
      saved about 10% in binary size, not worth the possible downsides.

--- cmake/arm-native.cmake
+++ cmake/arm-native.cmake
@@ -1,8 +1,10 @@
 set( LINUX_ON_ARM True )
-set(LINUX_ON_ARM_COMPILE_OPTIONS
+set(FLAGS
   -march=native
   -mcpu=native
   -Wno-psabi
   -flax-vector-conversions #FIXME - remove this
   )
-
+string(REPLACE ";" " " FLAGS "${FLAGS}")
+set(CMAKE_C_FLAGS_INIT "${FLAGS}" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS_INIT "${FLAGS}" CACHE STRING "" FORCE)
