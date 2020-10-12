500a3623e687afeec417c608c6e660cb6624ec1e
Paul Walker <paul@pwjw.com>
2020-10-11 20:30:04 -0400

    Cleanup old PNG references etc
    - Cleanup the old PNGs we dont need
    - Remove the unused files referencing them
    - Fix the emit-vector python script on windows and run it
    
    Closes #2706

--- src/common/resource.h
+++ src/common/resource.h
@@ -96,20 +96,6 @@
 #endif
 
 #if WINDOWS
-/*
-** Each of the bitmaps is also available in scaled form. The ID of the scaled bitmaps
-** is ID_BG+SCALABLE_200_OFFSET for the 200% image.
-**
-** Do NOT change the values of these constants without understanding scripts/win/emit-vector-rc.py
-**
-** This is windows only. Mac and Linux use different resource approaches
-*/
-#define SCALABLE_100_OFFSET 70000
-#define SCALABLE_125_OFFSET 71000
-#define SCALABLE_150_OFFSET 72000
-#define SCALABLE_200_OFFSET 73000
-#define SCALABLE_300_OFFSET 74000
-#define SCALABLE_400_OFFSET 75000
 #define SCALABLE_SVG_OFFSET 80000
 
 #include "scalableresource.h" // found in src/windows
