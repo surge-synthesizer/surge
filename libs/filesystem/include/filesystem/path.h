36967e08f474eb82f7b024195b1af439d053d05b
Matthias von Faber <mvf@users.noreply.github.com>
2020-10-22 23:37:40 +0200

    filesystem: Use fallback if library support is dodgy
    Some old versions of both GCC and Clang require linking additional libs
    for std::filesystem. Supporting these would require version checks, and
    even then there might still be setups that don't work.
    
    Luckily this is an issue only on POSIX, so let's just use the fallback
    filesystem, unless:
    
    - The compiler provides the <filesystem> header and
    - std::filesystem programs link without additional libraries
    
    This should fix it for just about any POSIX target out there.

--- libs/filesystem/include/filesystem/path.h
+++ libs/filesystem/include/filesystem/path.h
@@ -45,10 +45,7 @@ public:
    // native format observers                                                   [fs.path.native.obs]
    const string_type& native() const noexcept { return pth; }
    const value_type* c_str() const noexcept { return pth.c_str(); }
-#if __cplusplus < 201703L
-   // Pre-C++17 fstream compatibility. Otherwise disabled because this is unsafe on Windows.
    operator string_type() const { return pth; }
-#endif
    const std::string& string() const noexcept { return pth; }
    const std::string& u8string() const noexcept { return pth; }
 
