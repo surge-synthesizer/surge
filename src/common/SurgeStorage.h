surge-synthesizer/surge

commit cd2c84c15768f31c98a306142e03765ef48827bb

Remove CriticalSection, replace with std::mutex
Didn't seem to be used recursively. If it is after all, that's a bug
that needs fixing. If it can't be fixed right away, there's always
std::recursive_mutex to restore the old behavior.

src/common/SurgeStorage.h
@@ -19,8 +19,8 @@
 #include "ModulationSource.h"
 #include "Wavetable.h"
 #include <vector>
-#include <thread/CriticalSection.h>
 #include <memory>
+#include <mutex>
 #include <atomic>
 #include <stdint.h>
 
@@ -857,7 +857,7 @@ public:
    void storeMidiMappingToName( std::string name );
 
    // float table_sin[512],table_sin_offset[512];
-   Surge::CriticalSection CS_WaveTableData, CS_ModRouting;
+   std::mutex waveTableDataMutex, modRoutingMutex;
    Wavetable WindowWT;
 
    float note_to_pitch(float x);

CMakeLists.txt
@@ -209,7 +209,6 @@ set(SURGE_COMMON_SOURCES
   src/common/dsp/Wavetable.cpp
   src/common/dsp/WavetableOscillator.cpp
   src/common/dsp/WindowOscillator.cpp
-  src/common/thread/CriticalSection.cpp
   src/common/util/FpuState.cpp
   src/common/vt_dsp/basic_dsp.cpp
   src/common/vt_dsp/halfratefilter.cpp

src/common/SurgePatch.cpp
@@ -975,7 +975,7 @@ void SurgePatch::load_patch(const void* data, int datasize, bool preset)
 
                void* d = (void*)((char*)dr + sizeof(wt_header));
 
-               storage->CS_WaveTableData.enter();
+               storage->waveTableDataMutex.lock();
                scene[sc].osc[osc].wt.BuildWT(d, *wth, false);
                if( scene[sc].osc[osc].wavetable_display_name[0] == '\0' )
                {
@@ -988,7 +988,7 @@ void SurgePatch::load_patch(const void* data, int datasize, bool preset)
                      strncpy(scene[sc].osc[osc].wavetable_display_name, "(Patch Wavetable)", 256);
                   }
                }
-               storage->CS_WaveTableData.leave();
+               storage->waveTableDataMutex.unlock();
 
                dr += ph->wtsize[sc][osc];
             }

src/common/SurgeStorage.cpp
@@ -978,10 +978,10 @@ bool SurgeStorage::load_wt_wt(string filename, Wavetable* wt)
    data = malloc(ds);
    read = fread(data, 1, ds, f);
    // FIXME - error if read != ds
-   
-   CS_WaveTableData.enter();
+
+   waveTableDataMutex.lock();
    bool wasBuilt = wt->BuildWT(data, wh, false);
-   CS_WaveTableData.leave();
+   waveTableDataMutex.unlock();
    free(data);
 
    if (!wasBuilt)
@@ -1086,8 +1086,7 @@ void SurgeStorage::clipboard_copy(int type, int scene, int entry)
       return;
    }
 
-   // CS ENTER
-   CS_ModRouting.enter();
+   modRoutingMutex.lock();
    {
 
       clipboard_p.clear();
@@ -1138,8 +1137,7 @@ void SurgeStorage::clipboard_copy(int type, int scene, int entry)
          }
       }
    }
-   // CS LEAVE
-   CS_ModRouting.leave();
+   modRoutingMutex.unlock();
 }
 
 void SurgeStorage::clipboard_paste(int type, int scene, int entry)
@@ -1191,8 +1189,7 @@ void SurgeStorage::clipboard_paste(int type, int scene, int entry)
       return;
    }
 
-   // CS ENTER
-   CS_ModRouting.enter();
+   modRoutingMutex.lock();
    {
 
       for (int i = start; i < n; i++)
@@ -1274,8 +1271,8 @@ void SurgeStorage::clipboard_paste(int type, int scene, int entry)
       }
       }
    }
-   // CS LEAVE
-   CS_ModRouting.leave();
+
+   modRoutingMutex.unlock();
 }
 
 TiXmlElement* SurgeStorage::getSnapshotSection(const char* name)

src/common/SurgeSynthesizer.cpp
@@ -2087,7 +2087,7 @@ float SurgeSynthesizer::getModulation(long ptag, modsources modsource)
 
 void SurgeSynthesizer::clear_osc_modulation(int scene, int entry)
 {
-   storage.CS_ModRouting.enter();
+   storage.modRoutingMutex.lock();
    vector<ModulationRouting>::iterator iter;
 
    int pid = storage.getPatch().scene[scene].osc[entry].p[0].param_id_in_scene;
@@ -2111,7 +2111,7 @@ void SurgeSynthesizer::clear_osc_modulation(int scene, int entry)
       else
          iter++;
    }
-   storage.CS_ModRouting.leave();
+   storage.modRoutingMutex.unlock();
 }
 
 void SurgeSynthesizer::clearModulation(long ptag, modsources modsource, bool clearEvenIfInvalid)
@@ -2144,9 +2144,9 @@ void SurgeSynthesizer::clearModulation(long ptag, modsources modsource, bool cle
    {
       if ((modlist->at(i).destination_id == id) && (modlist->at(i).source_id == modsource))
       {
-         storage.CS_ModRouting.enter();
+         storage.modRoutingMutex.lock();
          modlist->erase(modlist->begin() + i);
-         storage.CS_ModRouting.leave();
+         storage.modRoutingMutex.unlock();
          return;
       }
    }
@@ -2173,7 +2173,7 @@ bool SurgeSynthesizer::setModulation(long ptag, modsources modsource, float val)
          modlist = &storage.getPatch().scene[scene - 1].modulation_voice;
    }
 
-   storage.CS_ModRouting.enter();
+   storage.modRoutingMutex.lock();
 
    int id = storage.getPatch().param_ptr[ptag]->param_id_in_scene;
    if (!scene)
@@ -2211,7 +2211,7 @@ bool SurgeSynthesizer::setModulation(long ptag, modsources modsource, float val)
          modlist->at(found_id).depth = value;
       }
    }
-   storage.CS_ModRouting.leave();
+   storage.modRoutingMutex.unlock();
 
    return true;
 }
@@ -2786,8 +2786,7 @@ void SurgeSynthesizer::process()
       clear_block_antidenormalnoise(fxsendout[1][1], BLOCK_SIZE_QUAD);
    }
 
-   // CS ENTER
-   storage.CS_ModRouting.enter();
+   storage.modRoutingMutex.lock();
    processControl();
 
    amp.set_target_smoothed(db_to_linear(storage.getPatch().volume.val.f));
@@ -2854,7 +2853,7 @@ void SurgeSynthesizer::process()
             iter++;
       }
 
-      storage.CS_ModRouting.leave();
+      storage.modRoutingMutex.unlock();
 
       fbq_global g;
       g.FU1ptr = GetQFPtrFilterUnit(storage.getPatch().scene[s].filterunit[0].type.val.i,
@@ -2895,11 +2894,10 @@ void SurgeSynthesizer::process()
          v->GetQFB(); // save filter state in voices after quad processing is done
          iter++;
       }
-      storage.CS_ModRouting.enter();
+      storage.modRoutingMutex.lock();
    }
 
-   // CS LEAVE
-   storage.CS_ModRouting.leave();
+   storage.modRoutingMutex.unlock();
    polydisplay = vcount;
 
    if (play_scene[0])
@@ -3136,7 +3134,7 @@ void SurgeSynthesizer::swapMetaControllers( int c1, int c2 )
    strncpy( storage.getPatch().CustomControllerLabel[c1], storage.getPatch().CustomControllerLabel[c2], 16 );
    strncpy( storage.getPatch().CustomControllerLabel[c2], nt, 16 );
 
-   storage.CS_ModRouting.enter();
+   storage.modRoutingMutex.lock();
 
    auto tmp1 = storage.getPatch().scene[0].modsources[ms_ctrl1 + c1];
    auto tmp2 = storage.getPatch().scene[0].modsources[ms_ctrl1 + c2];
@@ -3188,7 +3186,7 @@ void SurgeSynthesizer::swapMetaControllers( int c1, int c2 )
       }
    }
 
-   storage.CS_ModRouting.leave();
+   storage.modRoutingMutex.unlock();
 
    refresh_editor = true;
 }

src/common/WavSupport.cpp
@@ -463,11 +463,9 @@ void SurgeStorage::load_wt_wav_portable(std::string fn, Wavetable *wt)
 
     if( wavdata && wt )
     {
-        CS_WaveTableData.enter();
-        
+        waveTableDataMutex.lock();
         wt->BuildWT(wavdata, wh, wh.flags & wtf_is_sample);
-        
-        CS_WaveTableData.leave();
+        waveTableDataMutex.unlock();
         free( wavdata );
     }
     return;

src/common/gui/COscillatorDisplay.cpp
@@ -185,10 +185,10 @@ void COscillatorDisplay::draw(CDrawContext* dc)
             {
                if (uses_wavetabledata(oscdata->type.val.i))
                {
-                  storage->CS_WaveTableData.enter();
+                  storage->waveTableDataMutex.lock();
                   osc->process_block(disp_pitch_rs);
                   block_pos = 0;
-                  storage->CS_WaveTableData.leave();
+                  storage->waveTableDataMutex.unlock();
                }
                else
                {
@@ -304,7 +304,7 @@ void COscillatorDisplay::draw(CDrawContext* dc)
       rmenu.inset(14, 0);
       char wttxt[256];
 
-      storage->CS_WaveTableData.enter();
+      storage->waveTableDataMutex.lock();
 
       int wtid = oscdata->wt.current_id;
       if( oscdata->wavetable_display_name[0] != '\0' )
@@ -324,7 +324,7 @@ void COscillatorDisplay::draw(CDrawContext* dc)
          strcpy(wttxt, "(Patch Wavetable)");
       }
 
-      storage->CS_WaveTableData.leave();
+      storage->waveTableDataMutex.unlock();
 
       char* r = strrchr(wttxt, '.');
       if (r)

src/common/gui/SurgeGUIEditor.cpp
@@ -51,6 +51,10 @@
 #include <stack>
 #include <unordered_map>
 
+#if WINDOWS
+#include <windows.h>
+#endif
+
 #if TARGET_VST3
 #include "pluginterfaces/vst/ivstcontextmenu.h"
 #include "pluginterfaces/base/ustring.h"
@@ -246,8 +250,6 @@ SurgeGUIEditor::SurgeGUIEditor(void* effect, SurgeSynthesizer* synth, void* user
    _userdata = userdata;
    this->synth = synth;
 
-   // ToolTipWnd = 0;
-
    minimumZoom = 50;
 #if LINUX
    minimumZoom = 100; // See github issue #628
@@ -394,7 +396,7 @@ void SurgeGUIEditor::idle()
          int ptag = ((CControl*)v)->getTag() - start_paramtags;
          if (ptag >= 0)
          {
-            synth->storage.CS_ModRouting.enter();
+            synth->storage.modRoutingMutex.lock();
 
             for (int i = 1; i < n_modsources; i++)
             {
@@ -403,19 +405,19 @@ void SurgeGUIEditor::idle()
                      ->update_rt_vals(synth->isActiveModulation(ptag, (modsources)i), 0,
                                       synth->isModsourceUsed((modsources)i));
             }
-            synth->storage.CS_ModRouting.leave();
+            synth->storage.modRoutingMutex.unlock();
          }
       }
       else
       {
-         synth->storage.CS_ModRouting.enter();
+         synth->storage.modRoutingMutex.lock();
          for (int i = 1; i < n_modsources; i++)
          {
             if( gui_modsrc[i] )
                ((CModulationSourceButton*)gui_modsrc[i])
                   ->update_rt_vals(false, 0, synth->isModsourceUsed((modsources)i));
          }
-         synth->storage.CS_ModRouting.leave();
+         synth->storage.modRoutingMutex.unlock();
       }
 #if MAC
       idleinc++;
@@ -787,7 +789,7 @@ void SurgeGUIEditor::refresh_mod()
    if( cms->hasAlternate && cms->useAlternate )
       thisms = (modsources)cms->alternateId;
 
-   synth->storage.CS_ModRouting.enter();
+   synth->storage.modRoutingMutex.lock();
    for (int i = 0; i < n_paramslots; i++)
    {
       if (param[i])
@@ -817,7 +819,7 @@ void SurgeGUIEditor::refresh_mod()
    }
 #endif
 
-   synth->storage.CS_ModRouting.leave();
+   synth->storage.modRoutingMutex.unlock();
 
    for (int i = 1; i < n_modsources; i++)
    {

commit 9343b144080f9d4ebdb90c15005dd69a45192db4

SurgeStorage: Make modRoutingMutex a std::recursive_mutex for now
Turns out `SurgeStorage::modRoutingMutex` is used recursively after all:

> SurgeSynthesizer::clearModulation(long ptag, modsources modsource, bool clearEvenIfInvalid) 行 2140 C++
  SurgeSynthesizer::loadFx(bool initp, bool force_reload_all) 行 1752 C++
  SurgeSynthesizer::processControl() 行 2692 C++
> SurgeSynthesizer::process() 行 2803 C++
  SurgeVst3Processor::process(Steinberg::Vst::ProcessData & data) 行 503 C++

Broken by cd2c84c15768f31c98a306142e03765ef48827bb

src/common/SurgeStorage.h
@@ -922,7 +922,8 @@ public:
    void storeMidiMappingToName( std::string name );
 
    // float table_sin[512],table_sin_offset[512];
-   std::mutex waveTableDataMutex, modRoutingMutex;
+   std::mutex waveTableDataMutex;
+   std::recursive_mutex modRoutingMutex;
    Wavetable WindowWT;
 
    float note_to_pitch(float x);