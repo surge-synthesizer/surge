surge-synthesizer/surge

commit 54751ae99c47b60db78ffc41d66e3dd246e5d8d6

Add Skin Font Support on macOS (#3021)
Add the starts of Skin Font Support on macOS

1. Your skin can contain a fonts/ directory with ttf files
2. You can refer to that font family in your controls (currently only
   for CSurgeSlider)
3. And it all works

See the example 09.

Next is Win and Lin.

Addresses #2464

src/common/gui/SurgeGUIEditor.cpp
@@ -6664,6 +6664,12 @@ VSTGUI::CControl *SurgeGUIEditor::layoutComponentForSkin( std::shared_ptr<Surge:
       else
          hs->deactivated = false;
 
+      auto ff = currentSkin->propertyValue(skinCtrl, "font-family", "" );
+      if( ff.size() > 0 )
+      {
+         hs->setFont(new CFontDesc(ff.c_str(), 9 ));
+      }
+
       if (p->valtype == vt_int || p->valtype == vt_bool)
       {
          hs->isStepped = true;

CMakeLists.txt
@@ -276,7 +276,7 @@ set(SURGE_GUI_SOURCES
   src/common/gui/SurgeGUIEditorHtmlGenerators.cpp
   src/common/gui/UIInstrumentation.cpp
   src/common/gui/guihelpers.cpp
-        src/common/gui/CVerticalLabel.cpp src/common/gui/CursorControlGuard.h src/common/gui/CursorControlGuard.cpp)
+        src/common/gui/CVerticalLabel.cpp src/common/gui/CursorControlGuard.h src/common/gui/CursorControlGuard.cpp src/common/gui/SkinFontLoader.cpp)
 
 set(SURGE_VST3_SOURCES
   src/vst3/SurgeVst3Processor.cpp

resources/data/skins/Tutorial/09 Adding Fonts.surge-skin/skin.xml
@@ -1,0 +1,25 @@
+<!--[doc]
+Adding Fonts is under active development. Currently it only works on a limited set of controls and only
+on macOS. We will update this as that changes.
+
+Controls take a font-family name as an argument generally. (As of this writing, only sliders do!). That
+font-family sets the font family and so if there is a widely used font, like, say, comic-sans, you can choose
+that and the system font resolution will resolve the family name.
+
+If you want to add custom fonts and have appropriate license to distribute the ttf files, you can create a
+subdirectory of your skin called "fonts" which contains ttf files, and Surge will make those families available to
+you. This skin demonstrates that.
+
+This is clearly a work in progress and the design and specifications be updated as we approach 1.8.0. Please don't use
+it yet unless you are working with the dev team.
+[doc]-->
+<surge-skin name="09 Adding Fonts" category="Tutorial" author="Surge Synth Team" authorURL="https://surge-synth-team.org/" version="1">
+  <globals>
+  </globals>
+  <component-classes>
+  </component-classes>
+  <controls>
+    <control ui_identifier="osc.param_1" font-family="UglyTypist"/>
+    <control ui_identifier="osc.param_2" font-family="Mrs Beasley"/>
+  </controls>
+</surge-skin>

src/common/gui/CSurgeSlider.cpp
@@ -55,6 +55,9 @@ CSurgeSlider::CSurgeSlider(const CPoint& loc,
    
    this->storage = storage;
 
+   labfont = displayFont;
+   labfont->remember();
+
    modmode = 0;
    disabled = false;
    deactivated = false;
@@ -124,7 +127,9 @@ void CSurgeSlider::setModValue(float val)
 }
 
 CSurgeSlider::~CSurgeSlider()
-{}
+{
+   labfont->forget();
+}
 
 void CSurgeSlider::setLabel(const char* txt)
 {
@@ -238,7 +243,7 @@ void CSurgeSlider::draw(CDrawContext* dc)
          dc->setFontColor(skin->getColor(Colors::Slider::Label::Light));
       else
          dc->setFontColor(skin->getColor(Colors::Slider::Label::Dark));
-      dc->setFont(displayFont);
+      dc->setFont(labfont);
 
       //		int a = 'a' + (rand()&31);
       //		label[1] = a;
@@ -523,7 +528,7 @@ void CSurgeSlider::draw(CDrawContext* dc)
             }
             else
             {
-               dc->setFont(displayFont);
+               dc->setFont(labfont);
                dc->setFontColor(CColor(80,80,100));
                auto newRect = hrect;
                newRect.top += 1;
@@ -563,7 +568,7 @@ void CSurgeSlider::draw(CDrawContext* dc)
                newRect.bottom = newRect.top + 20;
                newRect.right = newRect.left + 16;
                
-               dc->setFont(displayFont);
+               dc->setFont(labfont);
                dc->setFontColor(CColor(80,80,100));
                
                auto tRect = newRect;

src/common/gui/CSurgeSlider.h
@@ -92,6 +92,13 @@ public:
    
    void SetQuantitizedDispValue(float f);
 
+   void setFont( VSTGUI::CFontRef f ) {
+      if( labfont ) labfont->forget();
+      labfont = f;
+      labfont->remember();
+   }
+   VSTGUI::CFontRef labfont = nullptr;
+
    CLASS_METHODS(CSurgeSlider, CControl)
 
    bool in_hover = false;

src/common/gui/SkinFontLoader.cpp
@@ -1,0 +1,45 @@
+/*
+** Surge Synthesizer is Free and Open Source Software
+**
+** Surge is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html
+**
+** Copyright 2004-2020 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** Surge was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Surge
+** open source in September 2018.
+*/
+
+#include "SkinSupport.h"
+#include <iostream>
+#if MAC
+#include <CoreFoundation/CoreFoundation.h>
+#include <CoreText/CoreText.h>
+#endif
+
+void Surge::UI::addFontSearchPathToSystem(const fs::path& p)
+{
+#if MAC
+   // Mac is traverse for TTF files and add to the path
+   for(  const fs::path& ent : fs::directory_iterator(p))
+   {
+      if( ent.extension().native() == ".ttf" )
+      {
+         CFURLRef url = nullptr;
+         auto path = path_to_string(ent);
+         url = CFURLCreateFromFileSystemRepresentation(NULL, (const unsigned char*)path.c_str(),
+                                                       path.size(), false);
+         if (url)
+         {
+            CTFontManagerRegisterFontsForURL(url, kCTFontManagerScopeProcess, NULL);
+            CFRelease(url);
+         }
+      }
+   }
+#else
+   std::cerr << "Unsupported font search path addition" << std::endl;
+#endif
+}

commit 035f484a72abe60eb76c9faf570d245346604c45

Temporary fix for crash introduced with #3021 on Win/Linux

src/common/gui/SurgeGUIEditor.cpp
@@ -6664,11 +6664,14 @@ VSTGUI::CControl *SurgeGUIEditor::layoutComponentForSkin( std::shared_ptr<Surge:
       else
          hs->deactivated = false;
 
+      // we had crashes on Win/Lin because of this, so until mvf does font loading on those two platforms, compile-time if this bugger!
+#if MAC
       auto ff = currentSkin->propertyValue(skinCtrl, "font-family", "" );
       if( ff.size() > 0 )
       {
          hs->setFont(new CFontDesc(ff.c_str(), 9 ));
       }
+#endif
 
       if (p->valtype == vt_int || p->valtype == vt_bool)
       {