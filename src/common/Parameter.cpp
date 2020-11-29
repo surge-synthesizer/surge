51c3f668ecd035da2ea9ce0b5c71fe4365409ef0
EvilDragon <mario.kruselj@gmail.com>
2020-11-30 00:49:47 +0100

    Fix filter subtype menus not able to select above 6th subtype (#3294)

--- src/common/Parameter.cpp
+++ src/common/Parameter.cpp
@@ -646,7 +646,7 @@ void Parameter::set_type(int ctrltype)
    case ct_filtersubtype:
       valtype = vt_int;
       val_min.i = 0;
-      val_max.i = 5;
+      val_max.i = n_max_filter_subtypes - 1;
       val_default.i = 0;
       break;
    case ct_wstype:
