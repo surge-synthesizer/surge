ce0d2d99e9d62d317cb737ed5006af02bf1f714c
Paul Walker <paul@pwjw.com>
2020-12-04 08:00:08 -0500

    Add a Hover-color Text Button
    Add a TextButton class with Hover states in place.
    Use it for the 'copy' button in CAboutBox
    
    Closes #3318

---
+++ src/common/gui/CTextButtonWithHover.h
@@ -0,0 +1,61 @@
+/*
+** Surge Synthesizer is Free and Open Source Software
+**
+** Surge is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html
+**
+** Copyright 2004-2020 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** Surge was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Surge
+** open source in September 2018.
+*/
+
+#pragma once
+#include <vstgui/vstgui.h>
+
+class CTextButtonWithHover : public VSTGUI::CTextButton
+{
+public:
+
+   template< typename T >
+   struct  Optional {
+      T item = T();
+      bool isSet = false;
+   };
+
+   CTextButtonWithHover(const VSTGUI::CRect &r, VSTGUI::IControlListener *l, int32_t tag, VSTGUI::UTF8String lab )
+       : CTextButton( r, l, tag, lab ) {}
+   VSTGUI::CMouseEventResult onMouseEntered(VSTGUI::CPoint& where,
+                                            const VSTGUI::CButtonState& buttons) override;
+   void draw(VSTGUI::CDrawContext* context) override;
+   VSTGUI::CMouseEventResult onMouseExited(VSTGUI::CPoint& where,
+                                           const VSTGUI::CButtonState& buttons) override;
+
+   /*
+    * Macros are still the answer sometimes.
+    */
+#define ADD_HOVER(m, T) \
+   Optional<T> hc_ ## m ;                 \
+   void setHover ## m ( T c ) { hc_ ## m.item = c; hc_ ## m.isSet = true; }
+
+#define ADD_HOVER_CR(m, T) \
+   Optional<T> hc_ ## m ;                 \
+   void setHover ## m ( const T &c ) { hc_ ## m.item = c; hc_ ## m.isSet = true; }
+
+   ADD_HOVER( Gradient, VSTGUI::CGradient * );
+   ADD_HOVER_CR( FrameColor, VSTGUI::CColor);
+   ADD_HOVER_CR( TextColor, VSTGUI::CColor );
+   ADD_HOVER( GradientHighlighted, VSTGUI::CGradient * );
+   ADD_HOVER_CR( FrameColorHighlighted, VSTGUI::CColor );
+   ADD_HOVER_CR( TextColorHighlighted, VSTGUI::CColor );
+
+#undef ADD_HOVER
+
+private:
+   bool isHovered = false;
+   CLASS_METHODS(CTextButtonWithHover,VSTGUI::CTextButton);
+};
+
