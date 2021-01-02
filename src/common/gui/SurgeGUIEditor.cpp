46a86dc5b14aed73969b1ef683681241cb99830a
Mario Kruselj <mario.kruselj@gmail.com>
2021-01-02 11:32:14 +0100

    Only show valid modulators in Add modulation from... menu Closes #3564

--- src/common/gui/SurgeGUIEditor.cpp
+++ src/common/gui/SurgeGUIEditor.cpp
@@ -3393,7 +3393,7 @@ int32_t SurgeGUIEditor::controlModifierClicked(CControl* control, CButtonState b
                for (int k = 1; k < n_modsources; k++)
                {
                   modsources ms = (modsources)modsource_display_order[k];
-                  if (!synth->isActiveModulation(ptag, (modsources)ms))
+                  if (!synth->isActiveModulation(ptag, (modsources)ms) && synth->isValidModulation(ptag, (modsources)ms))
                   {
                      char tmptxt[512];
                      sprintf(tmptxt, "%s", modulatorName(ms, false).c_str());
