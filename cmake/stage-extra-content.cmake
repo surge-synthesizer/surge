3317bda535b809090970e13b3b4c415ea283b7ad
Paul Walker <paul@pwjw.com>
2021-01-09 13:46:46 -0500

    Pipeline for Extra Content part 1: Surge-side
    This commit adds a couple of pipeline rules to download,
    stage extra content. Also it adds a pipeline-checks target
    so we can trigger a special VM just to do code quality checks
    in the PR pipeline
    
    Addresses #3517

---
+++ cmake/stage-extra-content.cmake
@@ -0,0 +1,31 @@
+#
+# This file is used to provide targets which stage the extra content external repo
+#
+
+set(SURGE_EXTRA_CONTENT_REPO https://github.com/surge-synthesizer/surge-extra-content.git)
+set(SURGE_EXTRA_CONTENT_HASH 59aafa2af2c02eeb7bdcbede4cae216a22e12c74)
+
+find_package(Git)
+if( ${Git_FOUND} )
+    message(STATUS "Adding download-extra-content target from ${SURGE_EXTRA_CONTENT_REPO}" )
+    add_custom_target(download-extra-content)
+    add_custom_command(TARGET download-extra-content
+            POST_BUILD
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+            COMMAND cmake -E make_directory surge-extra-content
+            COMMAND ${GIT_EXECUTABLE} clone ${SURGE_EXTRA_CONTENT_REPO} surge-extra-content
+            )
+    add_custom_command(TARGET download-extra-content
+            POST_BUILD
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/surge-extra-content
+            COMMAND ${GIT_EXECUTABLE} checkout ${SURGE_EXTRA_CONTENT_HASH}
+            )
+
+    # I explicitly do NOT make this depend on download since download twice in a row fails
+    add_custom_target(stage-extra-content)
+    add_custom_command(TARGET stage-extra-content
+            POST_BUILD
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            COMMAND cmake -E copy_directory ${CMAKE_BINARY_DIR}/surge-extra-content/Skins/ resources/data/skins
+            )
+endif()
