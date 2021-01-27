caf0076c4083e049d6da4a1e49595ac81d2a8803
Paul <baconpaul@users.noreply.github.com>
2021-01-27 18:28:07 -0500

    SSE inside the Resonator effect (#3734)
    This isn't finished and is not on in config.xml, but this is
    a complete implementation of how to use an SSE filter in the Resonator
    Effect which @mkruselj can use to finish this effect.

--- src/common/dsp/effect/ResonatorEffect.cpp
+++ src/common/dsp/effect/ResonatorEffect.cpp
@@ -14,15 +14,22 @@
 */
 
 #include "ResonatorEffect.h"
+#include "DebugHelpers.h"
 
 ResonatorEffect::ResonatorEffect(SurgeStorage *storage, FxStorage *fxdata, pdata *pd)
-    : Effect(storage, fxdata, pd)
+    : Effect(storage, fxdata, pd), halfbandIN(6, true), halfbandOUT(6, true)
 {
     gain.set_blocksize(BLOCK_SIZE);
     mix.set_blocksize(BLOCK_SIZE);
+
+    qfus = (QuadFilterUnitState *)_aligned_malloc(2 * sizeof(QuadFilterUnitState), 16);
+    for (int e = 0; e < 3; ++e)
+        for (int c = 0; c < 2; ++c)
+            for (int q = 0; q < n_filter_registers; ++q)
+                Reg[e][c][q] = 0;
 }
 
-ResonatorEffect::~ResonatorEffect() {}
+ResonatorEffect::~ResonatorEffect() { _aligned_free(qfus); }
 
 void ResonatorEffect::init()
 {
@@ -39,11 +46,18 @@ void ResonatorEffect::setvars(bool init)
 
         gain.instantize();
         mix.instantize();
+
+        halfbandOUT.reset();
+        halfbandIN.reset();
     }
     else
     {
     }
 }
+
+inline void set1f(__m128 &m, int i, float f) { *((float *)&m + i) = f; }
+
+inline float get1f(__m128 m, int i) { return *((float *)&m + i); }
 
 void ResonatorEffect::process(float *dataL, float *dataR)
 {
@@ -51,9 +65,133 @@ void ResonatorEffect::process(float *dataL, float *dataR)
         setvars(false);
     bi = (bi + 1) & slowrate_m1;
 
-    copy_block(dataL, L, BLOCK_SIZE_QUAD);
-    copy_block(dataR, R, BLOCK_SIZE_QUAD);
+    /*
+     * Strategy: Upsample, Set the coefficients, run the filter, Downsample
+     */
 
+    // Upsample phase. This wqorks and needs no more attention.
+    float dataOS alignas(16)[2][BLOCK_SIZE_OS];
+    halfbandIN.process_block_U2(dataL, dataR, dataOS[0], dataOS[1]);
+
+    /*
+     * Select the coefficients. Here you have to base yourself on the mode switch and
+     * call CoeffMaker with one fo the known models and subtypes, along with the resonance
+     * and the frequency of the particular band.
+     */
+    auto whichModel = *pdata_ival[resonator_mode];
+
+    int type = 0, subtype = 0;
+    switch (whichModel)
+    {
+    case 0:
+        type = fut_lp12;
+        subtype = 0;
+        // You need to adjust this switch statement
+        break;
+    default:
+        type = fut_none;
+        subtype = 0;
+        break;
+    }
+    FilterUnitQFPtr filtptr = GetQFPtrFilterUnit(type, subtype);
+
+    /*
+     * So now set up across the voices (e for 'entry' to match SurgeVoice) and the channels (c)
+     */
+    for (int e = 0; e < 3; ++e)
+    {
+        float frequencyInPitch = *f[resonator_freq1 + e * 3];
+        float resonance = *f[resonator_res1 + e * 3];
+        for (int c = 0; c < 2; ++c)
+        {
+            coeff[e][c].MakeCoeffs(frequencyInPitch, resonance, type, subtype, storage);
+
+            for (int i = 0; i < n_cm_coeffs; i++)
+            {
+                set1f(qfus[c].C[i], e, coeff[e][c].C[i]);
+                set1f(qfus[c].dC[i], e, coeff[e][c].dC[i]);
+            }
+
+            for (int i = 0; i < n_filter_registers; i++)
+            {
+                set1f(qfus[c].R[i], e, Reg[e][c][i]);
+            }
+
+            qfus[c].DB[e] = filterDelay[e][c];
+            qfus[c].WP[e] = WP[e][c];
+            qfus[c].WP[0] = subtype;
+
+            qfus[c].active[e] = 0xFFFFFFFF;
+            qfus[c].active[3] = 0;
+        }
+    }
+
+    /*
+     * FIXME - smooth this
+     */
+    float bankGain[3];
+    for (int i = 0; i < 3; ++i)
+    {
+        bankGain[i] = *f[resonator_gain1 + i * 3]; // probably some units wrk here
+    }
+
+    /* Run the filters. You need to grab a smoothed gain here rather than the hardcoded 0.3 */
+    for (int s = 0; s < BLOCK_SIZE_OS; ++s)
+    {
+        auto l128 = _mm_setzero_ps();
+        auto r128 = _mm_setzero_ps();
+
+        if (filtptr)
+        {
+            l128 = filtptr(&(qfus[0]), _mm_set1_ps(dataOS[0][s]));
+            r128 = filtptr(&(qfus[1]), _mm_set1_ps(dataOS[1][s]));
+        }
+        else
+        {
+            l128 = _mm_set1_ps(dataOS[0][s]);
+            r128 = _mm_set1_ps(dataOS[1][s]);
+        }
+
+        float tl[4], tr[4];
+        _mm_store_ps(tl, l128);
+        _mm_store_ps(tr, r128);
+        float mixl = 0, mixr = 0;
+        for (int i = 0; i < 3; ++i)
+        {
+            mixl += tl[i] * bankGain[i];
+            mixr += tr[i] * bankGain[i];
+        }
+
+        dataOS[0][s] = mixl;
+        dataOS[1][s] = mixr;
+    }
+
+    /* and preserve those registers and stuff. This all works */
+    for (int c = 0; c < 2; ++c)
+    {
+        for (int i = 0; i < n_filter_registers; i++)
+        {
+            for (int e = 0; e < 3; ++e)
+            {
+                Reg[e][c][i] = get1f(qfus[c].R[i], e);
+                coeff[e][c].C[i] = get1f(qfus[c].C[i], e);
+            }
+        }
+    }
+    for (int e = 0; e < 3; ++e)
+    {
+        for (int c = 0; c < 2; ++c)
+        {
+            WP[e][c] = qfus[c].WP[e];
+        }
+    }
+
+    /* Downsample out */
+    halfbandOUT.process_block_D2(dataOS[0], dataOS[1]);
+    copy_block(dataOS[0], L, BLOCK_SIZE_QUAD);
+    copy_block(dataOS[1], R, BLOCK_SIZE_QUAD);
+
+    // And gain and mix
     gain.set_target_smoothed(db_to_linear(*f[resonator_gain]));
     gain.multiply_2_blocks(L, R, BLOCK_SIZE_QUAD);
 
