f57ad63a1322510f002408c1a5215d4b7c3b15d3
Mario Kruselj <mario.kruselj@gmail.com>
2021-01-31 00:28:11 +0100

    Various CHOW tweaks Always oversample, remove slider Add Flip and Mix parameters Add ctype for Ratio parameter and adjust the range

--- src/common/dsp/effect/chowdsp/Chow.cpp
+++ src/common/dsp/effect/chowdsp/Chow.cpp
@@ -21,6 +21,7 @@ namespace chowdsp
 Chow::Chow(SurgeStorage *storage, FxStorage *fxdata, pdata *pd) : Effect(storage, fxdata, pd)
 {
     makeup.set_blocksize(BLOCK_SIZE);
+    mix.set_blocksize(BLOCK_SIZE);
 }
 
 Chow::~Chow() {}
@@ -29,18 +30,27 @@ void Chow::init()
 {
     os.reset();
     makeup.set_target(1.0f);
+    mix.set_target(1.0f);
+
     thresh_smooth.reset(SmoothSteps);
     ratio_smooth.reset(SmoothSteps);
+
+    mix.instantize();
 }
 
-static inline float chowProcess(float x, const float threshGain, const float ratio)
+static inline float chowProcess(float x, const float threshGain, const float ratio, const bool flip)
 {
     float y = x;
 
-    if (x > threshGain)
+    if (! flip && x > threshGain)
     {
         y = threshGain;
         y += (x - threshGain) / ratio;
+    }
+    else if (flip && x < -threshGain)
+    {
+        y = -threshGain;
+        y += (x + threshGain) / ratio;
     }
 
     return y;
@@ -50,26 +60,36 @@ void Chow::process(float *dataL, float *dataR)
 {
     set_params();
 
+    for (int i = 0; i < BLOCK_SIZE; i++)
+    {
+        L[i] = dataL[i];
+        R[i] = dataR[i];
+    }
+
     if (cur_os)
         process_block_os(dataL, dataR);
     else
         process_block(dataL, dataR);
 
     makeup.multiply_2_blocks(dataL, dataR, BLOCK_SIZE_QUAD);
+
+    mix.set_target_smoothed(limit_range(*f[chow_mix], 0.f, 1.f));
+    mix.fade_2_blocks_to(L, dataL, R, dataR, dataL, dataR, BLOCK_SIZE_QUAD);
 }
 
 void Chow::set_params()
 {
     const auto threshGain = db_to_linear(*f[chow_thresh]);
-    const auto ratio = 9.0f * std::pow(*f[chow_ratio] / -48.f, 2.584f) +
-                       1.0f; // hand-tuned from several years ago...
+    
+    // hand-tuned from several years ago...
+    const auto ratio = limit_range(*f[chow_ratio],
+                                   fxdata->p[chow_ratio].val_min.f,
+                                   fxdata->p[chow_ratio].val_max.f);
     auto makeup_gain = db_to_linear((*f[chow_thresh] / 12.f) * ((1.0f / ratio) - 1.0f) - 1.0f);
 
-    const auto oversample = static_cast<bool>(*pdata_ival[chow_os]);
-    makeup_gain *= oversample ? 4.0f : 1.0f;
+    makeup_gain *= cur_os ? 4.0f : 1.0f;
     makeup.set_target_smoothed(makeup_gain);
 
-    cur_os = oversample;
     thresh_smooth.setTargetValue(threshGain);
     ratio_smooth.setTargetValue(ratio);
 }
@@ -80,9 +100,10 @@ void Chow::process_block(float *dataL, float *dataR)
     {
         float cur_thresh = thresh_smooth.getNextValue();
         float cur_ratio = ratio_smooth.getNextValue();
+        bool cur_flip = fxdata->p[chow_flip].val.b;
 
-        dataL[k] = chowProcess(dataL[k], cur_thresh, cur_ratio);
-        dataR[k] = chowProcess(dataR[k], cur_thresh, cur_ratio);
+        dataL[k] = chowProcess(dataL[k], cur_thresh, cur_ratio, cur_flip);
+        dataR[k] = chowProcess(dataR[k], cur_thresh, cur_ratio, cur_flip);
     }
 }
 
@@ -91,16 +112,19 @@ void Chow::process_block_os(float *dataL, float *dataR)
     os.upsample(dataL, dataR);
 
     float cur_thresh, cur_ratio;
+    bool cur_flip;
+
     for (int k = 0; k < os.getUpBlockSize(); k++)
     {
         if (k % os.getOSRatio() == 0)
         {
             cur_thresh = thresh_smooth.getNextValue();
             cur_ratio = ratio_smooth.getNextValue();
+            cur_flip = fxdata->p[chow_flip].val.b;
         }
 
-        os.leftUp[k] = chowProcess(os.leftUp[k], cur_thresh, cur_ratio);
-        os.rightUp[k] = chowProcess(os.rightUp[k], cur_thresh, cur_ratio);
+        os.leftUp[k] = chowProcess(os.leftUp[k], cur_thresh, cur_ratio, cur_flip);
+        os.rightUp[k] = chowProcess(os.rightUp[k], cur_thresh, cur_ratio, cur_flip);
     }
 
     os.downsample(dataL, dataR);
@@ -117,19 +141,25 @@ void Chow::init_ctrltypes()
     fxdata->p[chow_thresh].posy_offset = 1;
 
     fxdata->p[chow_ratio].set_name("Ratio");
-    fxdata->p[chow_ratio].set_type(ct_decibel_attenuation);
+    fxdata->p[chow_ratio].set_type(ct_chow_ratio);
     fxdata->p[chow_ratio].posy_offset = 1;
 
-    fxdata->p[chow_os].set_name("Oversampling");
-    fxdata->p[chow_os].set_type(ct_bool);
-    fxdata->p[chow_os].posy_offset = 1;
+    fxdata->p[chow_flip].set_name("Flip");
+    fxdata->p[chow_flip].set_type(ct_bool);
+    fxdata->p[chow_flip].posy_offset = 1;
+
+    fxdata->p[chow_mix].set_name("Mix");
+    fxdata->p[chow_mix].set_type(ct_percent);
+    fxdata->p[chow_mix].posy_offset = 3;
+    fxdata->p[chow_mix].val_default.f = 1.f;
 }
 
 void Chow::init_default_values()
 {
-    fxdata->p[chow_thresh].val.f = -27.f;
-    fxdata->p[chow_ratio].val.f = -10.f;
-    fxdata->p[chow_os].val.f = 0.f;
+    fxdata->p[chow_thresh].val.f = -24.f;
+    fxdata->p[chow_ratio].val.f = 10.f;
+    
+    fxdata->p[chow_mix].val.f = 1.f;
 }
 
 const char *Chow::group_label(int id)
@@ -137,7 +167,9 @@ const char *Chow::group_label(int id)
     switch (id)
     {
     case 0:
-        return "CHOW";
+        return "Truculate";
+    case 1:
+        return "Output";
     }
 
     return 0;
@@ -149,6 +181,8 @@ int Chow::group_label_ypos(int id)
     {
     case 0:
         return 1;
+    case 1:
+        return 9;
     }
     return 0;
 }
