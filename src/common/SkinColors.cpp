e79c5943da00f1991dac4df479edf349e9ef56ce
EvilDragon <mario.kruselj@gmail.com>
2021-02-08 21:51:04 +0100

    Bipolar macro slider drawing tweak, orange current value color (#3852)

--- src/common/SkinColors.cpp
+++ src/common/SkinColors.cpp
@@ -211,7 +211,7 @@ namespace Macro
 {
 const Surge::Skin::Color Background("modbutton.macro.slider.background", 32, 93, 176),
     Fill("modbutton.macro.slider.fill", 46, 134, 254),
-    CurrentValue("modbutton.macro.slider.currentvalue", 144, 216, 255);
+    CurrentValue("modbutton.macro.slider.currentvalue", 255, 144, 0);
 
 }
 } // namespace ModSource
