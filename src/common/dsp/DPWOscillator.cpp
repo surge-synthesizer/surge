c21e0ff83fb84e3099b29d977c245ab7851d047d
Paul Walker <paul@pwjw.com>
2021-02-12 14:05:49 -0500

    DPW - A Deriv Polynomial Waveform Oscillator
    This is an implementation of a second derivative of a polynomial
    cubit to create saw, triangle, and pulse waves with unison.
    
    Addresses #3910

---
+++ src/common/dsp/DPWOscillator.cpp
@@ -0,0 +1,166 @@
+/*
+** Surge Synthesizer is Free and Open Source Software
+**
+** Surge is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html
+**
+** Copyright 2004-2021 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** Surge was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Surge
+** open source in September 2018.
+*/
+
+#include "DPWOscillator.h"
+#include "DebugHelpers.h"
+
+void DPWOscillator::process_block(float pitch, float drift, bool stereo, bool FM, float FMdepth)
+{
+    float ud = localcopy[oscdata->p[dpw_unison_detune].param_id_in_scene].f;
+    pitchlag.newValue(pitch);
+    for (int u = 0; u < n_unison; ++u)
+    {
+        dpbase[u].newValue(std::min(0.5, pitch_to_dphase(pitchlag.v + ud * unisonOffsets[u])));
+    }
+    sawmix.newValue(localcopy[oscdata->p[dpw_sawmix].param_id_in_scene].f);
+    trimix.newValue(localcopy[oscdata->p[dpw_trimix].param_id_in_scene].f);
+    sqrmix.newValue(localcopy[oscdata->p[dpw_sqrmix].param_id_in_scene].f);
+    pwidth.newValue(limit_range(1.f - localcopy[oscdata->p[dpw_pulse_width].param_id_in_scene].f,
+                                0.01f, 0.99f));
+
+    pitchlag.process();
+
+    for (int i = 0; i < BLOCK_SIZE_OS; ++i)
+    {
+        double vL = 0.0, vR = 0.0;
+        for (int u = 0; u < n_unison; ++u)
+        {
+            auto dp = dpbase[u].v;
+
+            for (int qx = sigbuf_len - 1; qx > 0; --qx)
+            {
+                sawBuffer[u][qx] = sawBuffer[u][qx - 1];
+                triBuffer[u][qx] = triBuffer[u][qx - 1];
+                sawOffBuffer[u][qx] = sawOffBuffer[u][qx - 1];
+            }
+
+            // Saw Component (p^3-p)/6
+            double p = (phase[u] - 0.5) * 2;
+            double p3 = p * p * p;
+            double sawcub = (p3 - p) / 6.0;
+            sawBuffer[u][0] = sawcub;
+            double saw =
+                (sawBuffer[u][0] + sawBuffer[u][2] - 2.0 * sawBuffer[u][1]) / (4.0 * dp * dp);
+
+            double tp = p + 0.5;
+            if (tp > 1.0)
+                tp -= 2;
+            double Q = tp < 0 ? -1 : 1;
+            double tricub = (2.0 * Q * tp * tp * tp - 3.0 * tp * tp - 2.0) / 6.0;
+            triBuffer[u][0] = tricub;
+            double tri =
+                (triBuffer[u][0] + triBuffer[u][2] - 2.0 * triBuffer[u][1]) / (4.0 * dp * dp);
+
+            double pwp = (phase[u] - 0.5 + pwidth.v) * 2;
+            if (pwp > 1)
+                pwp -= 2;
+            sawcub = (pwp * pwp * pwp - pwp) / 6.0;
+            sawOffBuffer[u][0] = sawcub;
+            double sawoff = (sawOffBuffer[u][0] + sawOffBuffer[u][2] - 2.0 * sawOffBuffer[u][1]) /
+                            (4.0 * dp * dp);
+            double sqr = sawoff - saw + (1 - pwidth.v * 2);
+            // sawBuffer[u][0] = sawmix.v * sawcub + trimix.v * tricub;
+
+            // double res = (sawBuffer[u][0] + sawBuffer[u][2] - 2.0 * sawBuffer[u][1]) /
+            //             (4.0 * dp * dp);
+
+            // Super important - you have to mix after differentiating to avoid
+            // zipper noise
+            double res = sawmix.v * saw + trimix.v * tri + sqrmix.v * sqr;
+            vL += res * mixL[u];
+            vR += res * mixR[u];
+            phase[u] += dp;
+
+            while (phase[u] > 1)
+                phase[u] -= 1;
+
+            dpbase[u].process();
+        }
+
+        output[i] = vL;
+        outputR[i] = vR;
+
+        sawmix.process();
+        trimix.process();
+        sqrmix.process();
+        pwidth.process();
+    }
+    if (starting)
+    {
+        for (int i = 0; i < 2; ++i)
+        {
+            output[i] = output[2];
+            outputR[i] = outputR[2];
+        }
+        starting = false;
+    }
+}
+
+void DPWOscillator::init_ctrltypes()
+{
+    oscdata->p[dpw_sawmix].set_name("Saw Mix");
+    oscdata->p[dpw_sawmix].set_type(ct_percent);
+
+    oscdata->p[dpw_trimix].set_name("Tri Mix");
+    oscdata->p[dpw_trimix].set_type(ct_percent);
+
+    oscdata->p[dpw_sqrmix].set_name("Square Mix");
+    oscdata->p[dpw_sqrmix].set_type(ct_percent);
+
+    oscdata->p[dpw_pulse_width].set_name("Pulse Width");
+    oscdata->p[dpw_pulse_width].set_type(ct_percent);
+
+    oscdata->p[dpw_sync].set_name("Currently Unimplemented");
+    oscdata->p[dpw_sync].set_type(ct_syncpitch);
+
+    oscdata->p[dpw_unison_detune].set_name("Unison Detune");
+    oscdata->p[dpw_unison_detune].set_type(ct_oscspread);
+    oscdata->p[dpw_unison_voices].set_name("Unison Voices");
+    oscdata->p[dpw_unison_voices].set_type(ct_osccount);
+}
+
+void DPWOscillator::init_default_values()
+{
+    oscdata->p[dpw_sawmix].val.f = 0.7;
+    oscdata->p[dpw_trimix].val.f = 0.0;
+    oscdata->p[dpw_sqrmix].val.f = 0.0;
+    oscdata->p[dpw_pulse_width].val.f = 0.5;
+    oscdata->p[dpw_sync].val.f = 0.0;
+    oscdata->p[dpw_unison_detune].val.f = 0.2;
+    oscdata->p[dpw_unison_voices].val.i = 1;
+}
+void DPWOscillator::init(float pitch, bool is_display)
+{
+    // We need a tiny little portamento since the derivative is pretty
+    // unstable under super big pitch changes
+    pitchlag.setRate(0.5);
+    pitchlag.startValue(pitch);
+
+    n_unison = is_display ? 1 : oscdata->p[dpw_unison_voices].val.i;
+    if (n_unison > 1)
+    {
+        for (int u = 0; u < n_unison; ++u)
+        {
+            float mx = 1.f * u / (n_unison - 1); // between 0 and 1
+            unisonOffsets[u] = (mx - 0.5);
+            mixL[u] = 1.0 - mx;
+            mixR[u] = mx;
+        }
+    }
+    else
+    {
+        unisonOffsets[0] = 0;
+    }
+}
---
+++ src/common/dsp/DPWOscillator.h
@@ -0,0 +1,71 @@
+/*
+** Surge Synthesizer is Free and Open Source Software
+**
+** Surge is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html
+**
+** Copyright 2004-2021 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** Surge was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Surge
+** open source in September 2018.
+*/
+
+#ifndef SURGE_DPWOSCILLATOR_H
+#define SURGE_DPWOSCILLATOR_H
+
+#include "OscillatorBase.h"
+#include "DspUtilities.h"
+
+class DPWOscillator : public Oscillator
+{
+  public:
+    enum dpw_params
+    {
+        dpw_sawmix = 0,
+        dpw_sqrmix,
+        dpw_pulse_width,
+        dpw_trimix,
+        dpw_sync,
+        dpw_unison_detune,
+        dpw_unison_voices,
+    };
+
+    static constexpr int sigbuf_len = 3;
+    DPWOscillator(SurgeStorage *s, OscillatorStorage *o, pdata *p) : Oscillator(s, o, p)
+    {
+        for (auto u = 0; u < MAX_UNISON; ++u)
+        {
+            phase[u] = 0;
+            for (int p = 0; p < sigbuf_len; ++p)
+            {
+                sawBuffer[u][p] = 0.f;
+                triBuffer[u][p] = 0.f;
+                sawOffBuffer[u][p] = 0.f;
+            }
+            mixL[u] = 1.f;
+            mixR[u] = 1.f;
+        }
+    }
+
+    virtual void init(float pitch, bool is_display = false);
+    virtual void init_ctrltypes(int scene, int oscnum) { init_ctrltypes(); };
+    virtual void init_ctrltypes();
+    virtual void init_default_values();
+    virtual void process_block(float pitch, float drift = 0.f, bool stereo = false, bool FM = false,
+                               float FMdepth = 0.f);
+
+    lag<double, true> sawmix, trimix, sqrmix, pwidth, dpbase[MAX_UNISON], detune, pitchlag;
+    int n_unison = 1;
+    bool starting = true;
+    double phase[MAX_UNISON];
+    double unisonOffsets[MAX_UNISON];
+    double mixL[MAX_UNISON], mixR[MAX_UNISON];
+
+    double sawBuffer[MAX_UNISON][sigbuf_len];
+    double triBuffer[MAX_UNISON][sigbuf_len];
+    double sawOffBuffer[MAX_UNISON][sigbuf_len];
+};
+#endif // SURGE_DPWOSCILLATOR_H
