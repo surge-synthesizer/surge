f1b7061f45d9c31efa833878ad9157b27759b15e
Paul <baconpaul@users.noreply.github.com>
2021-02-28 19:34:23 -0500

    Add DISTRHO option to juce access (#4004)

--- cmake/get-juce.cmake
+++ cmake/get-juce.cmake
@@ -1,7 +1,13 @@
-set( JUCE_LIB_VERSION 6.0.7 )  # If you mod this, change it in scripts/misc/defensive-submodule.sh also:wq
 
-set( JUCE_REPO git://github.com/juce-framework/JUCE )
-set( JUCE_LIB_DIR ${CMAKE_SOURCE_DIR}/libs/juce-${JUCE_LIB_VERSION}/juce )
+if( BUILD_SURGE_WITH_DISTRHO_JUCE )
+	set( JUCE_LIB_VERSION main )  # If you mod this, change it in scripts/misc/defensive-submodule.sh also:wq
+	set( JUCE_REPO git://github.com/DISTRHO/JUCE )
+	set( JUCE_LIB_DIR ${CMAKE_SOURCE_DIR}/libs/distrho-juce-${JUCE_LIB_VERSION}/juce )
+else()
+	set( JUCE_LIB_VERSION 6.0.7 )  # If you mod this, change it in scripts/misc/defensive-submodule.sh also:wq
+	set( JUCE_REPO git://github.com/juce-framework/JUCE )
+	set( JUCE_LIB_DIR ${CMAKE_SOURCE_DIR}/libs/juce-${JUCE_LIB_VERSION}/juce )
+endif()
 
 find_package(Git)
 
