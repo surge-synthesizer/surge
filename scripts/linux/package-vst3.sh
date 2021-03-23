55885869580cbebbeb351cfb1d05e8d9a0d78f6d
Paul Walker (baconpaul) <paul@pwjw.com>
2021-03-23 08:53:35 -0400

    Linux CMake Lists
    Don't strip the DLL in debug builds.
    Revert to Release as a default

--- scripts/linux/package-vst3.sh
+++ scripts/linux/package-vst3.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 OUTPUT_DIR="$2"
+NOSTRIP="$3"
 ARCH=`uname -m`
 
 BUNDLE_NAME="Surge.vst3"
@@ -17,4 +18,11 @@ fi
 VST_SO_DIR="$BUNDLE_DIR/Contents/${ARCH}-linux"
 mkdir -p "$VST_SO_DIR"
 cp $1 "$VST_SO_DIR"/Surge.so
-strip -s "$VST_SO_DIR"/Surge.so
+if [[ -z ${NOSTRIP} ]]; 
+then
+  echo "  Stripping ${VST_SO_DIR}/Surge.so"
+  strip -s "$VST_SO_DIR"/Surge.so
+else
+  echo "  Skipping strip phase for debug build"
+fi
+
