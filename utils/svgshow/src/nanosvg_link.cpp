a93159ecc0f2cca3a18e4a0f81d353bf1289f6d6
Paul Walker <paul@pwjw.com>
2021-04-18 15:59:05 -0400

    More CMake Cleanup
    1. Binary target. Addresses #2071 but we still need to do binary
       load of windows.wt
    2. Remove svgshow
    3. ASIO handling improved
    4. Remove the package-vst3 and so on scripts
    
    Addresses #4300

--- utils/svgshow/src/nanosvg_link.cpp
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#define NANOSVG_IMPLEMENTATION	// Expands implementation
-#include "nanosvg.h"
--- utils/svgshow/src/win32minimal.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <windows.h>
-#include <functional>
-#include "vstgui/lib/vstguifwd.h"
-
-int win32minimal_main( HINSTANCE hInstance,
-		       int w, int h, int nCmdShow,
-		       std::function<void(VSTGUI::CFrame *f)> frameCb);
