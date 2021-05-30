a71f49a05522187e0779c7d5d1addaca35aa6f3a
Paul <baconpaul@users.noreply.github.com>
2021-05-30 18:35:22 -0400

    CEffectSettings -> Surge::Widgets::EffectChooser (#4619)
    The EffectSettings gets a refresh/rewrite

---
+++ src/gui/widgets/EffectChooser.cpp
@@ -0,0 +1,288 @@
+/*
+** Surge Synthesizer is Free and Open Source Software
+**
+** Surge is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html
+**
+** Copyright 2004-2021 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** Surge was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Surge
+** open source in September 2018.
+*/
+
+#include "EffectChooser.h"
+#include "RuntimeFont.h"
+#include "SurgeGUIEditor.h"
+
+namespace Surge
+{
+namespace Widgets
+{
+EffectChooser::EffectChooser()
+{
+    setRepaintsOnMouseActivity(true);
+    for (int i = 0; i < n_fx_slots; ++i)
+        fxTypes[i] = fxt_off;
+};
+EffectChooser::~EffectChooser() = default;
+
+void EffectChooser::paint(juce::Graphics &g)
+{
+    if (skin->getVersion() < 2)
+    {
+        // FIXME implement this
+        jassert(false);
+        g.fillAll(juce::Colours::red);
+        g.setColour(juce::Colours::white);
+        g.drawText("Can't do V1 yet", getLocalBounds(), juce::Justification::centred);
+        return;
+    }
+    if (bg)
+        bg->draw(g, 1.0);
+
+    g.setFont(Surge::GUI::getFontManager()->getLatoAtSize(7));
+
+    for (int i = 0; i < n_scenes; ++i)
+    {
+        auto r = getSceneRectangle(i);
+        g.setColour(skin->getColor(Colors::Effect::Grid::Scene::Background));
+        g.fillRect(r);
+
+        g.setColour(skin->getColor(Colors::Effect::Grid::Scene::Border));
+        g.drawRect(r);
+
+        g.setColour(skin->getColor(Colors::Effect::Grid::Scene::Text));
+        g.drawText(scenename[i], r, juce::Justification::centred);
+    }
+
+    // FX slots
+    juce::Colour bgd, frm, txt;
+
+    for (int i = 0; i < n_fx_slots; i++)
+    {
+        auto r = getEffectRectangle(i);
+        getColorsForSlot(i, bgd, frm, txt);
+
+        g.setColour(bgd);
+        g.fillRect(r);
+        g.setColour(frm);
+        g.drawRect(r);
+        g.setColour(txt);
+        g.drawText(fx_type_shortnames[fxTypes[i]], r, juce::Justification::centred);
+    }
+
+    if (hasDragged && currentClicked >= 0)
+    {
+        auto r = getEffectRectangle(currentClicked)
+                     .translated(dragX, dragY)
+                     .constrainedWithin(getLocalBounds());
+        getColorsForSlot(currentClicked, bgd, frm, txt);
+
+        g.setColour(bgd);
+        g.fillRect(r);
+        g.setColour(frm);
+        g.drawRect(r);
+        g.setColour(txt);
+        g.drawText(fx_type_shortnames[fxTypes[currentClicked]], r, juce::Justification::centred);
+    }
+}
+
+juce::Rectangle<int> EffectChooser::getSceneRectangle(int i)
+{
+    const int scenelabelbox[n_scenes][2] = {{1, 1}, {1, 41}};
+    const int scenelabelboxWidth = 11, scenelabelboxHeight = 11;
+
+    auto r = juce::Rectangle<int>(scenelabelbox[i][0], scenelabelbox[i][1], scenelabelboxWidth,
+                                  scenelabelboxHeight);
+
+    return r;
+}
+
+juce::Rectangle<int> EffectChooser::getEffectRectangle(int i)
+{
+    const int fxslotWidth = 19, fxslotHeight = 11;
+    const int fxslotpos[n_fx_slots][2] = {{18, 1},  {44, 1},  {18, 41}, {44, 41},
+                                          {18, 21}, {44, 21}, {89, 11}, {89, 31}};
+
+    auto r = juce::Rectangle<int>(fxslotpos[i][0], fxslotpos[i][1], fxslotWidth, fxslotHeight);
+
+    return r;
+}
+
+void EffectChooser::mouseDown(const juce::MouseEvent &event)
+{
+    hasDragged = false;
+    currentClicked = -1;
+    for (int i = 0; i < n_fx_slots; ++i)
+    {
+        auto r = getEffectRectangle(i);
+        if (r.contains(event.getPosition()))
+        {
+            currentClicked = i;
+        }
+    }
+
+    for (int i = 0; i < n_scenes; ++i)
+    {
+        auto r = getSceneRectangle(i);
+        if (r.contains(event.getPosition()))
+        {
+            auto sge = firstListenerOfType<SurgeGUIEditor>();
+            if (sge)
+            {
+                sge->effectSettingsBackgroundClick(i);
+            }
+        }
+    }
+}
+
+void EffectChooser::mouseUp(const juce::MouseEvent &event)
+{
+    if (!hasDragged && currentClicked >= 0)
+    {
+        if (event.mods.isRightButtonDown())
+        {
+            deactivatedBitmask ^= (1 << currentClicked);
+        }
+        else
+        {
+            currentEffect = currentClicked;
+        }
+        notifyValueChanged();
+    }
+
+    if (hasDragged)
+    {
+        for (int i = 0; i < n_fx_slots; ++i)
+        {
+            auto r = getEffectRectangle(i);
+            if (r.contains(event.getPosition()))
+            {
+                auto m = SurgeSynthesizer::FXReorderMode::SWAP;
+                if (event.mods.isCtrlDown())
+                {
+                    m = SurgeSynthesizer::FXReorderMode::COPY;
+                }
+                if (event.mods.isShiftDown())
+                {
+                    m = SurgeSynthesizer::FXReorderMode::MOVE;
+                }
+                auto sge = firstListenerOfType<SurgeGUIEditor>();
+                if (sge)
+                {
+                    sge->swapFX(currentClicked, i, m);
+                    currentEffect = i;
+                    notifyValueChanged();
+                }
+            }
+        }
+        hasDragged = false;
+        repaint();
+    }
+}
+
+void EffectChooser::mouseDrag(const juce::MouseEvent &event)
+{
+    if (event.getDistanceFromDragStart() > 0)
+        hasDragged = true;
+
+    dragX = event.getDistanceFromDragStartX();
+    dragY = event.getDistanceFromDragStartY();
+    repaint();
+}
+
+void EffectChooser::mouseMove(const juce::MouseEvent &event)
+{
+    int nextHover = -1;
+    for (int i = 0; i < n_fx_slots; ++i)
+    {
+        auto r = getEffectRectangle(i);
+        if (r.contains(event.getPosition()))
+        {
+            nextHover = i;
+        }
+    }
+    if (nextHover != currentHover)
+    {
+        currentHover = nextHover;
+        repaint();
+    }
+}
+
+void EffectChooser::getColorsForSlot(int fxslot, juce::Colour &bgcol, juce::Colour &frcol,
+                                     juce::Colour &txtcol)
+{
+    // This is lunacy but hey.
+    bool byp = isBypassedOrDeactivated(fxslot);
+    if (fxslot == currentEffect)
+    {
+        if (byp)
+        {
+            if (isHovered && fxslot == currentHover)
+            {
+                bgcol = skin->getColor(Colors::Effect::Grid::BypassedSelected::BackgroundHover);
+                frcol = skin->getColor(Colors::Effect::Grid::BypassedSelected::BorderHover);
+                txtcol = skin->getColor(Colors::Effect::Grid::BypassedSelected::TextHover);
+            }
+            else
+            {
+                bgcol = skin->getColor(Colors::Effect::Grid::BypassedSelected::Background);
+                frcol = skin->getColor(Colors::Effect::Grid::BypassedSelected::Border);
+                txtcol = skin->getColor(Colors::Effect::Grid::BypassedSelected::Text);
+            }
+        }
+        else
+        {
+            if (isHovered && fxslot == currentHover)
+            {
+                bgcol = skin->getColor(Colors::Effect::Grid::Selected::BackgroundHover);
+                frcol = skin->getColor(Colors::Effect::Grid::Selected::BorderHover);
+                txtcol = skin->getColor(Colors::Effect::Grid::Selected::TextHover);
+            }
+            else
+            {
+                bgcol = skin->getColor(Colors::Effect::Grid::Selected::Background);
+                frcol = skin->getColor(Colors::Effect::Grid::Selected::Border);
+                txtcol = skin->getColor(Colors::Effect::Grid::Selected::Text);
+            }
+        }
+    }
+    else
+    {
+        if (byp)
+        {
+            if (isHovered && fxslot == currentHover)
+            {
+                bgcol = skin->getColor(Colors::Effect::Grid::Bypassed::BackgroundHover);
+                frcol = skin->getColor(Colors::Effect::Grid::Bypassed::BorderHover);
+                txtcol = skin->getColor(Colors::Effect::Grid::Bypassed::TextHover);
+            }
+            else
+            {
+                bgcol = skin->getColor(Colors::Effect::Grid::Bypassed::Background);
+                frcol = skin->getColor(Colors::Effect::Grid::Bypassed::Border);
+                txtcol = skin->getColor(Colors::Effect::Grid::Bypassed::Text);
+            }
+        }
+        else
+        {
+            if (isHovered && fxslot == currentHover)
+            {
+                bgcol = skin->getColor(Colors::Effect::Grid::Unselected::BackgroundHover);
+                frcol = skin->getColor(Colors::Effect::Grid::Unselected::BorderHover);
+                txtcol = skin->getColor(Colors::Effect::Grid::Unselected::TextHover);
+            }
+            else
+            {
+                bgcol = skin->getColor(Colors::Effect::Grid::Unselected::Background);
+                frcol = skin->getColor(Colors::Effect::Grid::Unselected::Border);
+                txtcol = skin->getColor(Colors::Effect::Grid::Unselected::Text);
+            }
+        }
+    }
+}
+} // namespace Widgets
+} // namespace Surge
---
+++ src/gui/widgets/EffectChooser.h
@@ -0,0 +1,114 @@
+/*
+** Surge Synthesizer is Free and Open Source Software
+**
+** Surge is made available under the Gnu General Public License, v3.0
+** https://www.gnu.org/licenses/gpl-3.0.en.html
+**
+** Copyright 2004-2021 by various individuals as described by the Git transaction log
+**
+** All source at: https://github.com/surge-synthesizer/surge.git
+**
+** Surge was a commercial product from 2004-2018, with Copyright and ownership
+** in that period held by Claes Johanson at Vember Audio. Claes made Surge
+** open source in September 2018.
+*/
+
+#ifndef SURGE_XT_EFFECTCHOOSER_H
+#define SURGE_XT_EFFECTCHOOSER_H
+
+#include <JuceHeader.h>
+#include <array>
+#include "SurgeStorage.h"
+#include "efvg/escape_from_vstgui.h"
+#include "SkinSupport.h"
+#include "WidgetBaseMixin.h"
+#include "SurgeJUCEHelpers.h"
+
+namespace Surge
+{
+namespace Widgets
+{
+struct EffectChooser : public juce::Component, public WidgetBaseMixin<EffectChooser>
+{
+    EffectChooser();
+    ~EffectChooser();
+    void paint(juce::Graphics &g) override;
+
+    /*
+     * These aren't used but are part of the interface. The callback casts.
+     */
+    float getValue() const override
+    {
+        jassert(false);
+        return value;
+    }
+    void setValue(float f) override
+    {
+        jassert(false);
+        value = f;
+    }
+    void valueChanged() override { jassert(false); }
+    float value{0.f};
+
+    void setCurrentEffect(int c) { currentEffect = c; }
+    int getCurrentEffect() const { return currentEffect; }
+    int currentEffect{0};
+
+    void setEffectType(int index, int type) { fxTypes[index] = type; }
+    std::array<int, n_fx_slots> fxTypes;
+
+    void mouseDown(const juce::MouseEvent &event) override;
+    void mouseUp(const juce::MouseEvent &event) override;
+    void mouseDrag(const juce::MouseEvent &event) override;
+    void mouseMove(const juce::MouseEvent &event) override;
+    void mouseEnter(const juce::MouseEvent &event) override
+    {
+        isHovered = true;
+        mouseMove(event);
+    }
+    void mouseExit(const juce::MouseEvent &event) override { isHovered = false; }
+    // State for mouse events
+    bool isHovered{false}, hasDragged{false};
+    int currentHover{-1}, currentClicked{-1};
+    int dragX, dragY;
+
+    void setBypass(int b) { bypassState = b; }
+    int bypassState{0};
+    void setDeactivatedBitmask(int d) { deactivatedBitmask = d; };
+    int getDeactivatedBitmask() const { return deactivatedBitmask; }
+    int deactivatedBitmask{0};
+
+    juce::Drawable *bg{nullptr};
+    void setBackgroundDrawable(juce::Drawable *b) { bg = b; }
+
+    juce::Rectangle<int> getSceneRectangle(int sceneNo);
+    juce::Rectangle<int> getEffectRectangle(int fx);
+    const char *scenename[n_scenes] = {"A", "B"};
+
+    bool isBypassedOrDeactivated(int fxslot)
+    {
+        if (deactivatedBitmask & (1 << fxslot))
+            return true;
+        switch (bypassState)
+        {
+        case fxb_no_fx:
+            return true;
+        case fxb_no_sends:
+            if ((fxslot == 4) || (fxslot == 5))
+                return true;
+            break;
+        case fxb_scene_fx_only:
+            if (fxslot > 3)
+                return true;
+            break;
+        }
+        return false;
+    }
+
+    void getColorsForSlot(int fxslot, juce::Colour &bgcol, juce::Colour &frcol,
+                          juce::Colour &txtcol);
+};
+} // namespace Widgets
+} // namespace Surge
+
+#endif // SURGE_XT_EFFECTCHOOSER_H
