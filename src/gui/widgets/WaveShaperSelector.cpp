19bb4e0b16e6eda75519e1e2b9ff7dc33e7ad300
Paul <baconpaul@users.noreply.github.com>
2021-07-25 14:55:53 -0400

    WaveShaper response overlay (#4770)
    Writing waveshapers you kinda want to see what they do. This
    lets you pop a little overlay that does it. Just like the rest
    of the widget, still basically unskinned.
    
    Addresses #1964

--- src/gui/widgets/WaveShaperSelector.cpp
+++ src/gui/widgets/WaveShaperSelector.cpp
@@ -5,12 +5,166 @@
 #include "WaveShaperSelector.h"
 #include "RuntimeFont.h"
 #include "QuadFilterUnit.h"
+#include "DSPUtils.h"
 
 namespace Surge
 {
 namespace Widgets
 {
+
+struct WaveShaperAnalysisWidget : public juce::Component
+{
+    WaveShaperAnalysisWidget()
+    {
+        if (ampLevs[0] == 0)
+        {
+            for (int i = 0; i < n_db_levs; ++i)
+            {
+                ampLevs[i] =
+                    powf(2.f, dbLevs[i] / 18.f); // db_to_amp(dbLevs[i]); db_to_amp is limited. Why?
+            }
+        }
+    }
+    void paint(juce::Graphics &g) override
+    {
+        g.fillAll(juce::Colour(10, 10, 20));
+        g.setColour(juce::Colours::lightgrey);
+        g.drawRect(getLocalBounds());
+
+        // Draw original last
+        for (int c = n_db_levs; c >= 0; --c)
+        {
+            juce::Path p;
+
+            for (int i = 0; i < npts; ++i)
+            {
+                if (i == 0)
+                {
+                    p.startNewSubPath(responseCurves[wstype][c][i].first,
+                                      responseCurves[wstype][c][i].second);
+                }
+                else
+                {
+                    p.lineTo(responseCurves[wstype][c][i].first,
+                             responseCurves[wstype][c][i].second);
+                }
+            }
+
+            // OK so this path is in x=0,1 y=-1,1
+            auto xf =
+                juce::AffineTransform()
+                    .translated(0, -1.0)
+                    .scaled(1, -0.5)
+                    .scaled(getWidth(), getHeight())
+                    .translated(2, 2)
+                    .scaled((getWidth() - 48.0) / getWidth(), (getHeight() - 4.0) / getHeight());
+
+            auto re = juce::Rectangle<float>{0, -1, 1, 2}.transformedBy(xf);
+
+            if (c == n_db_levs)
+            {
+                g.setColour(juce::Colour(10, 10, 20));
+                g.fillRect(re);
+                g.setColour(juce::Colours::lightgrey);
+                g.drawRect(re);
+            }
+
+            if (c == 0)
+                g.setColour(juce::Colours::white);
+            else
+            {
+                g.setColour(juce::Colour(0xFF * (c + 11) / 20, 0x90 * (c + 11) / 20, 0));
+            }
+            {
+                juce::Graphics::ScopedSaveState gs(g);
+                g.reduceClipRegion(re.toNearestIntEdges());
+                g.strokePath(p, juce::PathStrokeType(1.0), xf);
+            }
+
+            auto xpos = re.getRight() + 3;
+            auto ypos = re.getY() + (c + 1) * 12;
+            g.setFont(Surge::GUI::getFontManager()->getLatoAtSize(9));
+            auto tx = juce::Rectangle<float>{xpos, ypos, 50, 12};
+            std::ostringstream oss;
+            if (c == 0)
+                oss << "Signal";
+            else
+                oss << dbLevs[c - 1] << "dB";
+            g.drawText(oss.str().c_str(), tx, juce::Justification::centredLeft);
+            if (c == 0)
+            {
+                tx = tx.translated(0, -12);
+                g.drawText(wst_ui_names[wstype], tx, juce::Justification::centredLeft);
+            }
+        }
+    }
+    void setWST(ws_type w)
+    {
+        wstype = w;
+        if (responseCurves[wstype][0].empty())
+        {
+            // so the stragety is make a 128 point curve with a 2 sin oscillations in [0,1].
+            // We can use the SSE on drive to populate over two calls
+            float dx = 1.f / (npts - 1);
+
+            auto d1 = _mm_loadu_ps(ampLevs.data());
+            auto d2 = _mm_loadu_ps(ampLevs.data() + 4);
+
+            // FIXME - get this when we deal with init
+            QuadFilterWaveshaperState ws1, ws2;
+            for (int i = 0; i < n_waveshaper_registers; ++i)
+            {
+                ws1.R[i] = _mm_setzero_ps();
+                ws2.R[i] = _mm_setzero_ps();
+            }
+
+            auto wsop = GetQFPtrWaveshaper(wstype);
+
+            for (int i = 0; i < npts; i++)
+            {
+                float x = i * dx;
+                float inval = std::sin(x * 4.0 * M_PI);
+                auto ivs = _mm_set1_ps(inval);
+                auto ov1 = ivs;
+                auto ov2 = ivs;
+                if (wsop)
+                {
+                    ov1 = wsop(&ws1, ivs, d1);
+                    ov2 = wsop(&ws1, ivs, d2);
+                }
+                float r alignas(16)[8];
+                _mm_store_ps(r, ov1);
+                _mm_store_ps(r + 4, ov2);
+
+                responseCurves[wstype][0].emplace_back(x, inval);
+                for (int j = 0; j < n_db_levs; ++j)
+                {
+                    responseCurves[wstype][j + 1].emplace_back(x, r[j]);
+                }
+            }
+        }
+    }
+    ws_type wstype{wst_none};
+
+    static constexpr int n_db_levs = 8, npts = 128;
+    static std::array<float, n_db_levs> ampLevs, dbLevs;
+
+    typedef std::vector<std::pair<float, float>> curve_t;
+    static std::array<std::array<curve_t, n_db_levs + 1>, n_ws_types> responseCurves;
+};
+
+std::array<float, WaveShaperAnalysisWidget::n_db_levs> WaveShaperAnalysisWidget::dbLevs{
+    -24, -12, 0, 6, 8, 12, 18, 24};
+std::array<float, WaveShaperAnalysisWidget::n_db_levs> WaveShaperAnalysisWidget::ampLevs{
+    0, 0, 0, 0, 0, 0, 0, 0};
 std::array<std::vector<std::pair<float, float>>, n_ws_types> WaveShaperSelector::wsCurves;
+std::array<std::array<WaveShaperAnalysisWidget::curve_t, WaveShaperAnalysisWidget::n_db_levs + 1>,
+           n_ws_types>
+    WaveShaperAnalysisWidget::responseCurves;
+
+WaveShaperSelector::WaveShaperSelector() {}
+
+WaveShaperSelector::~WaveShaperSelector() { closeAnalysis(); }
 
 void WaveShaperSelector::paint(juce::Graphics &g)
 {
@@ -72,6 +226,13 @@ void WaveShaperSelector::paint(juce::Graphics &g)
     g.setColour(juce::Colours::lightgrey);
     g.drawRect(buttonP);
 
+    g.setColour(juce::Colours::darkgrey);
+    g.fillRect(buttonA);
+    g.setColour(juce::Colours::white);
+    g.drawText(analysisWidget ? "x" : "?", buttonA, juce::Justification::centred);
+    g.setColour(juce::Colours::lightgrey);
+    g.drawRect(buttonA);
+
     // So the wave is in -2,2 in x and -1,1 in y
     juce::Path curvePath;
     bool f = true;
@@ -105,9 +266,23 @@ void WaveShaperSelector::paint(juce::Graphics &g)
 void WaveShaperSelector::resized()
 {
     auto b = getLocalBounds().withY(getHeight() - 12).withHeight(12);
-    buttonM = b.withTrimmedRight(getWidth() / 2);
-    buttonP = b.withTrimmedLeft(getWidth() / 2);
+    buttonM = b.withTrimmedRight(2.f * getWidth() / 3);
+    buttonP = b.withTrimmedLeft(2.f * getWidth() / 3);
+    buttonA = b.withTrimmedRight(getWidth() / 3.f).withTrimmedLeft(getWidth() / 3.f);
     waveArea = getLocalBounds().withTrimmedTop(12).withTrimmedBottom(12);
+}
+
+void WaveShaperSelector::setValue(float f)
+{
+    value = f;
+    iValue = Parameter::intUnscaledFromFloat(value, n_ws_types - 1, 0);
+    repaint();
+
+    if (analysisWidget)
+    {
+        analysisWidget->setWST((ws_type)iValue);
+        analysisWidget->repaint();
+    }
 }
 
 void WaveShaperSelector::mouseDown(const juce::MouseEvent &event)
@@ -132,6 +307,10 @@ void WaveShaperSelector::mouseDown(const juce::MouseEvent &event)
         setValue(nextValueInOrder(value, +1));
         notifyValueChanged();
         notifyEndEdit();
+    }
+    if (buttonA.toFloat().contains(event.position))
+    {
+        toggleAnalysis();
     }
 }
 
@@ -243,5 +422,38 @@ float WaveShaperSelector::nextValueInOrder(float v, int inc)
 
     return r;
 }
+void WaveShaperSelector::parentHierarchyChanged() { closeAnalysis(); }
+
+void WaveShaperSelector::toggleAnalysis()
+{
+    if (analysisWidget)
+        closeAnalysis();
+    else
+        openAnalysis();
+}
+
+void WaveShaperSelector::closeAnalysis()
+{
+    if (!analysisWidget)
+        return;
+    if (getParentComponent() &&
+        getParentComponent()->getIndexOfChildComponent(analysisWidget.get()) >= 0)
+        getParentComponent()->removeChildComponent(analysisWidget.get());
+    analysisWidget.reset(nullptr);
+    repaint();
+}
+
+void WaveShaperSelector::openAnalysis()
+{
+    if (analysisWidget)
+        return;
+    analysisWidget = std::make_unique<WaveShaperAnalysisWidget>();
+    analysisWidget->setWST((ws_type)iValue);
+    auto b = getBoundsInParent().translated(-250, 0).withWidth(250).withHeight(125);
+    analysisWidget->setBounds(b);
+    getParentComponent()->addAndMakeVisible(*analysisWidget);
+    repaint();
+}
+
 } // namespace Widgets
 } // namespace Surge
