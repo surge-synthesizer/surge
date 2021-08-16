f691b9de0d093f47a4ab04dbd8f450246cf163a7
Paul Walker <paul@pwjw.com>
2021-08-16 18:50:33 -0400

    Fix pipelines
    Oops!

--- src/headless/UnitTests.cpp
+++ src/headless/UnitTests.cpp
@@ -1,6 +1,7 @@
 
 #define CATCH_CONFIG_RUNNER
 #include "catch2/catch2.hpp"
+#include "HeadlessUtils.h"
 
 /*
 ** All moved to UnitTestsBLAH.cpp
@@ -8,6 +9,11 @@
 
 int runAllTests(int argc, char **argv)
 {
+    auto surge = Surge::Headless::createSurge(44100);
+    std::cout << "Created surge with " << surge->storage.datapath << " & "
+              << surge->storage.userDataPath << std::endl;
+    std::cout << "WT/Patch = " << surge->storage.wt_list.size() << " & "
+              << surge->storage.patch_list.size() << std::endl;
     int result = Catch::Session().run(argc, argv);
     return result;
 }
