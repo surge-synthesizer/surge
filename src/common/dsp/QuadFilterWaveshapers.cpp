8de124f29ace71dac727f4cbff9115a745d93155
Paul <baconpaul@users.noreply.github.com>
2021-08-17 10:37:58 -0400

    Add two saturation waveshapers (#4851)
    OJD and ZamAudio
    
    Addresses #1964

--- src/common/dsp/QuadFilterWaveshapers.cpp
+++ src/common/dsp/QuadFilterWaveshapers.cpp
@@ -697,6 +697,59 @@ __m128 WAVEFOLDER(QuadFilterWaveshaperState *__restrict s, __m128 x, __m128 driv
     return ADAA<F, 0, 1>(s, x);
 }
 
+__m128 ZAMSAT(QuadFilterWaveshaperState *__restrict s, __m128 x, __m128 drive)
+{
+    x = CLIP(s, x, drive);
+
+    // 2x * ( 1 - |x| )
+    static const auto p1 = _mm_set1_ps(1.f);
+    static const auto p2 = _mm_set1_ps(2.f);
+
+    auto gz = _mm_cmpge_ps(x, _mm_setzero_ps());
+    auto sgn = _mm_sub_ps(_mm_and_ps(gz, p1), _mm_andnot_ps(gz, p1));
+
+    auto twox = _mm_mul_ps(p2, x);
+    auto F = _mm_sub_ps(twox, _mm_mul_ps(sgn, _mm_mul_ps(x, x)));
+    return F;
+}
+
+__m128 OJD(QuadFilterWaveshaperState *__restrict s, __m128 x, __m128 drive)
+{
+    x = _mm_mul_ps(x, drive);
+
+    static const auto pm17 = _mm_set1_ps(-1.7f);
+    static const auto p11 = _mm_set1_ps(1.1f);
+    static const auto pm03 = _mm_set1_ps(-0.3f);
+    static const auto p09 = _mm_set1_ps(0.9f);
+
+    static const auto denLow = _mm_set1_ps(1.f / (4 * (1 - 0.3f)));
+    static const auto denHigh = _mm_set1_ps(1.f / (4 * (1 - 0.9f)));
+
+    auto maskNeg = _mm_cmple_ps(x, pm17);                         // in <= -1.7f
+    auto maskPos = _mm_cmpge_ps(x, p11);                          // in > 1.1f
+    auto maskLow = _mm_andnot_ps(maskNeg, _mm_cmplt_ps(x, pm03)); // in > -1.7 && in < =0.3
+    auto maskHigh = _mm_andnot_ps(maskPos, _mm_cmpgt_ps(x, p09)); // in > 0.9 && in < 1.1
+    auto maskMid = _mm_and_ps(_mm_cmpge_ps(x, pm03), _mm_cmple_ps(x, p09)); // the middle
+
+    static const auto vNeg = _mm_set1_ps(-1.0);
+    static const auto vPos = _mm_set1_ps(1.0);
+    auto vMid = x;
+
+    auto xlow = _mm_sub_ps(x, pm03);
+    auto vLow = _mm_add_ps(xlow, _mm_mul_ps(denLow, _mm_mul_ps(xlow, xlow)));
+    vLow = _mm_add_ps(vLow, pm03);
+
+    auto xhi = _mm_sub_ps(x, p09);
+    auto vHi = _mm_sub_ps(xhi, _mm_mul_ps(denHigh, _mm_mul_ps(xhi, xhi)));
+    vHi = _mm_add_ps(vHi, p09);
+
+    auto res =
+        _mm_add_ps(_mm_add_ps(_mm_add_ps(_mm_and_ps(maskNeg, vNeg), _mm_and_ps(maskLow, vLow)),
+                              _mm_add_ps(_mm_and_ps(maskHigh, vHi), _mm_and_ps(maskPos, vPos))),
+                   _mm_and_ps(maskMid, vMid));
+
+    return res;
+}
 WaveshaperQFPtr GetQFPtrWaveshaper(int type)
 {
     switch (type)
@@ -785,6 +838,11 @@ WaveshaperQFPtr GetQFPtrWaveshaper(int type)
         return TableEval<SinNXBound<7>, 2048, CLIP, false>;
     case wst_10cycbound:
         return TableEval<SinNXBound<10>, 2048, CLIP, false>;
+
+    case wst_zamsat:
+        return ZAMSAT;
+    case wst_ojd:
+        return OJD;
     }
     return 0;
 }
