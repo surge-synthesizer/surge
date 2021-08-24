f1c9b4840d8ffb5e0d0632fca6a5a4b7eebb6325
Paul <baconpaul@users.noreply.github.com>
2021-08-24 12:25:27 -0400

    Build LuaJit if build host is ARM (#4905)
    The build host assumed x86_64 but not assumes `uname -m`

--- libs/LuaJitLib/build-fat-luajit.sh
+++ libs/LuaJitLib/build-fat-luajit.sh
@@ -25,11 +25,12 @@ cp -r LuaJIT/ "${SD}"
 
 cd "${SD}"
 MACOSX_DEPLOYMENT_TARGET=10.9 make clean
-MACOSX_DEPLOYMENT_TARGET=10.9 make HOST_CC="clang -target x86_64-apple-macos10.9" TARGET_CC="xcrun --toolchain arm64 clang -target arm64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path)"  || echo "That's OK though"
+MACOSX_DEPLOYMENT_TARGET=10.9 make HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain arm64 clang -target arm64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path)"  || echo "That's OK though"
 mv src/lib*a "${OD}/arm64"
 
 MACOSX_DEPLOYMENT_TARGET=10.9 make clean
-MACOSX_DEPLOYMENT_TARGET=10.9 make 
+MACOSX_DEPLOYMENT_TARGET=10.9 make HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain x86_64 clang -target x86_64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path)"  || echo "That's OK though"
+
 mv src/lib*a "${OD}/x86_64"
 
 lipo -create -arch arm64 "${OD}/arm64/libluajit.a" -arch x86_64 "${OD}/x86_64/libluajit.a" -output "${OD}/libluajit.a"
