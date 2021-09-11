adb4f5abbaf4cc0f15d16da735e58f11667fb48c
Mario Kruselj <mario.kruselj@gmail.com>
2021-09-11 15:07:12 +0200

    Reorder a couple of waveshapers

--- src/common/SurgeParamConfig.h
+++ src/common/SurgeParamConfig.h
@@ -1,19 +1,19 @@
 #pragma once
 
 /*
- * So look. Surge is an evolving system. We keep moving things and cleaning them up
+ * So look: Surge is an evolving system. We keep moving things and cleaning them up
  * and making things more separate. But that's the kind of work that's never done.
  *
- * Parameters in surge contain some informatino about the intended orientation of a slider.
- * Shoudl they? Well no of course not. We should have a separate map. We should not require
+ * Parameters in Surge contain some information about the intended orientation of a slider.
+ * Should they? Well no, of course not. We should have a separate map. We should not require
  * the non-UI code to know "Horizontal". But we still have that kicking around.
  *
- * Similarly effects can have a VU as a displaly item. Coo. But you you need to know
- * what kind of VU. So we could have a 'logical' type here nad a 'ui' type in src/gui
+ * Similarly, effects can have a VU as a display item. Cool. But do you need to know
+ * what kind of VU? So we could have a 'logical' type here and a 'UI' type in src/gui
  * and we should. I know we should. But we don't yet.
  *
  * So this is the place we put all the enums which should "strictly" be UI or plugin-only
- * concepts but which bind to params and dsp configuration. The list is super small so
+ * concepts, but which bind to parameters and DSP configuration. The list is super small so
  * that's good. But it's not zero.
  */
 
