4867cb8e7417a062afba0560790ed89c4272d82b
EvilDragon <mario.kruselj@gmail.com>
2021-09-14 01:52:56 +0200

    A few waveshaper fx fixes (#5087)
    Full init preset, bias actually works

--- src/common/SurgeStorage.h
+++ src/common/SurgeStorage.h
@@ -306,7 +306,7 @@ const char fx_type_names[n_fx_types][16] = {
     "Off",        "Delay",       "Reverb 1",   "Phaser",      "Rotary",    "Distortion", "EQ",
     "Freq Shift", "Conditioner", "Chorus",     "Vocoder",     "Reverb 2",  "Flanger",    "Ring Mod",
     "Airwindows", "Neuron",      "Graphic EQ", "Resonator",   "CHOW",      "Exciter",    "Ensemble",
-    "Combulator", "Nimbus",      "Tape",       "Treemonster", "WaveShaper"};
+    "Combulator", "Nimbus",      "Tape",       "Treemonster", "Waveshaper"};
 
 const char fx_type_shortnames[n_fx_types][8] = {
     "OFF", "DLY", "RV1", "PH",  "ROT", "DIST", "EQ",  "FRQ", "DYN", "CH",  "VOC",  "RV2", "FL",
