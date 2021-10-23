e75841fe1a48c133d4569c60e02f7c9d71df1f83
jatinchowdhury18 <jatinchowdhury18@users.noreply.github.com>
2021-10-23 22:01:47 +0100

    Spring Reverb: Fix build errors

--- src/common/dsp/effects/chowdsp/shared/chowdsp_DelayLine.cpp
+++ src/common/dsp/effects/chowdsp/shared/chowdsp_DelayLine.cpp
@@ -27,67 +27,69 @@ namespace chowdsp
 {
 //==============================================================================
 template <typename SampleType, typename InterpolationType>
-DelayLine<SampleType, InterpolationType>::DelayLine()
-    : DelayLine (0)
+DelayLine<SampleType, InterpolationType>::DelayLine() : DelayLine(0)
 {
 }
 
 template <typename SampleType, typename InterpolationType>
-DelayLine<SampleType, InterpolationType>::DelayLine (int maximumDelayInSamples)
+DelayLine<SampleType, InterpolationType>::DelayLine(int maximumDelayInSamples)
 {
-    jassert (maximumDelayInSamples >= 0);
+    jassert(maximumDelayInSamples >= 0);
 
-    totalSize = juce::jmax (4, maximumDelayInSamples + 1);
+    totalSize = juce::jmax(4, maximumDelayInSamples + 1);
 }
 
 //==============================================================================
 template <typename SampleType, typename InterpolationType>
-void DelayLine<SampleType, InterpolationType>::setDelay (DelayLine<SampleType, InterpolationType>::NumericType newDelayInSamples)
+void DelayLine<SampleType, InterpolationType>::setDelay(
+    DelayLine<SampleType, InterpolationType>::NumericType newDelayInSamples)
 {
-    auto upperLimit = (NumericType) (totalSize - 1);
-    jassert (juce::isPositiveAndNotGreaterThan (newDelayInSamples, upperLimit));
+    auto upperLimit = (NumericType)(totalSize - 1);
+    jassert(juce::isPositiveAndNotGreaterThan(newDelayInSamples, upperLimit));
 
-    delay = juce::jlimit ((NumericType) 0, upperLimit, newDelayInSamples);
-    delayInt = static_cast<int> (std::floor (delay));
-    delayFrac = delay - (NumericType) delayInt;
+    delay = juce::jlimit((NumericType)0, upperLimit, newDelayInSamples);
+    delayInt = static_cast<int>(std::floor(delay));
+    delayFrac = delay - (NumericType)delayInt;
 
-    interpolator.updateInternalVariables (delayInt, delayFrac);
+    interpolator.updateInternalVariables(delayInt, delayFrac);
 }
 
 template <typename SampleType, typename InterpolationType>
-typename DelayLine<SampleType, InterpolationType>::NumericType DelayLine<SampleType, InterpolationType>::getDelay() const
+typename DelayLine<SampleType, InterpolationType>::NumericType
+DelayLine<SampleType, InterpolationType>::getDelay() const
 {
     return delay;
 }
 
 //==============================================================================
 template <typename SampleType, typename InterpolationType>
-void DelayLine<SampleType, InterpolationType>::prepare (const juce::dsp::ProcessSpec& spec)
+void DelayLine<SampleType, InterpolationType>::prepare(const juce::dsp::ProcessSpec &spec)
 {
-    jassert (spec.numChannels > 0);
+    jassert(spec.numChannels > 0);
 
-    this->bufferData = juce::dsp::AudioBlock<SampleType> (this->dataBlock, spec.numChannels, 2 * (size_t) totalSize);
+    this->bufferData =
+        juce::dsp::AudioBlock<SampleType>(this->dataBlock, spec.numChannels, 2 * (size_t)totalSize);
 
-    this->writePos.resize (spec.numChannels);
-    this->readPos.resize (spec.numChannels);
+    this->writePos.resize(spec.numChannels);
+    this->readPos.resize(spec.numChannels);
 
-    this->v.resize (spec.numChannels);
-    interpolator.reset (totalSize);
+    this->v.resize(spec.numChannels);
+    interpolator.reset(totalSize);
 
     reset();
 
-    bufferPtrs.resize (spec.numChannels);
+    bufferPtrs.resize(spec.numChannels);
     for (size_t ch = 0; ch < spec.numChannels; ++ch)
-        bufferPtrs[ch] = this->bufferData.getChannelPointer (ch);
+        bufferPtrs[ch] = this->bufferData.getChannelPointer(ch);
 }
 
 template <typename SampleType, typename InterpolationType>
 void DelayLine<SampleType, InterpolationType>::reset()
 {
-    for (auto vec : { &this->writePos, &this->readPos })
-        std::fill (vec->begin(), vec->end(), 0);
+    for (auto vec : {&this->writePos, &this->readPos})
+        std::fill(vec->begin(), vec->end(), 0);
 
-    std::fill (this->v.begin(), this->v.end(), static_cast<SampleType> (0));
+    std::fill(this->v.begin(), this->v.end(), static_cast<SampleType>(0));
 
     this->bufferData.clear();
 }
