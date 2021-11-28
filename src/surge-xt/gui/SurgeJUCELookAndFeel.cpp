c47da592d68b0ac2b9ad6c3d744406423254344f
EvilDragon <mario.kruselj@gmail.com>
2021-11-29 00:40:26 +0100

    Override juce drawPopupMenuItem (#5529)
    ...in order to draw shortcut key text with the same font size as the rest of the menu entry.

--- src/surge-xt/gui/SurgeJUCELookAndFeel.cpp
+++ src/surge-xt/gui/SurgeJUCELookAndFeel.cpp
@@ -281,3 +281,90 @@ Button *SurgeJUCELookAndFeel::createDocumentWindowButton(int buttonType)
     jassertfalse;
     return nullptr;
 }
+
+// overriden here just to make the shortcut text same size as normal menu entry text
+void SurgeJUCELookAndFeel::drawPopupMenuItem(Graphics &g, const Rectangle<int> &area,
+                                             const bool isSeparator, const bool isActive,
+                                             const bool isHighlighted, const bool isTicked,
+                                             const bool hasSubMenu, const String &text,
+                                             const String &shortcutKeyText, const Drawable *icon,
+                                             const Colour *const textColourToUse)
+{
+    if (isSeparator)
+    {
+        auto r = area.reduced(5, 0);
+        r.removeFromTop(roundToInt(((float)r.getHeight() * 0.5f) - 0.5f));
+
+        g.setColour(findColour(PopupMenu::textColourId).withAlpha(0.3f));
+        g.fillRect(r.removeFromTop(1));
+    }
+    else
+    {
+        auto textColour =
+            (textColourToUse == nullptr ? findColour(PopupMenu::textColourId) : *textColourToUse);
+
+        auto r = area.reduced(1);
+
+        if (isHighlighted && isActive)
+        {
+            g.setColour(findColour(PopupMenu::highlightedBackgroundColourId));
+            g.fillRect(r);
+
+            g.setColour(findColour(PopupMenu::highlightedTextColourId));
+        }
+        else
+        {
+            g.setColour(textColour.withMultipliedAlpha(isActive ? 1.0f : 0.5f));
+        }
+
+        r.reduce(jmin(5, area.getWidth() / 20), 0);
+
+        auto font = getPopupMenuFont();
+
+        auto maxFontHeight = (float)r.getHeight() / 1.3f;
+
+        if (font.getHeight() > maxFontHeight)
+            font.setHeight(maxFontHeight);
+
+        g.setFont(font);
+
+        auto iconArea = r.removeFromLeft(roundToInt(maxFontHeight)).toFloat();
+
+        if (icon != nullptr)
+        {
+            icon->drawWithin(g, iconArea,
+                             RectanglePlacement::centred | RectanglePlacement::onlyReduceInSize,
+                             1.0f);
+            r.removeFromLeft(roundToInt(maxFontHeight * 0.5f));
+        }
+        else if (isTicked)
+        {
+            auto tick = getTickShape(1.0f);
+            g.fillPath(tick, tick.getTransformToScaleToFit(
+                                 iconArea.reduced(iconArea.getWidth() / 5, 0).toFloat(), true));
+        }
+
+        if (hasSubMenu)
+        {
+            auto arrowH = 0.6f * getPopupMenuFont().getAscent();
+
+            auto x = static_cast<float>(r.removeFromRight((int)arrowH).getX());
+            auto halfH = static_cast<float>(r.getCentreY());
+
+            Path path;
+            path.startNewSubPath(x, halfH - arrowH * 0.5f);
+            path.lineTo(x + arrowH * 0.6f, halfH);
+            path.lineTo(x, halfH + arrowH * 0.5f);
+
+            g.strokePath(path, PathStrokeType(2.0f));
+        }
+
+        r.removeFromRight(3);
+        g.drawFittedText(text, r, Justification::centredLeft, 1);
+
+        if (shortcutKeyText.isNotEmpty())
+        {
+            g.drawText(shortcutKeyText, r, Justification::centredRight, true);
+        }
+    }
+}
