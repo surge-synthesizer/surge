834240f1373a7f5cbcce05ef2f37926fb95f79aa
Paul <baconpaul@users.noreply.github.com>
2022-02-06 17:29:55 -0500

    A few cmake tweaks to XT (#5863)
    - Use the correct PRODUCT_NAME in the pluginval. Closes #5834
    - Conditionally deactivate CLAP builds for cmake versions
      less than 3.21. Closes #5862

--- src/cmake/pluginval.cmake
+++ src/cmake/pluginval.cmake
@@ -1,24 +1,24 @@
   message(STATUS "Validate me! Please!")
 
-  if (APPLE)
+  if(APPLE)
     set(pluginval_url "https://github.com/Tracktion/pluginval/releases/latest/download/pluginval_macOS.zip")
     set(pluginval_exe pluginval/pluginval.app/Contents/MacOS/pluginval)
-  elseif (WIN32)
+  elseif(WIN32)
     set(pluginval_url "https://github.com/Tracktion/pluginval/releases/latest/download/pluginval_Windows.zip")
     set(pluginval_exe pluginval/pluginval.exe)
-  else ()
+  else()
     set(pluginval_url "https://github.com/Tracktion/pluginval/releases/latest/download/pluginval_Linux.zip")
     set(pluginval_exe pluginval/pluginval)
-  endif ()
+  endif()
 
   add_custom_target(stage-pluginval)
   add_custom_command(TARGET stage-pluginval
-          POST_BUILD
-          WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-          COMMAND cmake -E make_directory pluginval
-          COMMAND curl -L ${pluginval_url} -o pluginval/pluginval.zip
-          COMMAND cd pluginval && unzip -o pluginval.zip
-          )
+    POST_BUILD
+    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+    COMMAND cmake -E make_directory pluginval
+    COMMAND curl -L ${pluginval_url} -o pluginval/pluginval.zip
+    COMMAND cd pluginval && unzip -o pluginval.zip
+    )
 
   add_custom_target(surge-pluginval-all)
   function(create_pluginval_target name target plugin)
@@ -28,17 +28,20 @@
     add_dependencies(${name} stage-pluginval)
     get_target_property(plugin_location ${target} LIBRARY_OUTPUT_DIRECTORY)
     add_custom_command(TARGET ${name}
-            POST_BUILD
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-            COMMAND ${pluginval_exe} --validate-in-process --output-dir "." --validate "${plugin_location}/${plugin_name}" || exit 1
-            )
+      POST_BUILD
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+      COMMAND ${pluginval_exe} --validate-in-process --output-dir "." --validate "${plugin_location}/${plugin_name}" || exit 1
+      )
     add_dependencies(surge-pluginval-all ${name})
   endfunction()
 
-  create_pluginval_target(surge-xt-pluginval-vst3 surge-xt_VST3 "Surge XT.vst3")
-  create_pluginval_target(surge-fx-pluginval-vst3 surge-fx_VST3 "${SURGE_FX_PRODUCT_NAME}.vst3")
+  get_target_property(fxn surge-fx JUCE_PRODUCT_NAME)
+  get_target_property(xtn surge-xt JUCE_PRODUCT_NAME)
 
-  if (APPLE)
-    create_pluginval_target(surge-xt-pluginval-au surge-xt_AU "Surge XT.component")
-    create_pluginval_target(surge-fx-pluginval-au surge-fx_AU "${SURGE_FX_PRODUCT_NAME}.component")
-  endif ()
+  create_pluginval_target(surge-xt-pluginval-vst3 surge-xt_VST3 "${xtn}.vst3")
+  create_pluginval_target(surge-fx-pluginval-vst3 surge-fx_VST3 "${fxn}.vst3")
+
+  if(APPLE)
+    create_pluginval_target(surge-xt-pluginval-au surge-xt_AU "${xtn}.component")
+    create_pluginval_target(surge-fx-pluginval-au surge-fx_AU "${fxn}.component")
+  endif()
