e5f598da959737a0a7aecf15aa36575f209d4ebc
Mario Kruselj <mario.kruselj@gmail.com>
2022-05-21 21:33:03 +0200

    Fix gradient fill in filter analysis windows

--- src/surge-xt/gui/overlays/FilterAnalysis.cpp
+++ src/surge-xt/gui/overlays/FilterAnalysis.cpp
@@ -150,6 +150,7 @@ void FilterAnalysis::paint(juce::Graphics &g)
     static constexpr auto dbMin = -42.f;
     static constexpr auto dbMax = 12.f;
     constexpr auto dbRange = dbMax - dbMin;
+
     auto freqToX = [&](float freq, int width) {
         auto xNorm = std::log(freq / lowFreq) / std::log(highFreq / lowFreq);
         return xNorm * (float)width;
@@ -158,24 +159,27 @@ void FilterAnalysis::paint(juce::Graphics &g)
     auto dbToY = [&](float db, int height) { return (float)height * (dbMax - db) / dbRange; };
 
     char nm[256], snm[256];
+
     fs.type.get_display(nm);
     fs.subtype.get_display(snm);
 
     auto label = fmt::format("{}", nm);
+
     if (sst::filters::fut_subcount[fs.type.val.i] > 0)
+    {
         label = fmt::format("{} ({})", nm, snm);
+    }
 
     g.fillAll(skin->getColor(Colors::MSEGEditor::Background));
 
     auto lb = getLocalBounds().transformedBy(getTransform().inverted());
-
     auto dRect = lb.withTrimmedTop(15).reduced(4);
-
     auto width = dRect.getWidth();
     auto height = dRect.getHeight();
     auto labelHeight = 9;
     auto font = skin->fontManager->getLatoAtSize(7);
 
+    // horizontal and vertical grids
     {
         auto gs = juce::Graphics::ScopedSaveState(g);
 
@@ -237,6 +241,7 @@ void FilterAnalysis::paint(juce::Graphics &g)
         }
     }
 
+    // construct filter response curve
     if (catchUpStore != evaluator->outboundUpdates)
     {
         std::pair<std::vector<float>, std::vector<float>> data;
@@ -251,16 +256,20 @@ void FilterAnalysis::paint(juce::Graphics &g)
         auto [freqAxis, magResponseDBSmoothed] = data;
         bool started = false;
         const auto nPoints = freqAxis.size();
+
         for (int i = 0; i < nPoints; ++i)
         {
             if (freqAxis[i] < lowFreq / 2.f || freqAxis[i] > highFreq * 1.01f)
+            {
                 continue;
+            }
 
-            auto xDraw = freqToX(freqAxis[i], dRect.getWidth());
-            auto yDraw = dbToY(magResponseDBSmoothed[i], dRect.getHeight());
+            auto xDraw = freqToX(freqAxis[i], width);
+            auto yDraw = dbToY(magResponseDBSmoothed[i], height);
 
             xDraw += dRect.getX();
             yDraw += dRect.getY();
+
             if (!started)
             {
                 plotPath.startNewSubPath(xDraw, yDraw);
@@ -273,16 +282,21 @@ void FilterAnalysis::paint(juce::Graphics &g)
         }
     }
 
+    // plot border
     g.setColour(skin->getColor(Colors::MSEGEditor::Grid::Primary));
     g.drawRect(dRect);
 
+    // fill below curve
     {
         auto gs = juce::Graphics::ScopedSaveState(g);
         auto fp = plotPath;
+
         fp.lineTo(dRect.getX() + width, dRect.getY() + height);
         fp.lineTo(0, dRect.getY() + height);
+        fp.closeSubPath();
 
         g.reduceClipRegion(dRect);
+
         auto cg = juce::ColourGradient::vertical(
             skin->getColor(Colors::MSEGEditor::GradientFill::StartColor),
             skin->getColor(Colors::MSEGEditor::GradientFill::EndColor), dRect);
@@ -290,15 +304,18 @@ void FilterAnalysis::paint(juce::Graphics &g)
         g.setGradientFill(cg);
         g.fillPath(fp);
     }
+
+    // plot curve
     {
         auto gs = juce::Graphics::ScopedSaveState(g);
+
         g.reduceClipRegion(dRect);
         g.setColour(skin->getColor(Colors::MSEGEditor::Curve));
-
         g.strokePath(plotPath, juce::PathStrokeType(1.f, juce::PathStrokeType::JointStyle::curved));
     }
+
     auto txtr = lb.withHeight(15);
-    // MSEG::Text is black - use the same color as the waveshaper preview for the title
+
     g.setColour(skin->getColor(Colors::Waveshaper::Preview::Text));
     g.setFont(skin->getFont(Fonts::Waveshaper::Preview::Title));
     g.drawText(label, txtr, juce::Justification::centred);
