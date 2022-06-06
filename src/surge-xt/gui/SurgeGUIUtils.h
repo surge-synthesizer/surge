fc8243779c4bf07ce1c949d590d7c66a7a7fca9f
EvilDragon <mario.kruselj@gmail.com>
2022-06-06 20:55:27 +0200

    A couple of step seq out of bounds edit fixes (#6250)
    First, when sweep dragging step seq triggers, these were constrained to just trigger rects. Update this so that we use event.pos.x so even if mouse leaves the trigger rects, the edits will apply.
    
    Second, right mouse arrow drag in step seq would not be applied if mouse was released out of bounds. Fix this by applying the same point on line within rectangle constrant as done in paintStepSeq(). Abstract that into a method in SurgeGUIUtils.

--- src/surge-xt/gui/SurgeGUIUtils.h
+++ src/surge-xt/gui/SurgeGUIUtils.h
@@ -2,6 +2,7 @@
 
 #include "SurgeStorage.h"
 #include "UserDefaults.h"
+#include "juce_gui_basics/juce_gui_basics.h"
 
 namespace Surge
 {
@@ -11,8 +12,11 @@ extern bool showCursor(SurgeStorage *storage);
 extern bool allowKeyboardEdits(SurgeStorage *storage);
 extern bool isTouchMode(SurgeStorage *storage);
 
-bool get_line_intersection(float p0_x, float p0_y, float p1_x, float p1_y, float p2_x, float p2_y,
-                           float p3_x, float p3_y, float *i_x, float *i_y);
+bool getLineIntersection(float p0_x, float p0_y, float p1_x, float p1_y, float p2_x, float p2_y,
+                         float p3_x, float p3_y, float *i_x, float *i_y);
+
+void constrainPointOnLineWithinRectangle(const juce::Rectangle<float> rect,
+                                         const juce::Line<float> l, juce::Point<float> &p);
 
 void openFileOrFolder(const std::string &f);
 inline void openFileOrFolder(const fs::path &f) { openFileOrFolder(path_to_string(f)); }
