c2b5ba76b4fc87ce0af16144d27028cdce7bf5d9
Paul Walker <paul@pwjw.com>
2022-06-23 06:58:53 -0400

    Revert "Change default MIDI/MPE bend smoothing to fast linear (#6305)"
    This reverts commit 3c31f8411df384464a4cf7bc257b425f49888f18.
    
    For some reason we don't understand, this diff breaks our regtests
    with some reliability. Revert while we figure it out (after my
    vaca probably)

--- src/common/ModulationSource.h
+++ src/common/ModulationSource.h
@@ -317,7 +317,7 @@ template <int NDX = 1> class ControllerModulationSourceVector : public Modulatio
 {
   public:
     // Smoothing and Shaping Behaviors
-    Modulator::SmoothingMode smoothingMode = Modulator::SmoothingMode::FAST_LINE;
+    Modulator::SmoothingMode smoothingMode = Modulator::SmoothingMode::LEGACY;
 
     ControllerModulationSourceVector()
     {
@@ -327,7 +327,7 @@ template <int NDX = 1> class ControllerModulationSourceVector : public Modulatio
             value[i] = 0.f;
             changed[i] = true;
         }
-        smoothingMode = Modulator::SmoothingMode::FAST_LINE;
+        smoothingMode = Modulator::SmoothingMode::LEGACY;
         bipolar = false;
     }
     ControllerModulationSourceVector(Modulator::SmoothingMode mode)
