0c4799dd38e368714a68ae0e34b255b9a78c9f8a
Mario Kruselj <mario.kruselj@gmail.com>
2023-01-22 23:43:22 +0100

    clangformat

--- src/common/Parameter.h
+++ src/common/Parameter.h
@@ -455,8 +455,7 @@ class Parameter
     float get_modulation_f01(float mod) const;
     float set_modulation_f01(float v) const;
 
-    float set_modulation_value_from_string(const std::string &s, std::string &errMsg,
-                                                 bool &valid);
+    float set_modulation_value_from_string(const std::string &s, std::string &errMsg, bool &valid);
 
     void bound_value(bool force_integer = false);
     std::string tempoSyncNotationValue(float f) const;
