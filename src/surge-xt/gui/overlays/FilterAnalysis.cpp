67568bc8014003dc52d48ffe2fe8ea483a61a886
Alexandr Zyurkalov <alexandr.zyurkalov@idealo.de>
2023-03-05 20:26:42 +0100

    extends highFreq

--- src/surge-xt/gui/overlays/FilterAnalysis.cpp
+++ src/surge-xt/gui/overlays/FilterAnalysis.cpp
@@ -146,7 +146,7 @@ void FilterAnalysis::paint(juce::Graphics &g)
     auto &fs = editor->getPatch().scene[editor->current_scene].filterunit[whichFilter];
 
     static constexpr auto lowFreq = 10.f;
-    static constexpr auto highFreq = 24000.f;
+    static constexpr auto highFreq = 25090.f;
     static constexpr auto dbMin = -42.f;
     static constexpr auto dbMax = 12.f;
     constexpr auto dbRange = dbMax - dbMin;
