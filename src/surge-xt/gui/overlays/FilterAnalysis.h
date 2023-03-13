9176dfcf7c02df6c9cfd68ce53357de9c7d6838e
Paul <baconpaul@users.noreply.github.com>
2023-03-13 19:43:22 +0100

    clang-format for FilterAnalysis.h

--- src/surge-xt/gui/overlays/FilterAnalysis.h
+++ src/surge-xt/gui/overlays/FilterAnalysis.h
@@ -38,7 +38,7 @@ struct FilterAnalysis : public OverlayComponent, Surge::GUI::SkinConsumingCompon
     SurgeStorage *storage{nullptr};
     FilterAnalysis(SurgeGUIEditor *e, SurgeStorage *s);
     ~FilterAnalysis();
-    void mouseDrag(const juce::MouseEvent& event) override;
+    void mouseDrag(const juce::MouseEvent &event) override;
     void mouseDown(const juce::MouseEvent &event) override;
     void paint(juce::Graphics &g) override;
     void onSkinChanged() override;
