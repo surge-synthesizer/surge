fbf793e165e00950ff4cb0669c6fcf5b97af7c0a
Mario Kruselj <mario.kruselj@gmail.com>
2023-06-26 21:06:35 +0200

    clangformat

--- src/common/SurgeStorage.h
+++ src/common/SurgeStorage.h
@@ -116,7 +116,7 @@ const int FIRoffsetI16 = FIRipolI16_N >> 1;
 // 19 -> 20 (XT 1.1 release)   added voice envelope mode, but super late so don't break 19
 // 20 -> 21 (XT 1.2 nightlies) added absolutable mode for Combulator Offset 1/2 (to match the behavior of Center parameter)
 //                             added oddsound_as_mts_main
-// 21 -> 22 (XT 1.3 nighlies)  added new ring modulator modes in the mixer, add bonsai distortion effect
+// 21 -> 22 (XT 1.3 nighlies)  added new ring modulator modes in the mixer, add bonsai distortion effect, changed MIDI mapping behavior
 // clang-format on
 
 const int ff_revision = 22;
@@ -945,9 +945,9 @@ struct DAWExtraStateStorage
 
     bool mapChannelToOctave = false;
 
-    std::map<int, int> midictrl_map;      // param -> midictrl
-    std::map<int, int> midichan_map;      // param -> midichan
-    std::map<int, int> customcontrol_map; // custom controller number -> midictrl
+    std::map<int, int> midictrl_map;           // param -> midictrl
+    std::map<int, int> midichan_map;           // param -> midichan
+    std::map<int, int> customcontrol_map;      // custom controller number -> midictrl
     std::map<int, int> customcontrol_chan_map; // custom controller number -> midichan
 
     int monoPedalMode = 0;
