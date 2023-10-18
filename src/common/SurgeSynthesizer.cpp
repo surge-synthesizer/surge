e48b841bb16052261cfe21afbd90f6f697a3bb24
EvilDragon <mario.kruselj@gmail.com>
2023-10-18 13:58:14 +0200

    Several minor things (#7263)
    Bump copyright year (a bit late but oh well!)
    Rename OSC unit test group
    Make loadPatchPy return bool instead of void

--- src/common/SurgeSynthesizer.cpp
+++ src/common/SurgeSynthesizer.cpp
@@ -2012,36 +2012,36 @@ void SurgeSynthesizer::sendParameterAutomation(long index, float value)
 
 void SurgeSynthesizer::onRPN(int channel, int lsbRPN, int msbRPN, int lsbValue, int msbValue)
 {
-    /* OK there are two things we are dealing with here
+    /* OK there are two things we are dealing with here.
 
-     1: The MPE specification v1.0 section 2.1.1 says the message format for RPN is
+       1: The MPE specification v1.0 section 2.1.1 says the message format for RPN is
 
-        Bn 64 06
-        Bn 65 00
-        Bn 06 mm
+          Bn 64 06
+          Bn 65 00
+          Bn 06 mm
 
-     where n = 0 is lower zone, n=F is upper zone, all others are invalid, and mm=0 means no MPE and
-     mm=1->F is zone.
+       where n = 0 is lower zone, n=F is upper zone, all others are invalid
+       mm=0 means no MPE and mm=1->F is zone.
 
-     So you would think the Roli Seaboard would send, since it is one zone
+       So you would think Roli Seaboard would send, since it is one zone
 
-        B0 64 06 B0 65 00 B0 06 0F
+          B0 64 06 B0 65 00 B0 06 0F
 
-     and be done with it. If it did this code would work.
+       and be done with it. If it did, this code would work.
 
-     But it doesn't. At least with Roli Seaboard Firmware 1.1.7.
+       But it doesn't. At least with Roli Seaboard Firmware 1.1.7.
 
-     Instead on *each* channel it sends
+       Instead on *each* channel it sends:
 
-        Bn 64 04 Bn 64 0 Bn 06 00
-        Bn 64 03 Bn 64 0 Bn 06 00
+          Bn 64 04 Bn 64 0 Bn 06 00
+          Bn 64 03 Bn 64 0 Bn 06 00
 
-     for each channel. Which seems unrelated to the spec. But as a result the original onRPN code
-     means you get no MPE with a Roli Seaboard.
+       Which seems unrelated to the spec. But as a result, the original onRPN() code
+       means you get no MPE with Roli Seaboard.
 
-     Hey one year later an edit: Those aren't coming from ROLI they are coming from Logic PRO and
-     now that I correct modify and stream MPE state, we should not listen to those messages.
-     */
+       EDIT: Those aren't coming from Roli, they are coming from Logic, and now that we correctly
+       modify and stream MPE state, we should not listen to those messages!
+    */
 
     if (lsbRPN == 0 && msbRPN == 0) // PITCH BEND RANGE
     {
@@ -2067,18 +2067,10 @@ void SurgeSynthesizer::onRPN(int channel, int lsbRPN, int msbRPN, int lsbValue, 
     else if (lsbRPN == 4 && msbRPN == 0 && channel != 0 && channel != 0xF)
     {
         /*
-        ** This is code sent by logic in all cases for some reason. In ancient times
-        ** I thought it came from a roli. But I since changed the MPE state management so
-        ** with 1.6.5 do this:
+        ** This is code sent by Logic in all cases for some reason. In ancient times
+        ** we thought it came from a Roli. But since we changed the MPE state management so
+        ** from 1.6.5 onwards do this:
         */
-#if 0
-       // This is the invalid message which the ROLI sends. Rather than have the Roli not work
-       mpeEnabled = true;
-       mpeVoices = msbValue & 0xF;
-       mpePitchBendRange = Surge::Storage::getUserDefaultValue(&storage, "mpePitchBendRange", 48);
-       std::cout << __LINE__ << " " << __FILE__ << " MPEE=" << mpeEnabled << " MPEPBR=" << mpePitchBendRange << std::endl;
-       mpeGlobalPitchBendRange = 0;
-#endif
         return;
     }
 }
