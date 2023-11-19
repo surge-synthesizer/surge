6bf1e4c8eb8e31df2a55727a3a8c9554327b6e0f
Paul <baconpaul@users.noreply.github.com>
2023-11-19 15:58:41 -0500

    Copy and Paste Scene includes Global Modes; Insert FX (#7312)
    Copy and Paste a scene now includes the following
    
    1. Any scene -> global modulators (like SLFO1 to global volume)
    2. The insert FX chain
    3. Any modulations to the insert FX move from A to B on paste so
       the topology remains
    
    Closes #7166

--- src/common/SurgeStorage.h
+++ src/common/SurgeStorage.h
@@ -1149,6 +1149,12 @@ class MTSClient;
 
 namespace Surge
 {
+
+namespace FxClipboard
+{
+struct Clipboard;
+}
+
 namespace Storage
 {
 struct ScenesOutputData
@@ -1348,9 +1354,11 @@ class alignas(16) SurgeStorage
     // and also to stop me having to move all of isValidModulation and its buddies onto SurgeStorage
     void clipboard_paste(
         int type, int scene, int entry, modsources ms = ms_original,
-        std::function<bool(int, modsources)> isValidModulation = [](auto a, auto b) {
-            return true;
-        });
+        std::function<bool(int, modsources)> isValidModulation = [](auto a,
+                                                                    auto b) { return true; },
+        std::function<void(std::unique_ptr<Surge::FxClipboard::Clipboard> &, int)> updateFxInSlot =
+            [](auto &a, auto b) {});
+    ;
     int get_clipboard_type() const;
     // direction: false for previous, true for next
     int getAdjacentWaveTable(int id, bool direction) const;
@@ -1637,6 +1645,8 @@ class alignas(16) SurgeStorage
     std::vector<Parameter> clipboard_p;
     int clipboard_type;
     StepSequencerStorage clipboard_stepsequences[n_lfos];
+    // UP this for the forward decl fix
+    std::unique_ptr<Surge::FxClipboard::Clipboard> clipboard_scenefx[n_fx_per_chain];
     MSEGStorage clipboard_msegs[n_lfos];
     FormulaModulatorStorage clipboard_formulae[n_lfos];
     OscillatorStorage::ExtraConfigurationData clipboard_extraconfig[n_oscs];
