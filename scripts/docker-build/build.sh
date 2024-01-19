c94a893934c56af1e2a3140fd727b0933e362893
baconpaul <paul@pwjw.com>
2024-01-19 09:00:34 -0500

    more

--- scripts/docker-build/build.sh
+++ scripts/docker-build/build.sh
@@ -5,7 +5,11 @@ set -x
 BUILD=$1
 echo "Building surge in ${BUILD}"
 
+whoami
+
 pushd /home/build/surge
+
+ls -al .
 
 cmake -B${BUILD} -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_C_COMPILER=gcc-11 -DSURGE_BUILD_LV2=TRUE
 cmake --build ${BUILD} ${@:2}
