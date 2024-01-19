d79c717663acdc0e50b24ae90ac0cc5dad0869ab
baconpaul <paul@pwjw.com>
2024-01-19 12:27:55 -0500

    Docker Image back to gcc9
    gcc11 had libcxx too high a version

--- scripts/docker-build/build.sh
+++ scripts/docker-build/build.sh
@@ -9,7 +9,7 @@ pushd /home/build/surge
 
 ls -al .
 
-cmake -B${BUILD} -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_C_COMPILER=gcc-11 -DSURGE_BUILD_LV2=TRUE
+cmake -B${BUILD} -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_C_COMPILER=gcc-9 -DSURGE_BUILD_LV2=TRUE
 cmake --build ${BUILD} ${@:2}
 
 popd
