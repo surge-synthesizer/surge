26469730b9e29aaa0ae4bfc941e78b244ddf60be
Paul Walker <paul@pwjw.com>
2024-03-03 10:10:40 -0500

    Fix a Symlink Scannign error
    If a patch was a symlink to a missing patch, the modtime scan
    at startup to rebuild the patch db would throw an uncaught
    filesystem exception. Catch it and compensate.
    
    Closes #7547

--- src/surge-xt/SurgeSynthProcessor.h
+++ src/surge-xt/SurgeSynthProcessor.h
@@ -519,7 +519,7 @@ class SurgeSynthProcessor : public juce::AudioProcessor,
   public:
     bool inputIsLatent{false};
 
-    std::string fatalErrorMessage{false};
+    std::string fatalErrorMessage{};
 
   private:
     // Have we warned about bad configurations
