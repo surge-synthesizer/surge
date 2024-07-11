fe0d1c3057785655735392f5fcc4f17fe943eb0a
nuoun <10226424+nuoun@users.noreply.github.com>
2024-07-11 23:50:34 +0200

    Switch to export MACOSX_DEPLOYMENT_TARGET

--- libs/LuaJitLib/build-macos-luajit.sh
+++ libs/LuaJitLib/build-macos-luajit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-BD=$1
-if [[ -z ${BD} ]]; then
-    echo "Usage: build-fat-luajit.sh BUILDDIR"
+BD="$1"
+if [[ -z "${BD}" ]]; then
+    echo "Usage: build-fat-luajit.sh \"BUILDDIR\""
     exit 1
 fi
-echo "Building in ${BD}"
+echo "Building in \"${BD}\""
 
-SD=${BD}/src
-OD=${BD}/bin
-HD=${BD}/include
+SD="${BD}/src"
+OD="${BD}/bin"
+HD="${BD}/include"
 
 rm -rf "${BD}" || error=1
 mkdir -p "${BD}" || error=1
@@ -22,11 +22,12 @@ mkdir "${HD}" || error=1
 cp -r LuaJIT "${SD}" || error=1
 cd "${SD}/LuaJIT/src" || error=1
 
-MACOSX_DEPLOYMENT_TARGET=10.9 make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain arm64 clang -target arm64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3" || error=1
+export MACOSX_DEPLOYMENT_TARGET=10.9
+make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain arm64 clang -target arm64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3" || error=1
 cp lib*a "${OD}/arm64" || error=1
 
-MACOSX_DEPLOYMENT_TARGET=10.9 make clean || error=1
-MACOSX_DEPLOYMENT_TARGET=10.9 make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain x86_64 clang -target x86_64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3" || error=1
+make clean || error=1
+make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain x86_64 clang -target x86_64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3" || error=1
 cp lib*a "${OD}/x86_64" || error=1
 
 lipo -create -arch arm64 "${OD}/arm64/libluajit.a" -arch x86_64 "${OD}/x86_64/libluajit.a" -output "${OD}/libluajit.a" || exit 17
