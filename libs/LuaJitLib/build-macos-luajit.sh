1ce107fa1b5660bbb9e553ecf982168a351daf56
nuoun <10226424+nuoun@users.noreply.github.com>
2024-07-12 01:10:52 +0200

    Cleanup and comments

--- libs/LuaJitLib/build-macos-luajit.sh
+++ libs/LuaJitLib/build-macos-luajit.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-BD=$1
-if [[ -z ${BD} ]]; then
-    echo "Usage: build-fat-luajit.sh BUILDDIR"
+BD="$1"
+if [[ -z "${BD}" ]]; then
+    echo "Usage: build-fat-luajit.sh \"BUILDDIR\""
     exit 1
 fi
-echo "Building in ${BD}"
+echo "Building in \"${BD}\""
 
-SD=${BD}/src
-OD=${BD}/bin
-HD=${BD}/include
+SD="${BD}/src"
+OD="${BD}/bin"
+HD="${BD}/include"
 
 rm -rf "${BD}" || error=1
 mkdir -p "${BD}" || error=1
@@ -20,22 +20,27 @@ mkdir "${OD}/x86_64" || error=1
 mkdir "${HD}" || error=1
 
 cp -r LuaJIT "${SD}" || error=1
-cd "${SD}/LuaJIT/src" || error=1
+# LuaJIT's top level makefile also has Darwin specific config so use that
+cd "${SD}/LuaJIT" || error=1
 
-MACOSX_DEPLOYMENT_TARGET=10.9 make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain arm64 clang -target arm64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3" || echo "That's OK though"
-cp lib*a "${OD}/arm64" || echo "That's OK though"
+# Setting MACOSX_DEPLOYMENT_TARGET is required for building LuaJIT
+export MACOSX_DEPLOYMENT_TARGET=10.9
 
-MACOSX_DEPLOYMENT_TARGET=10.9 make clean
-MACOSX_DEPLOYMENT_TARGET=10.9 make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain x86_64 clang -target x86_64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3" || echo "That's OK though"
-cp lib*a "${OD}/x86_64" || echo "That's OK though"
+# Logical or to suppress linker warnings?
+make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain arm64 clang -target arm64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3" || echo "That's OK though"
+cp src/lib*a "${OD}/arm64" || error=1
+
+make clean
+make amalg -j HOST_CC="clang -target `uname -m`-apple-macos10.9" TARGET_CC="xcrun --toolchain x86_64 clang -target x86_64-apply-macos10.9 -isysroot $(xcrun --sdk macosx --show-sdk-path) -fvisibility=hidden -fvisibility-inlines-hidden" TARGET_CFLAGS="-O3" || echo "That's OK though"
+cp src/lib*a "${OD}/x86_64" || error=1
 
 lipo -create -arch arm64 "${OD}/arm64/libluajit.a" -arch x86_64 "${OD}/x86_64/libluajit.a" -output "${OD}/libluajit.a" || exit 17
 
 echo "${OD}"
 ls -al "${OD}" || error=1
 
-cp *.h "${HD}" || error=1
-cp *.hpp "${HD}" || error=1
+cp src/*.h "${HD}" || error=1
+cp src/*.hpp "${HD}" || error=1
 echo "${HD}"
 ls -al "${HD}" || error=1
 
