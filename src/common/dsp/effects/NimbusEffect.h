f2ab32824eff84227ac20fc14af5fdd39784bbd6
Paul <baconpaul@users.noreply.github.com>
2024-07-26 11:26:43 -0400

    Move Nimbus to sst-effects (#7729)
    * Move Nimbus to sst-effects
    
    Addresses #7359
    
    With an almost-complete port (param name dynamism is in surge
    but not in the submodule yet)
    
    * f

--- src/common/dsp/effects/NimbusEffect.h
+++ src/common/dsp/effects/NimbusEffect.h
@@ -24,87 +24,28 @@
 #define SURGE_SRC_COMMON_DSP_EFFECTS_NIMBUSEFFECT_H
 
 #include "Effect.h"
+#include "SurgeSSTFXAdapter.h"
 
 #include <memory>
 #include <vembertech/lipol.h>
 #include "sst/basic-blocks/dsp/LanczosResampler.h"
+#include "sst/effects/Nimbus.h"
 
 namespace clouds
 {
 class GranularProcessor;
 }
 
-struct SRC_STATE_tag;
-
-class NimbusEffect : public Effect
+struct NimbusEffect
+    : public surge::sstfx::SurgeSSTFXBase<sst::effects::nimbus::Nimbus<surge::sstfx::SurgeFXConfig>>
 {
-  public:
-    enum nmb_params
-    {
-        nmb_mode,
-        nmb_quality,
+    using parent_t =
+        surge::sstfx::SurgeSSTFXBase<sst::effects::nimbus::Nimbus<surge::sstfx::SurgeFXConfig>>;
 
-        nmb_position,
-        nmb_size,
-        nmb_pitch,
-        nmb_density,
-        nmb_texture,
-        nmb_spread,
-
-        nmb_freeze,
-        nmb_feedback,
-
-        nmb_reverb,
-        nmb_mix,
-
-        nmb_num_params,
-    };
-
-  private:
-    lipol_ps_blocksz mix alignas(16);
-    float L alignas(16)[BLOCK_SIZE], R alignas(16)[BLOCK_SIZE];
-
-  public:
     NimbusEffect(SurgeStorage *storage, FxStorage *fxdata, pdata *pd);
-    virtual ~NimbusEffect();
-    virtual const char *get_effectname() override { return "Nimbus"; }
-    virtual void init() override;
-    virtual void process(float *dataL, float *dataR) override;
-    virtual void suspend() override;
-    void setvars(bool init);
     virtual void init_ctrltypes() override;
-    virtual void init_default_values() override;
-
     virtual const char *group_label(int id) override;
     virtual int group_label_ypos(int id) override;
-
-    virtual int get_ringout_decay() override { return -1; }
-
-    // Only used by rack
-    void setNimbusTrigger(bool b) { nimbusTrigger = b; }
-
-    void sampleRateReset() override;
-
-  private:
-    uint8_t *block_mem, *block_ccm;
-    clouds::GranularProcessor *processor;
-    static constexpr int processor_sr = 32000;
-    static constexpr float processor_sr_inv = 1.f / 32000;
-    int old_nmb_mode = 0;
-    bool nimbusTrigger{false};
-
-    using resamp_t = sst::basic_blocks::dsp::LanczosResampler<BLOCK_SIZE>;
-    std::unique_ptr<resamp_t> surgeSR_to_euroSR, euroSR_to_surgeSR;
-
-    static constexpr int raw_out_sz = BLOCK_SIZE_OS << 5; // power of 2 pls
-    float resampled_output[2][raw_out_sz];                // at sr
-    size_t resampReadPtr = 0, resampWritePtr = 1;         // see comment in init
-
-    static constexpr int nimbusprocess_blocksize = 8;
-    float stub_input[2][nimbusprocess_blocksize]; // This is the extra sample we have around
-    size_t numStubs{0};
-    int consumed = 0, created = 0;
-    bool builtBuffer{false};
 };
 
 #endif // SURGE_NIMBUSEFFECT_H
