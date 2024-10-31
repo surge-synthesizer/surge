3ac78737544956639900cabae5a0edf17ca6fcc1
Paul Walker <paul@pwjw.com>
2024-10-31 17:33:57 -0400

    format

--- src/common/dsp/effects/chowdsp/bbd_utils/BBDNonlin.h
+++ src/common/dsp/effects/chowdsp/bbd_utils/BBDNonlin.h
@@ -196,7 +196,7 @@ class BBDNonlin
 
     void setDrive(float newDrive) { drive = newDrive; }
 
-    inline __m128 processSample(__m128 Vg) noexcept 
+    inline __m128 processSample(__m128 Vg) noexcept
     {
         Vin->setVoltage(Vg);
         Is->setCurrent(getCurrent(Vg, Vp));
@@ -208,7 +208,7 @@ class BBDNonlin
         auto vd = vLoad1(drive);
         auto vdm = vLoad1(1.0f - drive);
         auto a1 = vMul(Vp, vd);
-        auto a2 = vMul(Vg,vdm);
+        auto a2 = vMul(Vg, vdm);
         auto res = vAdd(a1, a2);
         return res;
     }
