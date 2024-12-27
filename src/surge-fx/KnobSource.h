54ce67cde2746252c5e14cb2ce4e3a94ac244221
yoshih <christophershih2@gmail.com>
2024-12-28 00:14:36 +0800

    cleanup

---
+++ src/surge-fx/KnobSource.h
@@ -0,0 +1,39 @@
+#pragma once // this doesn't seem like the norm in this repo but still a wip
+
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <sst/jucegui/data/Continuous.h>
+#include <sst/jucegui/data/Discrete.h>
+#include <sst/jucegui/util/DebugHelpers.h>
+
+struct ConcreteCM : sst::jucegui::data::ContinuousModulatable
+{
+    ConcreteCM() {};
+    std::string label{"A Knob"};
+    std::string getLabel() const override { return label; }
+    float value{0};
+    float getValue() const override { return value; }
+    float getDefaultValue() const override { return (getMax() - getMin()) / 2.0; }
+    void setValueFromGUI(const float &f) override
+    {
+        value = f;
+        for (auto *l : guilisteners)
+            l->dataChanged();
+        for (auto *l : modellisteners)
+            l->dataChanged();
+    }
+    void setValueFromModel(const float &f) override
+    {
+        value = f;
+        for (auto *l : guilisteners)
+            l->dataChanged();
+    }
+
+    float min{0}, max{1};
+    float getMin() const override { return min; }
+    float getMax() const override { return max; }
+
+    float mv{0.2};
+    float getModulationValuePM1() const override { return mv; }
+    void setModulationValuePM1(const float &f) override { mv = f; }
+    bool isModulationBipolar() const override { return isBipolar(); } // sure why not
+};
