c01f9e1947e31f9c00d9a810ea5673d3bca8e387
Paweł Ulita <pawelulita@users.noreply.github.com>
2025-05-21 18:47:44 +0200

    Fix popup menu scaling
    Make sure that the popup menus follow the scale of the rest of the
    interface.

--- src/surge-xt/gui/SurgeGUIEditor.cpp
+++ src/surge-xt/gui/SurgeGUIEditor.cpp
@@ -2424,6 +2424,9 @@ bool SurgeGUIEditor::open(void *parent)
     frame->setBounds(0, 0, currentSkin->getWindowSizeX(), currentSkin->getWindowSizeY());
     frame->setSurgeGUIEditor(this);
 
+    menu_scaler = std::make_unique<juce::Component>();
+    juceEditor->addChildComponent(*menu_scaler);
+
     // Comment this in to always start with focus debugger
     // debugFocus = true;
     // y-frame->debugFocus = true;
@@ -2889,7 +2892,7 @@ void SurgeGUIEditor::toggleMPE()
 juce::PopupMenu::Options SurgeGUIEditor::popupMenuOptions(const juce::Point<int> &where)
 {
     auto o = juce::PopupMenu::Options();
-    o = o.withTargetComponent(juceEditor);
+    o = o.withTargetComponent(*menu_scaler);
     if (where.x > 0 && where.y > 0)
     {
         auto r = juce::Rectangle<int>().withWidth(1).withHeight(1).withPosition(
@@ -3060,6 +3063,12 @@ void SurgeGUIEditor::setZoomFactor(float zf, bool resizeWindow)
     if (frame)
     {
         frame->setTransform(juce::AffineTransform().scaled(zff));
+    }
+
+    if (menu_scaler)
+    {
+        // 0.75 scales the menus so that they match the rest of the UI.
+        menu_scaler->setTransform(juce::AffineTransform().scaled(zff * 0.75));
     }
 
     if (oscWaveform)
