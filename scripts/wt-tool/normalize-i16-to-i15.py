af1400977d76599d6d8dae5dfb07a73e3da97e25
EvilDragon <mario.kruselj@gmail.com>
2025-08-20 09:27:39 +0200

    Copy .wt fileformat doc into doc folder (#8078)
    Also tweak some verbiage in the related Python scripts
    
    Also fix Pulse.wt header to contain the correct number of frames (2 instead of 3)

--- scripts/wt-tool/normalize-i16-to-i15.py
+++ scripts/wt-tool/normalize-i16-to-i15.py
@@ -8,13 +8,16 @@ import wave
 def main():
     parser = OptionParser(usage="usage: % prog [options] infile outfile")
     parser.add_option("-n", "--normalize", dest="normalize", default="half", metavar="MODE",
-                      help="""(Create only) how to normalize input.
-'half' input wav are divided by 2 (so 2^16 range becomes 2^15 range);
-'peak' input wav are scanned and re-peaked to 2^15.
+                      help="""(Create only) Sets how the input should be normalized. Options are:
+'none' leave input untouched
+'half' input WAV is divided by 2 (so 2^16 range becomes 2^15 range)
+'peak' input WAV is scanned and normalized to -6 dBFS
+'peak16' input WAV is scanned and normalized to 0 dBFS
 """)
     (options, args) = parser.parse_args()
+
     if(len(args) != 2):
-        print("Please specify an input file and output file\n")
+        print("Please specify an input and an output file!\n")
         parser.print_help()
         return
 
@@ -29,20 +32,24 @@ def main():
         wavdata = wf.readframes(nf * sw * c0)
 
     if (c0 != 1):
-        raise RuntimeError("wt-tool only processes mono inputs")
+        raise RuntimeError("Please use 44.1k, 16-bit, mono WAV files! This file has more than one audio channel.")
     if (fr != 44100):
-        raise RuntimeError("Please use 44.1k mono 16bit PCM wav files")
+        raise RuntimeError("Please use 44.1k, 16-bit, mono WAV files! This file has a different sample rate.")
     if (sw != 2):
-        raise RuntimeError("Please use 44.1k mono 16bit PCM wav files")
+        raise RuntimeError("Please use 44.1k, 16-bit, mono WAV files. This file is not 16-bit.")
 
     newrec = bytearray(len(wavdata))
 
     if(options.normalize == "half"):
+        print("Normalizing by half")
+
         for i in range(int(len(wavdata) / 2)):
             ls = wavdata[2 * i]
             ms = wavdata[2 * i + 1]
+
             if(ms >= 128):
                 ms -= 256
+
             r = int((ls + ms * 256) / 2)
 
             nls = int(r % 256)
@@ -50,45 +57,69 @@ def main():
 
             if(r < 0 and nls != 0):
                 nms -= 1
+
             if(nms < 0):
                 nms += 256
+
             newrec[2 * i] = nls
             newrec[2 * i + 1] = nms
 
-    elif (options.normalize == "peak"):
+    elif (options.normalize == "peak" or options.normalize == "peak16"):
+        print("Normalizing to " + options.normalize)
+
         peakp = 0
         peakm = 0
+
         d = wavdata
+
         for i in range(int(len(d) / 2)):
             ls = d[2 * i]
             ms = d[2 * i + 1]
+
             if(ms >= 128):
                 ms -= 256
+
             r = int(ls + ms * 256)
+
             if(r > peakp):
                 peakp = r
+
             if(r < peakm):
                 peakm = r
+
         if(-peakm > peakp):
             peakp = -peakm
+
         print("Peak value is ", peakp)
+
+        den = 16384.0
+
+        if (norm == "peak16"):
+            den = den * 2
+
         for i in range(int(len(d) / 2)):
             ls = d[2 * i]
             ms = d[2 * i + 1]
+
             if(ms >= 128):
                 ms -= 256
-            r = int((ls + ms * 256) * 16384.0 / peakp)
+
+            r = int((ls + ms * 256) * den / peakp)
 
             nls = int(r % 256)
             nms = int(r / 256)
+
             if(r < 0 and nls != 0):
                 nms -= 1
+
             if(nms < 0):
                 nms += 256
+
             newrec[2 * i] = nls
             newrec[2 * i + 1] = nms
+
     else:
-        print("Unknown normalization ", options.normalize)
+        print(f"Unknown normalization {options.normalize}!")
         parser.print_help()
 
     with wave.open(outf, "wb") as wav_file:
--- scripts/wt-tool/wt-info.py
+++ scripts/wt-tool/wt-info.py
@@ -1,16 +1,32 @@
 import os
 
-for dirpath, dirnames, files in os.walk("resources/data"):
-    for name in files:
-        if name.lower().endswith(".wt"):
-            wt = os.path.join(dirpath, name)
-            with open(wt, "rb") as f:
-                header = {}
-                header["ident"] = f.read(4)
-                # FIXME: Check that this is actually vawt
+yn = ["no", "yes"]
+fi = ["16-bit integer", "32-bit float"]
+pk = ["-6 dBFS", "0 dBFS"]
 
+for dirpath, dirnames, files in os.walk(os.getcwd()):
+    for f in files:
+        path = os.path.join(dirpath, f)
+
+        if os.path.isfile(path) and path.lower().endswith(".wt"):
+
+            with open(path, "rb") as f:
+                header = {}
+
+                header["ident"] = f.read(4)
                 header["wavsz"] = f.read(4)
                 header["wavct"] = f.read(2)
                 header["flags"] = f.read(2)
 
-                print( wt, " ", header["flags" ][0], header["flags"][1] )
+                size  = int.from_bytes(header["wavsz"], "little")
+                count = int.from_bytes(header["wavct"], "little")
+                flags = int.from_bytes(header["flags"], "little")
+
+                print(os.path.relpath(path))
+                print("  frame size    : " + f"{size} samples")
+                print("  frame count   : " + str(count))
+                print("  is sample     : " + yn[flags &  1 == 1])
+                print("  is looped     : " + yn[flags &  2 == 1])
+                print("  sample format : " + fi[flags &  4 == 1])
+                print("  peak value    : " + pk[flags &  8 == 1])
+                print("  has metadata  : " + yn[flags & 10 == 1])
