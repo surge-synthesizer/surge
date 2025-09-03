e024ce707128e5c232e3e1ea1758d6a9412544a3
nuoun <10226424+nuoun@users.noreply.github.com>
2025-09-03 23:34:12 +0200

    Find and call vcvars64_wrapper.bat directly from build-msvc-luajit.bat

--- libs/luajitlib/build-msvc-luajit.bat
+++ libs/luajitlib/build-msvc-luajit.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set BD=%~1
+set "BD=%~1"
 if "%BD%"=="" (
     echo Usage: build-msvc-luajit.bat BUILDDIR
     exit /b 1
@@ -20,8 +20,32 @@ mkdir "%HD%"
 
 xcopy /e /i /h LuaJIT "%SD%\LuaJIT"
 copy /y msvcbuild.bat "%SD%\LuaJIT\src\"
-cd "%SD%\LuaJIT\src"
 
+cd /d "%BD%"
+set "P=%BD%"
+
+:find_vcvars
+if exist "%P%\CMakeFiles\vcvars64_wrapper.bat" (
+    set "VCVARS=%P%\CMakeFiles\vcvars64_wrapper.bat"
+    goto found_vcvars
+)
+
+cd ..
+set "NEWP=%CD%"
+
+if /i "%NEWP%"=="%P%" (
+    echo ERROR: couldn't locate vcvars64_wrapper.bat starting from "%BD%"
+    exit /b 1
+)
+
+set "P=%NEWP%"
+goto find_vcvars
+
+:found_vcvars
+echo Found vcvars64_wrapper.bat at "%VCVARS%"
+call "%VCVARS%"
+
+cd /d "%SD%\LuaJIT\src"
 call msvcbuild.bat static
 
 copy lua51.lib "%OD%"
