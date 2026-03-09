226f7abced1791ee59cd6ab531ba4a6159ad9986
Paul Walker <paul@pwjw.com>
2026-03-09 08:51:35 -0400

    Revert the SurgeFX on JuceGUI changes
    The various surge fx on jucegui changes were incomplete. Since
    we should probably just rewrite this all using the json tools
    in short circuit anyway, lets revert for now so we can think
    about shipping a 1.4.
    
    This addresses the latch problem in logic and the accesible
    crash in the item
    
    This reverts commit 65fb51fa54176be876f3b1d329d43d3d06a145d7.
    This reverts commit cf87ac7bd23cf3ece771a75c8f33101d6f8938e1.
    
    Closes #8284
    Closes #8278

--- src/surge-fx/SurgeFXEditor.h
+++ src/surge-fx/SurgeFXEditor.h
@@ -24,14 +24,9 @@
 #define SURGE_SRC_SURGE_FX_SURGEFXEDITOR_H
 
 #include "SurgeFXProcessor.h"
-#include "ParameterPanel.h"
 #include "SurgeLookAndFeel.h"
-#include "KnobSource.h"
 
 #include "juce_gui_basics/juce_gui_basics.h"
-
-#include <sst/jucegui/style/StyleSheet.h>
-#include <sst/jucegui/components/Knob.h>
 
 //==============================================================================
 /**
@@ -57,7 +52,7 @@ class SurgefxAudioProcessorEditor : public juce::AudioProcessorEditor,
     };
     std::vector<FxMenu> menu;
     std::unique_ptr<juce::Component> picker;
-    std::unique_ptr<ParameterPanel> deafultParameterPanel;
+
     static constexpr int topSection = 80;
 
     void makeMenu();
@@ -113,6 +108,66 @@ class SurgefxAudioProcessorEditor : public juce::AudioProcessorEditor,
     static constexpr int baseWidth = 600, baseHeight = 55 * 6 + 80 + topSection;
 
   private:
+    struct AccSlider : public juce::Slider
+    {
+        AccSlider() { setWantsKeyboardFocus(true); }
+        juce::String getTextFromValue(double v) override
+        {
+            // std::cout << "GTFV " << v << std::endl;
+            // return juce::Slider::getTextFromValue(v);
+            //  This is a bit of a hack to externalize this but
+            return tv;
+        }
+
+        juce::String tv;
+        void setTextValue(juce::String s)
+        {
+            tv = s;
+            if (auto *handler = getAccessibilityHandler())
+            {
+                handler->notifyAccessibilityEvent(juce::AccessibilityEvent::valueChanged);
+            }
+        }
+        bool keyPressed(const juce::KeyPress &key) override
+        {
+            float amt = 0.05;
+            if (key.getModifiers().isShiftDown())
+                amt = 0.01;
+            if (key.getKeyCode() == juce::KeyPress::upKey)
+            {
+                setValue(std::clamp(getValue() + amt, 0., 1.),
+                         juce::NotificationType::sendNotification);
+                return true;
+            }
+
+            if (key.getKeyCode() == juce::KeyPress::downKey)
+            {
+                setValue(std::clamp(getValue() - amt, 0., 1.),
+                         juce::NotificationType::sendNotification);
+                return true;
+            }
+
+            if (key.getKeyCode() == juce::KeyPress::homeKey)
+            {
+                setValue(1., juce::NotificationType::sendNotification);
+                return true;
+            }
+
+            if (key.getKeyCode() == juce::KeyPress::endKey)
+            {
+                setValue(0., juce::NotificationType::sendNotification);
+                return true;
+            }
+            return false;
+        }
+    };
+    AccSlider fxParamSliders[n_fx_params];
+    SurgeFXParamDisplay fxParamDisplay[n_fx_params];
+    SurgeTempoSyncSwitch fxTempoSync[n_fx_params];
+    SurgeTempoSyncSwitch fxDeactivated[n_fx_params];
+    SurgeTempoSyncSwitch fxExtended[n_fx_params];
+    SurgeTempoSyncSwitch fxAbsoluted[n_fx_params];
+
     void blastToggleState(int i);
     void resetLabels();
 
