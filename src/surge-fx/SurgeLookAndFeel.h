2f3d32d78db6708278d35fd83736137ba62b698f
Paul <baconpaul@users.noreply.github.com>
2026-03-09 09:27:28 -0400

    Revert the SurgeFX on JuceGUI changes (#8295)
    The various surge fx on jucegui changes were incomplete. Since
    we should probably just rewrite this all using the json tools
    in short circuit anyway, lets revert for now so we can think
    about shipping a 1.4.
    
    This addresses the latch problem in logic and the accesible
    crash in the item
    
    This reverts commit 65fb51fa54176be876f3b1d329d43d3d06a145d7.
    This reverts commit cf87ac7bd23cf3ece771a75c8f33101d6f8938e1.
    
    Closes #8284
    Closes #8278

--- src/surge-fx/SurgeLookAndFeel.h
+++ src/surge-fx/SurgeLookAndFeel.h
@@ -20,8 +20,6 @@
  * https://github.com/surge-synthesizer/surge
  */
 
-#ifndef SURGE_SRC_SURGE_FX_SURGELOOKANDFEEL_H
-#define SURGE_SRC_SURGE_FX_SURGELOOKANDFEEL_H
 #include "version.h"
 
 #include "juce_gui_basics/juce_gui_basics.h"
@@ -226,7 +224,7 @@ class SurgeLookAndFeel : public juce::LookAndFeel_V4
                                        shouldDrawButtonAsDown);
     }
 
-    void drawCornerResizer(juce::Graphics &g, int w, int h, bool, bool) override {}
+    void drawCornerResizer(juce::Graphics &g, int w, int h, bool, bool) override {};
 
     void paintComponentBackground(juce::Graphics &g, int w, int h)
     {
@@ -512,4 +510,3 @@ class SurgeTempoSyncSwitch : public juce::ToggleButton
             g.fillRoundedRectangle(kbounds, controlRadius);
     }
 };
-#endif // SURGE_SRC_SURGE_FX_SURGELOOKANDFEEL_H
