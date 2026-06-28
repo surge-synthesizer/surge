058e3582721449b7213285d3c0276907018e360e
nuoun <10226424+nuoun@users.noreply.github.com>
2026-06-28 19:26:44 +0200

    Add Find and Goto line to the Lua editors' Prelude tabs

--- src/surge-xt/gui/overlays/LuaEditors.cpp
+++ src/surge-xt/gui/overlays/LuaEditors.cpp
@@ -776,6 +776,11 @@ bool CodeEditorSearch::keyPressed(const juce::KeyPress &key, juce::Component *or
 
 void CodeEditorSearch::replaceResults(bool all)
 {
+    if (ed->isReadOnly())
+    {
+        return;
+    }
+
     if (resultTotal > 0)
     {
         resultHasChanged = true;
@@ -1053,6 +1058,28 @@ void SurgeCodeEditorComponent::mouseDrag(const juce::MouseEvent &event)
 
 bool SurgeCodeEditorComponent::keyPressed(const juce::KeyPress &key)
 {
+    // Prelude find
+    if (search != nullptr && key.getModifiers().isCommandDown() && key.isKeyCode('F'))
+    {
+        if (gotoLine != nullptr)
+        {
+            gotoLine->hide();
+        }
+        search->show();
+        search->showReplace(false);
+        return true;
+    }
+
+    // Prelude go to line
+    if (gotoLine != nullptr && key.getModifiers().isCommandDown() && key.isKeyCode('G'))
+    {
+        if (search != nullptr)
+        {
+            search->hide();
+        }
+        gotoLine->show();
+        return true;
+    }
 
     // update search results
     if (search != nullptr)
@@ -1164,6 +1191,11 @@ void SurgeCodeEditorComponent::addPopupMenuItems(juce::PopupMenu &menuToAddTo,
     if (search == nullptr)
     {
         find.setEnabled(false);
+        replace.setEnabled(false);
+    }
+
+    if (isReadOnly())
+    {
         replace.setEnabled(false);
     }
 
@@ -2751,6 +2783,8 @@ FormulaModulatorEditor::FormulaModulatorEditor(SurgeGUIEditor *ed, SurgeStorage 
     preludeDisplay->setScrollbarThickness(8);
     preludeDisplay->setTitle("Formula Modulator Prelude Code");
     preludeDisplay->setDescription("Formula Modulator Prelude Code");
+    preludeDisplay->setSearch(*search);
+    preludeDisplay->setGotoLine(*gotoLine);
 
     EditorColors::setColorsFromSkin(preludeDisplay.get(), skin);
 
@@ -2875,16 +2909,38 @@ void FormulaModulatorEditor::resized()
 
 void FormulaModulatorEditor::showModulatorCode()
 {
+    const bool keepFocus = hasKeyboardFocus(true);
+
+    search->hide();
+    gotoLine->hide();
     preludeDisplay->setVisible(false);
     mainEditor->setVisible(true);
+    search->setEditor(*mainEditor);
+    gotoLine->setEditor(*mainEditor);
     getEditState().codeOrPrelude = 0;
+
+    if (keepFocus)
+    {
+        mainEditor->grabKeyboardFocus();
+    }
 }
 
 void FormulaModulatorEditor::showPreludeCode()
 {
+    const bool keepFocus = hasKeyboardFocus(true);
+
+    search->hide();
+    gotoLine->hide();
     preludeDisplay->setVisible(true);
     mainEditor->setVisible(false);
+    search->setEditor(*preludeDisplay);
+    gotoLine->setEditor(*preludeDisplay);
     getEditState().codeOrPrelude = 1;
+
+    if (keepFocus)
+    {
+        preludeDisplay->grabKeyboardFocus();
+    }
 }
 
 void FormulaModulatorEditor::updateDebuggerIfNeeded()
@@ -3812,6 +3868,9 @@ WavetableScriptEditor::WavetableScriptEditor(SurgeGUIEditor *ed, SurgeStorage *s
     preludeDisplay->setScrollbarThickness(8);
     preludeDisplay->setTitle("Wavetable Prelude Code");
     preludeDisplay->setDescription("Wavetable Prelude Code");
+    preludeDisplay->setSearch(*search);
+    preludeDisplay->setGotoLine(*gotoLine);
+
     EditorColors::setColorsFromSkin(preludeDisplay.get(), skin);
 
     controlArea = std::make_unique<WavetableScriptControlArea>(this, editor);
@@ -3948,16 +4007,38 @@ void WavetableScriptEditor::resized()
 
 void WavetableScriptEditor::showModulatorCode()
 {
+    const bool keepFocus = hasKeyboardFocus(true);
+
+    search->hide();
+    gotoLine->hide();
     preludeDisplay->setVisible(false);
     mainEditor->setVisible(true);
+    search->setEditor(*mainEditor);
+    gotoLine->setEditor(*mainEditor);
     getEditState().codeOrPrelude = 0;
+
+    if (keepFocus)
+    {
+        mainEditor->grabKeyboardFocus();
+    }
 }
 
 void WavetableScriptEditor::showPreludeCode()
 {
+    const bool keepFocus = hasKeyboardFocus(true);
+
+    search->hide();
+    gotoLine->hide();
     preludeDisplay->setVisible(true);
     mainEditor->setVisible(false);
+    search->setEditor(*preludeDisplay);
+    gotoLine->setEditor(*preludeDisplay);
     getEditState().codeOrPrelude = 1;
+
+    if (keepFocus)
+    {
+        preludeDisplay->grabKeyboardFocus();
+    }
 }
 
 void WavetableScriptEditor::rerenderFromUIState()
