surge-synthesizer/surge

commit 4a6d358fe55c27a07cbe85659b381c95a91995b5

Only refresh CPU meter if the integer % value changed, not when decimals change (#8465)
Fixes #8462

src/surge-xt/gui/SurgeGUIEditor.cpp
@@ -1045,15 +1045,29 @@ void SurgeGUIEditor::idle()
 
             vu->setIsAudioActive(synth->audio_processing_active);
 
-            if (synth->cpu_level != vu->getCpuLevel())
-            {
-                vu->setCpuLevel(synth->cpu_level);
-                vuInvalid = true;
-            }
+            bool showCPU = Surge::Storage::getUserDefaultValue(&(synth->storage),
+                                                               Surge::Storage::ShowCPUUsage, false);
 
-            if (vuInvalid)
+            if (showCPU)
             {
-                vu->repaint();
+                auto quantizedCpuPct = [](float f) {
+                    return (int)(std::clamp(f, 0.f, 1.f) * 100.f);
+                };
+
+                bool cpuLevelChanged =
+                    quantizedCpuPct(synth->cpu_level) != quantizedCpuPct(vu->getCpuLevel());
+
+                vu->setCpuLevel(synth->cpu_level);
+
+                if (cpuLevelChanged)
+                {
+                    vuInvalid = true;
+                }
+
+                if (vuInvalid)
+                {
+                    vu->repaint();
+                }
             }
         }
 

commit f21b19f4bde766280411f8102786f541e435976a

Fix regression caused by #8465
Move the VU meter repaint block outside of showCPU block.

Also, fix an off-by-one indexing for Disable/Enable Tempo Sync for all LFO params menu entries.

Addresses #8462

src/surge-xt/gui/SurgeGUIEditor.cpp
@@ -1039,8 +1039,8 @@ void SurgeGUIEditor::idle()
         {
             if (synth->vu_peak[0] != vu->getValue())
             {
-                vuInvalid = true;
                 vu->setValue(synth->vu_peak[0]);
+                vuInvalid = true;
             }
 
             if (synth->vu_peak[1] != vu->getValueR())
@@ -1069,11 +1069,11 @@ void SurgeGUIEditor::idle()
                 {
                     vuInvalid = true;
                 }
+            }
 
-                if (vuInvalid)
-                {
-                    vu->repaint();
-                }
+            if (vuInvalid)
+            {
+                vu->repaint();
             }
         }
 

src/surge-xt/gui/SurgeGUIEditorValueCallbacks.cpp
@@ -2109,11 +2109,11 @@ int32_t SurgeGUIEditor::controlModifierClicked(Surge::GUI::IComponentTagValue *c
                         }
                         else if (p->ctrlgroup == cg_LFO)
                         {
-                            if (a >= ms_lfo1 && a <= ms_lfo1 + n_lfos_voice)
+                            if (a >= ms_lfo1 && a < ms_lfo1 + n_lfos_voice)
                             {
                                 prefix = "Voice LFO " + std::to_string(a - ms_lfo1 + 1);
                             }
-                            else if (a >= ms_slfo1 && a <= ms_slfo1 + n_lfos_scene)
+                            else if (a >= ms_slfo1 && a < ms_slfo1 + n_lfos_scene)
                             {
                                 prefix = "Scene LFO " + std::to_string(a - ms_slfo1 + 1);
                             }